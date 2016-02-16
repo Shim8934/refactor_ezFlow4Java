8aaccdd0d7b407cec99d9de4c7ac7ab7c7095231
yoonz44 <yoonz44@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-02-16 00:35:29 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1349 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezOrgan/service/impl/EzOrganServiceImpl.java
@@ -0,0 +1,10 @@
+package egovframework.ezEKP.ezOrgan.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import egovframework.ezEKP.ezOrgan.service.EzOrganService;
+
+@Service("EzOrganService")
+public class EzOrganServiceImpl implements EzOrganService {
+
+}
