11cdb3bac723d36d1a48d4e2bafce81d00e35aea
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-02-18 04:26:40 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1391 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezBoard/vo/BoardConfigVO.java
+++ src/main/java/egovframework/ezEKP/ezBoard/vo/BoardConfigVO.java
@@ -4,68 +4,52 @@ public class BoardConfigVO {
 	private String userId; //유저아이디
 	private int listCount; // 보여줄 리스트 수
 	private String preview; // OFF , W, H 표시
-	private int previewWlist; // 오른쪽 리스트
+	private int previewWList; // 오른쪽 리스트
 	private int previewWContent; // 오른쪽 미리보기 영역
-	private int previewHlist; // 아래쪽 리스트
+	private int previewHList; // 아래쪽 리스트
 	private int previewHContent; // 아래쪽 미리보기 영역
 	
-	public BoardConfigVO() {
-		
-	}
-
 	public String getUserId() {
 		return userId;
 	}
-
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-
 	public int getListCount() {
 		return listCount;
 	}
-
 	public void setListCount(int listCount) {
 		this.listCount = listCount;
 	}
-
 	public String getPreview() {
 		return preview;
 	}
-
 	public void setPreview(String preview) {
 		this.preview = preview;
 	}
-
-	public int getPreviewWlist() {
-		return previewWlist;
+	public int getPreviewWList() {
+		return previewWList;
 	}
-
-	public void setPreviewWlist(int previewWlist) {
-		this.previewWlist = previewWlist;
+	public void setPreviewWList(int previewWList) {
+		this.previewWList = previewWList;
 	}
-
 	public int getPreviewWContent() {
 		return previewWContent;
 	}
-
 	public void setPreviewWContent(int previewWContent) {
 		this.previewWContent = previewWContent;
 	}
-
-	public int getPreviewHlist() {
-		return previewHlist;
+	public int getPreviewHList() {
+		return previewHList;
 	}
-
-	public void setPreviewHlist(int previewHlist) {
-		this.previewHlist = previewHlist;
+	public void setPreviewHList(int previewHList) {
+		this.previewHList = previewHList;
 	}
-
 	public int getPreviewHContent() {
 		return previewHContent;
 	}
-
 	public void setPreviewHContent(int previewHContent) {
 		this.previewHContent = previewHContent;
 	}
+	
 }
