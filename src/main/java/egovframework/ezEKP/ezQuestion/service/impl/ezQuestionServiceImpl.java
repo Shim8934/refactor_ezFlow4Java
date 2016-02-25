7c1a6629625c10b1b50501b045cb0782bc2cefd1
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-02-25 04:03:18 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1454 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezQuestion/service/impl/ezQuestionServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezQuestion/service/impl/ezQuestionServiceImpl.java
@@ -1,10 +1,15 @@
 package egovframework.ezEKP.ezQuestion.service.impl;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import egovframework.ezEKP.ezQuestion.dao.ezQuestionDAO;
 import egovframework.ezEKP.ezQuestion.service.ezQuestionService;
 
 @Service("EzQuestionService")
 public class ezQuestionServiceImpl implements ezQuestionService{
-
+	
+	@Resource(name="EzQuestionDAO")
+	private ezQuestionDAO ezQuestionDAO;
 }
