b6f19be7a6f1efbd5702b5146ac2ccb0a8c111d4
hyojin0414 <hyojin0414@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-02-26 08:05:04 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1492 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezQuestion/vo/QuestionListVO.java
+++ src/main/java/egovframework/ezEKP/ezQuestion/vo/QuestionListVO.java
@@ -1,5 +1,80 @@
 package egovframework.ezEKP.ezQuestion.vo;
 
-public class QuestionListVO {
+import java.sql.Date;
 
+public class QuestionListVO {
+	/** 설문번호*/
+	public int itemNo;
+	/** 유저아이디*/
+	public String userID;
+	/** 제목*/
+	public String title;
+	/** 이라ㅓㅁㄴ이ㅏㅓㄹ 주소값*/
+	public String receve;
+	/** 기명여부*/
+	public String publicFlg;
+	/** 유효일*/
+	public Date pollEndDate;
+	/** 대상자*/
+	public String responsRange;
+	/** 유저이름*/
+	public String userName;
+	/** 결과공개*/
+	public String resultFlg;
+	
+	public int getItemNo() {
+		return itemNo;
+	}
+	public void setItemNo(int itemNo) {
+		this.itemNo = itemNo;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getReceve() {
+		return receve;
+	}
+	public void setReceve(String receve) {
+		this.receve = receve;
+	}
+	public String getPublicFlg() {
+		return publicFlg;
+	}
+	public void setPublicFlg(String publicFlg) {
+		this.publicFlg = publicFlg;
+	}
+	public Date getPollEndDate() {
+		return pollEndDate;
+	}
+	public void setPollEndDate(Date pollEndDate) {
+		this.pollEndDate = pollEndDate;
+	}
+	public String getResponsRange() {
+		return responsRange;
+	}
+	public void setResponsRange(String responsRange) {
+		this.responsRange = responsRange;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getResultFlg() {
+		return resultFlg;
+	}
+	public void setResultFlg(String resultFlg) {
+		this.resultFlg = resultFlg;
+	}
+	
 }
