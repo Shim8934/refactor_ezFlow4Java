03857f57ab5d3c21b3beafc3c00f7dab8470891a
hyojin0414 <hyojin0414@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-03 08:44:18 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1503 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/webapp/js/ezQuestion/lang/ezQuestion.js
@@ -0,0 +1,15 @@
+// \ezQuestion\poll\js\composeappt.js
+// \ezQuestion\poll\js\ezcomposeappt.js
+// \ezQuestion\poll\js\string_component.js
+// \ezQuestion\poll\common.js
+var strLang1 = "문장이 처음 나오는 위치 반환"
+var strLang2 = "모든 이미지 파일";
+var strLang3 = "모든 음악 파일";
+var strLang4 = "모든 동영상 파일";
+// \ezQuestion\common.js
+var strLang39 = "이전"
+var strLang40 = "다음"
+var strLang41 = "총 "
+var strLang42 = "개"
+var strLang43 = " 명"
+var strLang44 = "본인이 작성한 설문만 재사용 가능합니다.";
