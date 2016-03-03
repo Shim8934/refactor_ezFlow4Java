3fdb26846b4ddfe6a7ef0e301c6dee02be0f55c4
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-03 08:51:17 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1514 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezQuestion/service/EzQuestionService.java
+++ src/main/java/egovframework/ezEKP/ezQuestion/service/EzQuestionService.java
@@ -3,8 +3,7 @@ package egovframework.ezEKP.ezQuestion.service;
 import java.util.List;
 
 import egovframework.ezEKP.ezQuestion.vo.QuestionListVO;
-
-
+	 
 public interface EzQuestionService {
 	public int getQstListCnt(QuestionListVO questionListVO) throws Exception;
 	
