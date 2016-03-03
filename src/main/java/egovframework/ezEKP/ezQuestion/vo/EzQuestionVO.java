92f76c187637ee873e52f26e81d6da727b1f2cd0
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-03 08:49:13 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1513 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezQuestion/vo/EzQuestionVO.java
+++ src/main/java/egovframework/ezEKP/ezQuestion/vo/EzQuestionVO.java
@@ -1,5 +1,145 @@
 package egovframework.ezEKP.ezQuestion.vo;
 
 public class EzQuestionVO {
-
+	private String txtExpiredate; 
+	private String setAnonymity;
+	private String hidanonymity; 
+	private String hidopenResult; 
+	private String hidMultiResponse;
+	private String hidTarget; 
+	private String brdId; 
+	private String brdNm; 
+	private String brdPostterm; 
+	private String itemNo; 
+	private String hidStartDate; 
+	private String hidEndDate; 
+	private String selectYN; 
+	private String RangeXMLStr; 
+	private String setMultiResponse; 
+	private String setopenResult; 
+	private String importance; 
+	private String setTarget; 
+	private String txtSubject; 
+	private String txtContent;
+	
+	public String getTxtExpiredate() {
+		return txtExpiredate;
+	}
+	public void setTxtExpiredate(String txtExpiredate) {
+		this.txtExpiredate = txtExpiredate;
+	}
+	public String getSetAnonymity() {
+		return setAnonymity;
+	}
+	public void setSetAnonymity(String setAnonymity) {
+		this.setAnonymity = setAnonymity;
+	}
+	public String getHidanonymity() {
+		return hidanonymity;
+	}
+	public void setHidanonymity(String hidanonymity) {
+		this.hidanonymity = hidanonymity;
+	}
+	public String getHidopenResult() {
+		return hidopenResult;
+	}
+	public void setHidopenResult(String hidopenResult) {
+		this.hidopenResult = hidopenResult;
+	}
+	public String getHidMultiResponse() {
+		return hidMultiResponse;
+	}
+	public void setHidMultiResponse(String hidMultiResponse) {
+		this.hidMultiResponse = hidMultiResponse;
+	}
+	public String getHidTarget() {
+		return hidTarget;
+	}
+	public void setHidTarget(String hidTarget) {
+		this.hidTarget = hidTarget;
+	}
+	public String getBrdId() {
+		return brdId;
+	}
+	public void setBrdId(String brdId) {
+		this.brdId = brdId;
+	}
+	public String getBrdNm() {
+		return brdNm;
+	}
+	public void setBrdNm(String brdNm) {
+		this.brdNm = brdNm;
+	}
+	public String getBrdPostterm() {
+		return brdPostterm;
+	}
+	public void setBrdPostterm(String brdPostterm) {
+		this.brdPostterm = brdPostterm;
+	}
+	public String getItemNo() {
+		return itemNo;
+	}
+	public void setItemNo(String itemNo) {
+		this.itemNo = itemNo;
+	}
+	public String getHidStartDate() {
+		return hidStartDate;
+	}
+	public void setHidStartDate(String hidStartDate) {
+		this.hidStartDate = hidStartDate;
+	}
+	public String getHidEndDate() {
+		return hidEndDate;
+	}
+	public void setHidEndDate(String hidEndDate) {
+		this.hidEndDate = hidEndDate;
+	}
+	public String getSelectYN() {
+		return selectYN;
+	}
+	public void setSelectYN(String selectYN) {
+		this.selectYN = selectYN;
+	}
+	public String getRangeXMLStr() {
+		return RangeXMLStr;
+	}
+	public void setRangeXMLStr(String rangeXMLStr) {
+		RangeXMLStr = rangeXMLStr;
+	}
+	public String getSetMultiResponse() {
+		return setMultiResponse;
+	}
+	public void setSetMultiResponse(String setMultiResponse) {
+		this.setMultiResponse = setMultiResponse;
+	}
+	public String getSetopenResult() {
+		return setopenResult;
+	}
+	public void setSetopenResult(String setopenResult) {
+		this.setopenResult = setopenResult;
+	}
+	public String getImportance() {
+		return importance;
+	}
+	public void setImportance(String importance) {
+		this.importance = importance;
+	}
+	public String getSetTarget() {
+		return setTarget;
+	}
+	public void setSetTarget(String setTarget) {
+		this.setTarget = setTarget;
+	}
+	public String getTxtSubject() {
+		return txtSubject;
+	}
+	public void setTxtSubject(String txtSubject) {
+		this.txtSubject = txtSubject;
+	}
+	public String getTxtContent() {
+		return txtContent;
+	}
+	public void setTxtContent(String txtContent) {
+		this.txtContent = txtContent;
+	}
 }
