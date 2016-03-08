9acfb216b3eadbbbe18fd2b071583d549c23c938
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-08 12:28:09 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1578 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezQuestion/vo/StepSaveVO.java
@@ -0,0 +1,138 @@
+package egovframework.ezEKP.ezQuestion.vo;
+
+public class StepSaveVO {
+	private int brdId;
+	private int itemNo;
+	private String userId;
+	private String userNm;
+	private String userNm2;
+	private String userEmail;
+	private String title;
+	private String content;
+	private String postDate;
+	private int postTerm;
+	private int itemRef;
+	private String itemImp;
+	private String sDate;
+	private String eDate;
+	private int dataCount;
+	private String resultFlg;
+	private String publicFlg;
+	private String multiFlg;
+	private String resRange;
+	
+	public int getBrdId() {
+		return brdId;
+	}
+	public void setBrdId(int brdId) {
+		this.brdId = brdId;
+	}
+	public int getItemNo() {
+		return itemNo;
+	}
+	public void setItemNo(int itemNo) {
+		this.itemNo = itemNo;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserNm() {
+		return userNm;
+	}
+	public void setUserNm(String userNm) {
+		this.userNm = userNm;
+	}
+	public String getUserNm2() {
+		return userNm2;
+	}
+	public void setUserNm2(String userNm2) {
+		this.userNm2 = userNm2;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPostDate() {
+		return postDate;
+	}
+	public void setPostDate(String postDate) {
+		this.postDate = postDate;
+	}
+	public int getPostTerm() {
+		return postTerm;
+	}
+	public void setPostTerm(int postTerm) {
+		this.postTerm = postTerm;
+	}
+	public int getItemRef() {
+		return itemRef;
+	}
+	public void setItemRef(int itemRef) {
+		this.itemRef = itemRef;
+	}
+	public String getItemImp() {
+		return itemImp;
+	}
+	public void setItemImp(String itemImp) {
+		this.itemImp = itemImp;
+	}
+	public String getsDate() {
+		return sDate;
+	}
+	public void setsDate(String sDate) {
+		this.sDate = sDate;
+	}
+	public String geteDate() {
+		return eDate;
+	}
+	public void seteDate(String eDate) {
+		this.eDate = eDate;
+	}
+	public int getDataCount() {
+		return dataCount;
+	}
+	public void setDataCount(int dataCount) {
+		this.dataCount = dataCount;
+	}
+	public String getResultFlg() {
+		return resultFlg;
+	}
+	public void setResultFlg(String resultFlg) {
+		this.resultFlg = resultFlg;
+	}
+	public String getPublicFlg() {
+		return publicFlg;
+	}
+	public void setPublicFlg(String publicFlg) {
+		this.publicFlg = publicFlg;
+	}
+	public String getMultiFlg() {
+		return multiFlg;
+	}
+	public void setMultiFlg(String multiFlg) {
+		this.multiFlg = multiFlg;
+	}
+	public String getResRange() {
+		return resRange;
+	}
+	public void setResRange(String resRange) {
+		this.resRange = resRange;
+	}
+}
