ac0de0e4d9b77eca03eb992ea48a6dbe3d13ce26
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-09 00:27:57 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1583 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezQuestion/service/EzQuestionService.java
+++ src/main/java/egovframework/ezEKP/ezQuestion/service/EzQuestionService.java
@@ -1,7 +1,7 @@
 package egovframework.ezEKP.ezQuestion.service;
 
 import java.util.List;
-
+import java.util.Map;
 import egovframework.ezEKP.ezQuestion.vo.QuestionListVO;
 import egovframework.ezEKP.ezQuestion.vo.UserPermissionVO;
 import egovframework.ezEKP.ezQuestion.vo.UserPollItemVO;
@@ -11,10 +11,21 @@ public interface EzQuestionService {
 	
 	public List<QuestionListVO> getQstList(QuestionListVO questionListVO) throws Exception;
 	
+	public int getItemNoCnt(int brdId,int itemNo) throws Exception;
+	
+	public String getItemSeq(String brdId) throws Exception;
+	
+	public void stepSave(String pUserID, Map<String, Object> map) throws Exception;
+	
+	public void stepSave2(Map<String, Object> map) throws Exception;
+	
+	public void insertItemSeq(String brdId) throws Exception;
+	
+	public void updateItemSeq(int brdId,int itemNo) throws Exception;
+	
 	public UserPollItemVO getUserPollItem(UserPollItemVO userPollItemVO) throws Exception;
 	
 	public UserPermissionVO getUserPermission(UserPermissionVO userPermissionVO) throws Exception;
 	
 	public int getUserResponseCnt(UserPermissionVO userPermissionVO) throws Exception;
-	
 }
