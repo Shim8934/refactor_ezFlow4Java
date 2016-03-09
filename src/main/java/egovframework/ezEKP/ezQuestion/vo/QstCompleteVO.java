4ae69792ec554721b477fc234f4650fa647976b2
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-09 02:49:38 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1590 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezQuestion/vo/QstCompleteVO.java
@@ -0,0 +1,208 @@
+package egovframework.ezEKP.ezQuestion.vo;
+
+public class QstCompleteVO {
+	private int strBrdID;
+	private int itemNo;
+	private String gubunFg;
+	private String gubunID;
+	private String gubunNm;
+	private String gubunNm2;
+	private String userID;
+	private String userNm;
+	private String userNm2;
+	private String userEmail;
+	private String userDeptID;
+	private String userDeptNm;
+	private String userDeptNm2;
+	private String userPOS;
+	private String userPOS2;
+	private String userGender;
+	private int userAge;
+	private int count;
+	private int quesNo;
+	private String quesContent;
+	private int answerType;
+	private String multiSelect;
+	private int answerNo;
+	private int attachNo;
+	private String attachName;
+	private String attachURL;
+	private String attachType;
+	private String answerAnswerContent;
+	private String answerContent;
+	
+	public int getStrBrdID() {
+		return strBrdID;
+	}
+	public void setStrBrdID(int strBrdID) {
+		this.strBrdID = strBrdID;
+	}
+	public int getItemNo() {
+		return itemNo;
+	}
+	public void setItemNo(int itemNo) {
+		this.itemNo = itemNo;
+	}
+	public String getGubunFg() {
+		return gubunFg;
+	}
+	public void setGubunFg(String gubunFg) {
+		this.gubunFg = gubunFg;
+	}
+	public String getGubunID() {
+		return gubunID;
+	}
+	public void setGubunID(String gubunID) {
+		this.gubunID = gubunID;
+	}
+	public String getGubunNm() {
+		return gubunNm;
+	}
+	public void setGubunNm(String gubunNm) {
+		this.gubunNm = gubunNm;
+	}
+	public String getGubunNm2() {
+		return gubunNm2;
+	}
+	public void setGubunNm2(String gubunNm2) {
+		this.gubunNm2 = gubunNm2;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserNm() {
+		return userNm;
+	}
+	public void setUserNm(String userNm) {
+		this.userNm = userNm;
+	}
+	public String getUserNm2() {
+		return userNm2;
+	}
+	public void setUserNm2(String userNm2) {
+		this.userNm2 = userNm2;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserDeptID() {
+		return userDeptID;
+	}
+	public void setUserDeptID(String userDeptID) {
+		this.userDeptID = userDeptID;
+	}
+	public String getUserDeptNm() {
+		return userDeptNm;
+	}
+	public void setUserDeptNm(String userDeptNm) {
+		this.userDeptNm = userDeptNm;
+	}
+	public String getUserDeptNm2() {
+		return userDeptNm2;
+	}
+	public void setUserDeptNm2(String userDeptNm2) {
+		this.userDeptNm2 = userDeptNm2;
+	}
+	public String getUserPOS() {
+		return userPOS;
+	}
+	public void setUserPOS(String userPOS) {
+		this.userPOS = userPOS;
+	}
+	public String getUserPOS2() {
+		return userPOS2;
+	}
+	public void setUserPOS2(String userPOS2) {
+		this.userPOS2 = userPOS2;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getQuesNo() {
+		return quesNo;
+	}
+	public void setQuesNo(int quesNo) {
+		this.quesNo = quesNo;
+	}
+	public String getQuesContent() {
+		return quesContent;
+	}
+	public void setQuesContent(String quesContent) {
+		this.quesContent = quesContent;
+	}
+	public int getAnswerType() {
+		return answerType;
+	}
+	public void setAnswerType(int answerType) {
+		this.answerType = answerType;
+	}
+	public String getMultiSelect() {
+		return multiSelect;
+	}
+	public void setMultiSelect(String multiSelect) {
+		this.multiSelect = multiSelect;
+	}
+	public int getAnswerNo() {
+		return answerNo;
+	}
+	public void setAnswerNo(int answerNo) {
+		this.answerNo = answerNo;
+	}
+	public int getAttachNo() {
+		return attachNo;
+	}
+	public void setAttachNo(int attachNo) {
+		this.attachNo = attachNo;
+	}
+	public String getAttachName() {
+		return attachName;
+	}
+	public void setAttachName(String attachName) {
+		this.attachName = attachName;
+	}
+	public String getAttachURL() {
+		return attachURL;
+	}
+	public void setAttachURL(String attachURL) {
+		this.attachURL = attachURL;
+	}
+	public String getAttachType() {
+		return attachType;
+	}
+	public void setAttachType(String attachType) {
+		this.attachType = attachType;
+	}
+	public String getAnswerAnswerContent() {
+		return answerAnswerContent;
+	}
+	public void setAnswerAnswerContent(String answerAnswerContent) {
+		this.answerAnswerContent = answerAnswerContent;
+	}
+	public String getAnswerContent() {
+		return answerContent;
+	}
+	public void setAnswerContent(String answerContent) {
+		this.answerContent = answerContent;
+	}
+}
