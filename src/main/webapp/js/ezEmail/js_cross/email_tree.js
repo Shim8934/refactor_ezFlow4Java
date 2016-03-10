74be7a07016aa017cf89cb6f260b840f5e99d568
skyblue0o0 <skyblue0o0@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-10 00:23:13 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1618 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/webapp/js/ezEmail/js_cross/email_tree.js
@@ -0,0 +1,189 @@
+﻿function get_childXML_2010(url, broot, bcount) {
+    if (navigator.userAgent.indexOf('MSIE') == -1) {
+        var xmlDOM = createXmlDom();
+
+        var objRoot = xmlDOM.createElement("DATA");
+        xmlDOM.appendChild(objRoot);
+
+        var objNode = xmlDOM.createElement("URL");
+        objNode.appendChild(xmlDOM.createTextNode(url));
+        objRoot.appendChild(objNode);
+
+        var objNode = xmlDOM.createElement("BCOUNT");
+        if(bcount)
+            objNode.appendChild(xmlDOM.createTextNode("-1"));
+        else
+            objNode.appendChild(xmlDOM.createTextNode("0"));
+        objRoot.appendChild(objNode);
+
+        var xmlHTTP = new XMLHttpRequest();
+        xmlHTTP.open("POST", "/myoffice/ezEmail/remote/mail_get_folderlist.aspx", false);
+	    xmlHTTP.send(xmlDOM);
+    	
+    	
+	    if( xmlHTTP.status != 207 && xmlHTTP.status != 200 )
+	    {
+	        xmlHTTP = null;
+	        return '';
+	    }
+    	
+	    return xmlHTTP.responseText;
+    }
+    else {
+        var xmlHTTP = new ActiveXObject("Microsoft.XMLHttp");
+        var xmlDOM = new ActiveXObject("Microsoft.XMLDOM");
+
+        var objRoot = xmlDOM.createNode(1, "DATA", "");
+        xmlDOM.appendChild(objRoot);
+
+        var objNode = xmlDOM.createNode(1, "URL", "");
+        objNode.text = url;
+        objRoot.appendChild(objNode);
+
+        var objNode = xmlDOM.createNode(1, "BCOUNT", "");
+        objNode.text = bcount;
+        objRoot.appendChild(objNode);
+
+        xmlHTTP.open("POST", "/myoffice/ezEmail/remote/mail_get_folderlist.aspx", false);
+	    xmlHTTP.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
+	    xmlHTTP.send(xmlDOM.xml);
+    	
+    	
+	    if( xmlHTTP.Status != 207 && xmlHTTP.Status != 200 )
+	    {
+	        xmlHTTP = null;
+	        return '';
+	    }
+    	
+	    return xmlHTTP.responseText;
+    }
+}
+
+function get_childXML(url, broot, bcount)
+{
+    return get_childXML_2010(url, broot, bcount);
+    
+	var strXML = "<?xml version='1.0'?>" + 
+					"<d:searchrequest xmlns:d='DAV:'>" +
+					"<d:sql>" +
+					"Select \"DAV:displayname\", \"DAV:href\", \"DAV:hassubs\", " +
+					"\"urn:schemas:httpmail:unreadcount\" " +
+					"From scope('Shallow traversal of \"" + url + "\"')" +
+					"Where \"DAV:isfolder\" = True And " +
+					"\"DAV:ishidden\" = False " +
+					"Order By \"DAV:displayname\"" +
+					"</d:sql>" +
+					"</d:searchrequest>";
+		     
+		     
+	var xmlHTTP = new ActiveXObject("Microsoft.XMLHttp");
+	xmlHTTP.open("SEARCH", url, false);
+	xmlHTTP.setRequestHeader("Content-Type", "text/xml; charset=utf-8");		
+	xmlHTTP.send(strXML);
+	
+	if( xmlHTTP.Status == 207 || xmlHTTP.Status == 200 )
+	{
+		var NameNodes = xmlHTTP.responseXML.selectNodes("a:multistatus/a:response/a:propstat/a:prop/a:displayname");
+		var HrefNodes = xmlHTTP.responseXML.selectNodes("a:multistatus/a:response/a:propstat/a:prop/a:href");
+		var HasSubNodes = xmlHTTP.responseXML.selectNodes("a:multistatus/a:response/a:propstat/a:prop/a:hassubs");
+		var UnreadNodes = xmlHTTP.responseXML.selectNodes("a:multistatus/a:response/a:propstat/a:prop/d:unreadcount");
+		xmlHTTP = null;
+        
+		var childXML = "";
+
+		if (broot)
+		{
+		    if(lang != "3")
+		    {
+			    var FolderNameKo = new Array( "받은 편지함" ,  "보낸 편지함" ,  "임시 보관함" ,  "지운 편지함" ,  "개인편지함" );
+			    var FolderNameEn = new Array("Inbox", "Sent Items", "Drafts", "Deleted Items", "PERSONAL");
+			    var DispName = new Array(strLang68, strLang69, strLang70, strLang4, strLang71);
+			}
+			else
+			{
+			    var FolderNameKo = new Array( "받은 편지함", "임시 보관함",  "보낸 편지함" ,  "지운 편지함" ,  "개인편지함" );
+			    var FolderNameEn = new Array("Inbox", "Drafts", "Sent Items", "Deleted Items", "PERSONAL");
+			    var DispName = new Array(strLang68, strLang70, strLang69, strLang4, strLang71);
+			}
+			
+			var PersonalExist = false;
+
+			for (var j=0; j<5; j++)
+				for (var i=0; i<NameNodes.length; i++)
+				{
+					if (NameNodes.item(i).text != FolderNameKo[j] && NameNodes.item(i).text != FolderNameEn[j])
+						continue;
+					
+					if (NameNodes.item(i).text == "PERSONAL")
+						PersonalExist = true;
+					
+					if (NameNodes.item(i).text ==  strLang67)
+						PersonalExist = true;
+					
+
+					childXML += "<node imgidx='1' caption=\"";
+					if (UnreadNodes.item(i).text != "0" && bcount)
+						childXML += (MakeRightField(DispName[j]) + "(" + UnreadNodes.item(i).text + ")");
+					else
+						childXML += (MakeRightField(DispName[j]) + "\" ");
+			
+					childXML += ("foldername=\"" + MakeRightField(DispName[j]) + "\" ");
+					childXML += ("href='" + HrefNodes.item(i).text + "' ");
+					if (HasSubNodes.item(i).text == "1")
+						childXML += "hassub='1' ";
+		
+					childXML += "/>";
+					break;
+				}
+
+			if (!PersonalExist)
+			{
+				CreateFolder(url + "/PERSONAL");
+				childXML += "<node imgidx='1' caption='" + strLang71 + "' href='" + url + "/PERSONAL" + "' />"
+			}
+		}
+		else
+			for (var i=0; i<NameNodes.length; i++)
+			{
+				childXML += "<node imgidx='1' caption=\"";
+				if (UnreadNodes.item(i).text != "0" && bcount)
+					childXML += (MakeRightField(NameNodes.item(i).text) + "(" + UnreadNodes.item(i).text + ")");
+				else
+					childXML += (MakeRightField(NameNodes.item(i).text) + "\" ");
+			
+				childXML += ("foldername=\"" + MakeRightField(NameNodes.item(i).text) + "\" ");
+				childXML += ("href='" + HrefNodes.item(i).text + "' ");
+				if (HasSubNodes.item(i).text == "1")
+					childXML += "hassub='1' ";
+	
+				childXML += "/>";
+			}
+
+		return childXML;
+	}
+	else
+	{
+		xmlHTTP = null;
+		return "";
+	}
+}
+
+function MakeRightField(orgStr)
+{
+	return ReplaceText(ReplaceText(ReplaceText(ReplaceText(orgStr, "&", "&amp;"), "\"", "&quot;"), "<", "&lt;"), ">", "&gt;");
+}
+
+function ReplaceText( orgStr, findStr, replaceStr )
+{
+	var re = new RegExp( findStr, "gi" );
+	
+	return ( orgStr.replace( re, replaceStr ) );
+}
+
+function CreateFolder(url) {
+    var xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    xmlHttp.Open("MKCOL", url, false);
+    xmlHttp.send();
+    xmlHttp = null;
+}
+
