60bc0a7f9bef46e8373a19347055386c69225dc6
dhlee <dhlee@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-13 12:03:16 +0000

    . Mail List Controller update
    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1672 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezEmail/web/EzEmailMailListController.java
+++ src/main/java/egovframework/ezEKP/ezEmail/web/EzEmailMailListController.java
@@ -5,6 +5,8 @@ import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +17,7 @@ import egovframework.let.utl.fcc.service.CommonUtil;
 
 @Controller
 @RequestMapping("/ezEmail/mailList.do")
-public class EzMailMailListControllder {
+public class EzEmailMailListController {
 	
 	@Autowired
 	private CommonUtil commonUtil;
@@ -23,7 +25,16 @@ public class EzMailMailListControllder {
 	@Autowired
 	private Properties config;
 
+    private static final Logger logger = LoggerFactory.getLogger(EzEmailMailListController.class);
+	
+	@RequestMapping()
 	public String showMailList(HttpServletRequest request, Model model, LoginVO loginVO, HttpServletResponse response) throws Exception{
+		logger.debug("showMailList started");
+		
+		model.addAttribute("folderName", "받은 편지함");
+		
+		logger.debug("showMailList ended");
+		
 		return "ezEmail/mailList";
 	}
 	
