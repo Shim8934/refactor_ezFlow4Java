761ec3aad0bf770582e88621f6752ed62e01092e
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-15 06:45:33 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1697 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezQuestion/vo/QstStep1VO.java
@@ -0,0 +1,152 @@
+package egovframework.ezEKP.ezQuestion.vo;
+
+public class QstStep1VO {
+	private String txtExpiredate; 
+	private String setAnonymity;
+	private String hidAnonymity; 
+	private String hidOpenResult; 
+	private String hidMultiResponse;
+	private String hidTarget; 
+	private String brdId; 
+	private String brdNm; 
+	private String brdPostterm; 
+	private String itemNo; 
+	private String hidStartDate; 
+	private String hidEndDate; 
+	private String selectYN; 
+	private String RangeXMLStr; 
+	private String setMultiResponse; 
+	private String setOpenResult; 
+	private String importance; 
+	private String setTarget; 
+	private String txtSubject; 
+	private String txtContent;
+	private String itemId;
+	
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public String getTxtExpiredate() {
+		return txtExpiredate;
+	}
+	public void setTxtExpiredate(String txtExpiredate) {
+		this.txtExpiredate = txtExpiredate;
+	}
+	public String getSetAnonymity() {
+		return setAnonymity;
+	}
+	public void setSetAnonymity(String setAnonymity) {
+		this.setAnonymity = setAnonymity;
+	}
+	public String getHidAnonymity() {
+		return hidAnonymity;
+	}
+	public void setHidAnonymity(String hidAnonymity) {
+		this.hidAnonymity = hidAnonymity;
+	}
+	public String getHidOpenResult() {
+		return hidOpenResult;
+	}
+	public void setHidOpenResult(String hidOpenResult) {
+		this.hidOpenResult = hidOpenResult;
+	}
+	public String getHidMultiResponse() {
+		return hidMultiResponse;
+	}
+	public void setHidMultiResponse(String hidMultiResponse) {
+		this.hidMultiResponse = hidMultiResponse;
+	}
+	public String getHidTarget() {
+		return hidTarget;
+	}
+	public void setHidTarget(String hidTarget) {
+		this.hidTarget = hidTarget;
+	}
+	public String getBrdId() {
+		return brdId;
+	}
+	public void setBrdId(String brdId) {
+		this.brdId = brdId;
+	}
+	public String getBrdNm() {
+		return brdNm;
+	}
+	public void setBrdNm(String brdNm) {
+		this.brdNm = brdNm;
+	}
+	public String getBrdPostterm() {
+		return brdPostterm;
+	}
+	public void setBrdPostterm(String brdPostterm) {
+		this.brdPostterm = brdPostterm;
+	}
+	public String getItemNo() {
+		return itemNo;
+	}
+	public void setItemNo(String itemNo) {
+		this.itemNo = itemNo;
+	}
+	public String getHidStartDate() {
+		return hidStartDate;
+	}
+	public void setHidStartDate(String hidStartDate) {
+		this.hidStartDate = hidStartDate;
+	}
+	public String getHidEndDate() {
+		return hidEndDate;
+	}
+	public void setHidEndDate(String hidEndDate) {
+		this.hidEndDate = hidEndDate;
+	}
+	public String getSelectYN() {
+		return selectYN;
+	}
+	public void setSelectYN(String selectYN) {
+		this.selectYN = selectYN;
+	}
+	public String getRangeXMLStr() {
+		return RangeXMLStr;
+	}
+	public void setRangeXMLStr(String rangeXMLStr) {
+		RangeXMLStr = rangeXMLStr;
+	}
+	public String getSetMultiResponse() {
+		return setMultiResponse;
+	}
+	public void setSetMultiResponse(String setMultiResponse) {
+		this.setMultiResponse = setMultiResponse;
+	}
+	public String getSetOpenResult() {
+		return setOpenResult;
+	}
+	public void setSetOpenResult(String setOpenResult) {
+		this.setOpenResult = setOpenResult;
+	}
+	public String getImportance() {
+		return importance;
+	}
+	public void setImportance(String importance) {
+		this.importance = importance;
+	}
+	public String getSetTarget() {
+		return setTarget;
+	}
+	public void setSetTarget(String setTarget) {
+		this.setTarget = setTarget;
+	}
+	public String getTxtSubject() {
+		return txtSubject;
+	}
+	public void setTxtSubject(String txtSubject) {
+		this.txtSubject = txtSubject;
+	}
+	public String getTxtContent() {
+		return txtContent;
+	}
+	public void setTxtContent(String txtContent) {
+		this.txtContent = txtContent;
+	}
+}
