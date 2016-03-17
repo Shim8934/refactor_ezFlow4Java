2be6415e63eda34dc68d8281f2e11de6a2f03833
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-17 05:44:44 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1754 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezQuestion/dao/EzQuestionDAO.java
+++ src/main/java/egovframework/ezEKP/ezQuestion/dao/EzQuestionDAO.java
@@ -13,39 +13,14 @@ import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
 @Repository("EzQuestionDAO")
 public class EzQuestionDAO extends EgovAbstractDAO{
 	
-	public int getQstListCnt(Map<String, Object> map){
-		select("EzQuestionDAO.getQstListCnt", map);
-		return (int) map.get("v_pCount");
-	}
-	
 	@SuppressWarnings("unchecked")
 	public List<QstListVO> getQstList(Map<String, Object> map){
 		return (List<QstListVO>) list("EzQuestionDAO.getQstList", map);
 	}
 	
-	public int getItemNoCnt(Map<String, Object> map) throws Exception {
-		select("EzQuestionDAO.getItemNoCnt", map);
-		return (int) map.get("v_pCount");
-	}
-	
-	public void stepSave(Map<String,Object> map) {
-		update("EzQuestionDAO.stepSave", map);
-	}
-	
-	public String getItemSeq(String brdId) {
-		return (String)select("EzQuestionDAO.getItemSeq", brdId);
-	}
-	
-	public void insertItemSeq(String brdId) {
-		select("EzQuestionDAO.insertItemSeq", brdId);
-	}
-	
-	public void updateItemSeq(Map<String,Object> map) {
-		update("EzQuestionDAO.updateItemSeq", map);
-	}
-	
-	public void stepSave2(Map<String,Object> map) {
-		update("EzQuestionDAO.stepSave2", map);
+	@SuppressWarnings("unchecked")
+	public List<String> getUserIdAdmin(int brdId) {
+		return (List<String>) select("EzQuestionDAO.getUserIdAdmin", brdId);
 	}
 	
 	public QstUserPollItemVO getUserPollItem(Map<String, Object> map){
@@ -56,13 +31,43 @@ public class EzQuestionDAO extends EgovAbstractDAO{
 		return (QstUserPermissionVO) select("EzQuestionDAO.getUserPermission", map);
 	}
 	
+	public String getItemSeq(String brdId) {
+		return (String)select("EzQuestionDAO.getItemSeq", brdId);
+	}
+	
+	public int getQstListCnt(Map<String, Object> map){
+		select("EzQuestionDAO.getQstListCnt", map);
+		return (int) map.get("v_pCount");
+	}
+	
+	public int getItemNoCnt(Map<String, Object> map) throws Exception {
+		select("EzQuestionDAO.getItemNoCnt", map);
+		return (int) map.get("v_pCount");
+	}
+	
 	public int getUserResponseCnt(Map<String, Object> map){
 		return (int) select("EzQuestionDAO.getUserResponseCnt", map);
 	}
 	
-	@SuppressWarnings("unchecked")
-	public List<String> getUserIdAdmin(int brdId) {
-		return (List<String>) select("EzQuestionDAO.getUserIdAdmin", brdId);
+	public int getQuestionNo(Map<String,Object> map) throws Exception {
+		select("EzQuestionDAO.getQuestionNo", map);
+		int temp = 0;
+		if(map.get("v_pCount") != null) {
+			temp = (int)map.get("v_pCount");
+		}
+		return temp;
+	}
+	
+	public int getResponseDateCnt(Map<String,Object> map){
+		return (int) select("EzQuestionDAO.getResponseDateCnt", map);
+	}
+	
+	public int resCount(Map<String, Object> map) {
+		return (int) select("EzQuestionDAO.resCount", map);
+	}
+	
+	public int getReadDateItem(Map<String, Object> map) {
+		return (int) select("EzQuestionDAO.getReadDateItem", map);
 	}
 	
 	public void callCreateMother(Map<String,Object> map) {
@@ -75,15 +80,6 @@ public class EzQuestionDAO extends EgovAbstractDAO{
 	
 	public void callInsertPollResponseper(Map<String,Object> map) {
 		insert("EzQuestionDAO.callInsertPollResponseper", map);
-	}
-	
-	public int getQuestionNo(Map<String,Object> map) throws Exception {
-		select("EzQuestionDAO.getQuestionNo", map);
-		int temp = 0;
-		if(map.get("v_pCount") != null) {
-			temp = (int)map.get("v_pCount");
-		}
-		return temp;
 	}
 	
 	public void insertQuestion(Map<String,Object> map) {
@@ -102,35 +98,38 @@ public class EzQuestionDAO extends EgovAbstractDAO{
 		insert("EzQuestionDAO.insertAnswerContent", map);
 	}
 	
+	public void insertItemRead(Map<String, Object> map) {
+		insert("EzQuestionDAO.insertItemRead", map);
+	}
+	
+	public void insertItemSeq(String brdId) {
+		select("EzQuestionDAO.insertItemSeq", brdId);
+	}
+	
+	public void stepSave(Map<String,Object> map) {
+		update("EzQuestionDAO.stepSave", map);
+	}
+	
+	public void updateItemSeq(Map<String,Object> map) {
+		update("EzQuestionDAO.updateItemSeq", map);
+	}
+	
+	public void stepSave2(Map<String,Object> map) {
+		update("EzQuestionDAO.stepSave2", map);
+	}
+	
 	public void updatePollItem(Map<String,Object> map) {
 		update("EzQuestionDAO.updatePollItem", map);
 	}
 	
-	public void deleteItem(Map<String,Object> map) {
-		delete("EzQuestionDAO.deleteItem", map);
-	}
-	
-	public int getResponseDateCnt(Map<String,Object> map){
-		return (int) select("EzQuestionDAO.getResponseDateCnt", map);
-	}
-	
-	public int resCount(Map<String, Object> map) {
-		return (int) select("EzQuestionDAO.resCount", map);
-	}
-
 	public void updateReadCnt(Map<String, Object> map) {
 		update("EzQuestionDAO.updateReadCnt", map);
-	}
-
-	public int getReadDateItem(Map<String, Object> map) {
-		return (int) select("EzQuestionDAO.getReadDateItem", map);
 	}
 
 	public void updateReadDate(Map<String, Object> map) {
 		update("EzQuestionDAO.updateReadDate", map);
 	}
-
-	public void insertItemRead(Map<String, Object> map) {
-		insert("EzQuestionDAO.insertItemRead", map);
+	public void deleteItem(Map<String,Object> map) {
+		delete("EzQuestionDAO.deleteItem", map);
 	}
 }
