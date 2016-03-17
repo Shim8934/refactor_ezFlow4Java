60637febbbe196f68359466d218049a9ecaba635
hyojin0414 <hyojin0414@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-03-17 05:43:03 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1751 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezQuestion/service/EzQuestionService.java
+++ src/main/java/egovframework/ezEKP/ezQuestion/service/EzQuestionService.java
@@ -1,74 +1,72 @@
 package egovframework.ezEKP.ezQuestion.service;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import egovframework.ezEKP.ezQuestion.vo.QstCompleteVO;
 import egovframework.ezEKP.ezQuestion.vo.QstListVO;
-import egovframework.ezEKP.ezQuestion.vo.QstVO;
 import egovframework.ezEKP.ezQuestion.vo.QstUserPermissionVO;
 import egovframework.ezEKP.ezQuestion.vo.QstUserPollItemVO;
+import egovframework.ezEKP.ezQuestion.vo.QstVO;
 import egovframework.let.user.login.vo.LoginVO;
 
-	 
 public interface EzQuestionService {
-
-	public int getQstListCnt(QstListVO qstListVO) throws Exception;
 
 	public List<QstListVO> getQstList(QstListVO qstListVO) throws Exception;
 
-	public int getItemNoCnt(int brdId,int itemNo) throws Exception;
-	
-	public String getItemSeq(String brdId) throws Exception;
-	
-	public void stepSave(String pUserID, Map<String, Object> map) throws Exception;
-	
-	public void stepSave2(Map<String, Object> map) throws Exception;
-	
-	public void insertItemSeq(String brdId) throws Exception;
-	
-	public void updateItemSeq(int brdId,int itemNo) throws Exception;
-	
-	public QstUserPollItemVO getUserPollItem(QstUserPollItemVO qstUserPollItemVO) throws Exception;
-	
-	public QstUserPermissionVO getUserPermission(QstUserPermissionVO qstUserPermissionVO) throws Exception;
-	
-	public int getUserResponseCnt(QstUserPermissionVO userPermissionVO,String userId) throws Exception;
-	
+	public List<QstVO> getQuestionForResponse(QstVO questionVO);
+
 	public List<String> getUserIdAdmin(int brd) throws Exception;
-	
-	public void callCreateMother(QstCompleteVO qstCompleteVO) throws Exception;
-	
-	public void callInsertPollResponsep1(QstCompleteVO qstCompleteVO) throws Exception;
-	
-	public void callInsertPollResponseper(QstCompleteVO qstCompleteVO) throws Exception;
-	
+
+	public QstUserPollItemVO getUserPollItem(QstUserPollItemVO qstUserPollItemVO) throws Exception;
+
+	public QstUserPermissionVO getUserPermission(QstUserPermissionVO qstUserPermissionVO) throws Exception;
+
+	public String getItemSeq(String brdId) throws Exception;
+
+	public int getQstListCnt(QstListVO qstListVO) throws Exception;
+
+	public int getItemNoCnt(int brdId,int itemNo) throws Exception;
+
+	public int getUserResponseCnt(QstUserPermissionVO userPermissionVO,String userId) throws Exception;
+
 	public int getQuestionNo(int brdId,int itemNo) throws Exception;
-	
-	public void insertQuestion(QstCompleteVO qstCompleteVO) throws Exception;
-	
-	public void pollSaveAttach(QstCompleteVO qstCompleteVO) throws Exception;
-	
-	public void insertAnswerAnswerContent(QstCompleteVO qstCompleteVO) throws Exception;
-	
-	public void insertAnswerContent(QstCompleteVO qstCompleteVO) throws Exception;
-	
-	public void updatePollItem(QstCompleteVO qstCompleteVO) throws Exception;
-	
-	public void deleteItem(QstCompleteVO qstCompleteVO) throws Exception;
 
 	public int getResponseDateCnt(QstUserPermissionVO userPermissionVO, String userId);
+	
+	public int getReadDateItem(QstUserPollItemVO qstUserPollItemVO, String userId);
 
 	public int resCount(String brdId,String itemNo);
 
+	public void stepSave(String pUserID, Map<String, Object> map) throws Exception;
+
+	public void stepSave2(Map<String, Object> map) throws Exception;
+
+	public void insertItemSeq(String brdId) throws Exception;
+	
+	public void insertQuestion(QstCompleteVO qstCompleteVO) throws Exception;
+	
+	public void insertAnswerAnswerContent(QstCompleteVO qstCompleteVO) throws Exception;
+
+	public void insertAnswerContent(QstCompleteVO qstCompleteVO) throws Exception;
+	
+	public void insertItemRead(LoginVO loginVO, QstUserPollItemVO qstUserPollItemVO, String readDate);
+	
+	public void callCreateMother(QstCompleteVO qstCompleteVO) throws Exception;
+
+	public void callInsertPollResponsep1(QstCompleteVO qstCompleteVO) throws Exception;
+
+	public void callInsertPollResponseper(QstCompleteVO qstCompleteVO) throws Exception;
+	
+	public void pollSaveAttach(QstCompleteVO qstCompleteVO) throws Exception;
+
+	public void updateItemSeq(int brdId,int itemNo) throws Exception;
+	
+	public void updatePollItem(QstCompleteVO qstCompleteVO) throws Exception;
+	
 	public void updateReadCnt(QstUserPollItemVO qstUserPollItemVO);
 
-	public int getReadDateItem(QstUserPollItemVO qstUserPollItemVO, String userId);
-	
 	public void updateReadDate(QstUserPollItemVO userPollItemVO, String readDate, String userId);
 
-	public List<QstVO> getQuestionForResponse(QstVO questionVO);
-
-	public void insertItemRead(LoginVO loginVO, QstUserPollItemVO qstUserPollItemVO, String readDate);
+	public void deleteItem(QstCompleteVO qstCompleteVO) throws Exception;	
 }
