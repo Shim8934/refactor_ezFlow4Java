7b9d250336ecf9d7830a662312121ba327755cdc
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-01 04:23:24 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1906 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezQuestion/vo/QstRangeSelectVO.java
@@ -0,0 +1,62 @@
+package egovframework.ezEKP.ezQuestion.vo;
+
+public class QstRangeSelectVO {
+	private String brdId;
+	private String itemId;
+	private String strGenderACL0;
+	private String strGenderACL1;
+	private String strGenderACL2;
+	private String userInfoDeptCode;
+	private String pCompanyID;
+	private String serverName;
+	
+	public String getBrdId() {
+		return brdId;
+	}
+	public void setBrdId(String brdId) {
+		this.brdId = brdId;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public String getStrGenderACL0() {
+		return strGenderACL0;
+	}
+	public void setStrGenderACL0(String strGenderACL0) {
+		this.strGenderACL0 = strGenderACL0;
+	}
+	public String getStrGenderACL1() {
+		return strGenderACL1;
+	}
+	public void setStrGenderACL1(String strGenderACL1) {
+		this.strGenderACL1 = strGenderACL1;
+	}
+	public String getStrGenderACL2() {
+		return strGenderACL2;
+	}
+	public void setStrGenderACL2(String strGenderACL2) {
+		this.strGenderACL2 = strGenderACL2;
+	}
+	public String getUserInfoDeptCode() {
+		return userInfoDeptCode;
+	}
+	public void setUserInfoDeptCode(String userInfoDeptCode) {
+		this.userInfoDeptCode = userInfoDeptCode;
+	}
+	public String getpCompanyID() {
+		return pCompanyID;
+	}
+	public void setpCompanyID(String pCompanyID) {
+		this.pCompanyID = pCompanyID;
+	}
+	public String getServerName() {
+		return serverName;
+	}
+	public void setServerName(String serverName) {
+		this.serverName = serverName;
+	}
+}
