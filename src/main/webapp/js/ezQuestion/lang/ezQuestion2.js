5bf194fc81f71ee7e0b041e75c7febc7aaf504ee
hyojin0414 <hyojin0414@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-05 09:31:20 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1934 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/webapp/js/ezQuestion/lang/ezQuestion2.js
@@ -0,0 +1,15 @@
+// \ezQuestion\poll\js\composeappt.js
+// \ezQuestion\poll\js\ezcomposeappt.js
+// \ezQuestion\poll\js\string_component.js
+// \ezQuestion\poll\common.js
+var strLang1 = "문장이 처음 나오는 위치 반환"
+var strLang2 = "All image file";
+var strLang3 = "All music file";
+var strLang4 = "All video file";
+// \ezQuestion\common.js
+
+var strLang39 = "Prev"
+var strLang40 = "Next"
+var strLang41 = "Total "
+var strLang42 = "-items"
+var strLang43 = "  persons"
---
+++ src/main/webapp/js/ezQuestion/lang/ezQuestion4.js
@@ -0,0 +1,15 @@
+// \ezQuestion\poll\js\composeappt.js
+// \ezQuestion\poll\js\ezcomposeappt.js
+// \ezQuestion\poll\js\string_component.js
+// \ezQuestion\poll\common.js
+var strLang1 = "返还最初显示文章的位置。"
+var strLang2 = "所有图象文件";
+var strLang3 = "所有音乐文件";
+var strLang4 = "所有视频​​文件";
+// \ezQuestion\common.js
+
+var strLang39 = "上一页"
+var strLang40 = "下一页"
+var strLang41 = "总 "
+var strLang42 = "个"
+var strLang43 = " 名"
