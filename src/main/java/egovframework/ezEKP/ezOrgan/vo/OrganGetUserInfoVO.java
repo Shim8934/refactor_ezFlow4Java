0b600826465bbb3a905f082e3a77f33f66a6c440
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-06 10:09:46 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@1956 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezOrgan/vo/OrganGetUserInfoVO.java
@@ -0,0 +1,89 @@
+package egovframework.ezEKP.ezOrgan.vo;
+
+public class OrganGetUserInfoVO {
+	private String cn;
+	/** 회사/부서명*/
+	private String department;
+	/** 부서명*/
+	private String displayName;
+	/** 부서명(다국어)*/
+	private String displayName1;
+	/** 부서명(다국어)*/
+	private String displayName2;	
+	/** 메일주소*/
+	private String mail;
+	/** */
+	private String description;
+	/** */
+	private String description1;
+	/** */
+	private String description2;
+	/** */
+	private String physicalDeliveryOfficeName;
+	/** */
+	private String company;
+	/** */
+	private String company1;
+	/** */
+	private String company2;
+	/** */
+	private String title;
+	/** */
+	private String title1;
+	/** */
+	private String title2;
+	/** */
+	private String telephoneNumber;
+	/** */
+	private String homePhone;
+	/** */
+	private String mobile;
+	/** */
+	private String facsimileTelephoneNumber;
+	/** */
+	private String streetAddress;
+	/** */
+	private String info;
+	/** 상위부서ID*/
+	private String extensionAttribute1;
+	/** 회사ID*/
+	private String extensionAttribute2;
+	/** 회사명*/
+	private String extensionAttribute3;
+	/** 문서과ID*/
+	private String extensionAttribute4;
+	/** 발신명의*/
+	private String extensionAttribute5;
+	/** 부서약어*/
+	private String extensionAttribute6;
+	/** 관인경로*/
+	private String extensionAttribute7;
+	/** 기관여부*/
+	private String extensionAttribute8;
+	/** 부서장ID*/
+	private String extensionAttribute9;
+	/** DocSymBol*/
+	private String extensionAttribute10;
+	/** */
+	private String extensionAttribute11;
+	/** */
+	private String extensionAttribute12;
+	/** */
+	private String extensionAttribute13;
+	/** */
+	private String extensionAttribute14;
+	/** 정렬순서값*/
+	private String extensionAttribute15;
+	/** LDAP 경로*/
+	private String adsPath;
+	/** 최종업데이트일자*/
+	private String updateDT;
+	/** */
+	private String type;
+	/** */
+	private String positionCD;
+	/** */
+	private String birth;
+	/** */
+	private String birthType;
+}
