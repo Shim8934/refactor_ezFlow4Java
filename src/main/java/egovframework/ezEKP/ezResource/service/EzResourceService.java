dd606994ffdf52ec29e74ca9a6e4da3a44c1ff6a
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-19 06:53:28 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@2118 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezResource/service/EzResourceService.java
+++ src/main/java/egovframework/ezEKP/ezResource/service/EzResourceService.java
@@ -1,5 +1,11 @@
 package egovframework.ezEKP.ezResource.service;
 
-public interface EzResourceService {
+import java.util.List;
 
+import egovframework.ezEKP.ezResource.vo.ResGetAdmSubClsTreeVO;
+
+public interface EzResourceService {
+	public List<ResGetAdmSubClsTreeVO> getAdmSubClsTree(String parentID, String companyID, String treeType) throws Exception;
+	
+	public List<ResGetAdmSubClsTreeVO> getSubClsTree(String parentID, String companyID, String treeType, String pUserID, String comID, String deptID, String userID) throws Exception;
 }
