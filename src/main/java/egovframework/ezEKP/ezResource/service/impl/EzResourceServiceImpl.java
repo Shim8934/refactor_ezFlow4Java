85275bb5790cf921bf52fd73df33f821a96e31ee
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-19 06:53:33 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@2119 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezResource/service/impl/EzResourceServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezResource/service/impl/EzResourceServiceImpl.java
@@ -1,10 +1,41 @@
 package egovframework.ezEKP.ezResource.service.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import egovframework.ezEKP.ezResource.dao.EzResourceDAO;
 import egovframework.ezEKP.ezResource.service.EzResourceService;
+import egovframework.ezEKP.ezResource.vo.ResGetAdmSubClsTreeVO;
 
 @Service("EzResourceService")
 public class EzResourceServiceImpl implements EzResourceService{
+	@Resource(name="EzResourceDAO")
+	private EzResourceDAO ezResourceDAO;
+	
+	@Override
+	public List<ResGetAdmSubClsTreeVO> getAdmSubClsTree(String parentID,String companyID, String treeType) throws Exception {
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("v_P_ParentID", parentID);
+		map.put("v_P_CompanyID", companyID);
+		map.put("v_P_TreeType", treeType);
+		return ezResourceDAO.getAdmSubClsTree(map);
+	}
 
+	@Override
+	public List<ResGetAdmSubClsTreeVO> getSubClsTree(String parentID, String companyID, String treeType, String pUserID, String comID, String deptID, String userID) throws Exception {
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("v_P_ParentID", parentID);
+		map.put("v_P_CompanyID", companyID);
+		map.put("v_P_TreeType", treeType);
+		map.put("v_P_USERID", pUserID);
+		map.put("v_PCOMID", comID);
+		map.put("v_PDEPTID", deptID);
+		map.put("v_PUSERID", userID);
+		return ezResourceDAO.getSubClsTree(map);
+	}
 }
