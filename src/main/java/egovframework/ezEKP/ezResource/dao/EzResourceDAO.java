436adb9ece8329d277f3558997a6b0c2f7cbc261
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-19 06:53:39 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@2120 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezResource/dao/EzResourceDAO.java
+++ src/main/java/egovframework/ezEKP/ezResource/dao/EzResourceDAO.java
@@ -1,9 +1,24 @@
 package egovframework.ezEKP.ezResource.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
-@Repository("EzResourceDAO")
-public class EzResourceDAO {
+import egovframework.ezEKP.ezResource.vo.ResGetAdmSubClsTreeVO;
+import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
 
+@Repository("EzResourceDAO")
+public class EzResourceDAO extends EgovAbstractDAO {
+	
+	@SuppressWarnings("unchecked")
+	public List<ResGetAdmSubClsTreeVO> getAdmSubClsTree(Map<String, Object> map){
+		return  (List<ResGetAdmSubClsTreeVO>) list("EzResourceDAO.getAdmSubClsTree", map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ResGetAdmSubClsTreeVO> getSubClsTree(Map<String, Object> map){
+		return  (List<ResGetAdmSubClsTreeVO>) list("EzResourceDAO.getSubClsTree", map);
+	}
 }
 
