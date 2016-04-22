0aa51fd101803b19303d590caef79943729709b0
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-22 08:42:36 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@2209 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezResource/dao/EzResourceDAO.java
+++ src/main/java/egovframework/ezEKP/ezResource/dao/EzResourceDAO.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import egovframework.ezEKP.ezResource.vo.ResGetAdmSubClsTreeVO;
+import egovframework.ezEKP.ezResource.vo.ResGetAdminFlagVO;
+import egovframework.ezEKP.ezResource.vo.ResGetItemListVO;
 import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
 
 @Repository("EzResourceDAO")
@@ -19,6 +21,15 @@ public class EzResourceDAO extends EgovAbstractDAO {
 	@SuppressWarnings("unchecked")
 	public List<ResGetAdmSubClsTreeVO> getSubClsTree(Map<String, Object> map){
 		return  (List<ResGetAdmSubClsTreeVO>) list("EzResourceDAO.getSubClsTree", map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ResGetItemListVO> getBrdMainList(Map<String, Object> map){
+		return  (List<ResGetItemListVO>) list("EzResourceDAO.getBrdMainList", map);
+	}
+	
+	public ResGetAdminFlagVO getAdminFlag(Map<String, Object> map) {
+		return (ResGetAdminFlagVO) select("EzResourceDAO.getAdminFlag", map);
 	}
 }
 
