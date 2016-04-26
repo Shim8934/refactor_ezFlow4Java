702d3b5a79325c2d4de9779e251cd504ae6bf9bc
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-26 09:19:26 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@2274 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezQuestion/vo/QstDeleteAttachUrlVO.java
@@ -0,0 +1,20 @@
+package egovframework.ezEKP.ezQuestion.vo;
+
+public class QstDeleteAttachUrlVO {
+	/** 첨부URL*/
+	private String attachUrl;
+	/** 첨부유형*/
+	private String attachType;
+	public String getAttachUrl() {
+		return attachUrl;
+	}
+	public void setAttachUrl(String attachUrl) {
+		this.attachUrl = attachUrl;
+	}
+	public String getAttachType() {
+		return attachType;
+	}
+	public void setAttachType(String attachType) {
+		this.attachType = attachType;
+	}
+}
