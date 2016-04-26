8e6b7e2b709fe4f3c4c838e8d83c693f791fbc7d
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-26 05:34:31 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@2256 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezResource/service/EzResourceService.java
+++ src/main/java/egovframework/ezEKP/ezResource/service/EzResourceService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import egovframework.ezEKP.ezResource.vo.ResGetAdmSubClsTreeVO;
 import egovframework.ezEKP.ezResource.vo.ResGetAdminFlagVO;
 import egovframework.ezEKP.ezResource.vo.ResGetItemListVO;
+import egovframework.ezEKP.ezResource.vo.ResGetScheduleListMainVO;
+import egovframework.ezEKP.ezResource.vo.ResGetScheduleListVO;
 
 public interface EzResourceService {
 	public List<ResGetAdmSubClsTreeVO> getAdmSubClsTree(String parentID, String companyID, String treeType) throws Exception;
@@ -12,6 +14,14 @@ public interface EzResourceService {
 	public List<ResGetAdmSubClsTreeVO> getSubClsTree(String parentID, String companyID, String treeType, String pUserID, String comID, String deptID, String userID) throws Exception;
 	
 	public List<ResGetItemListVO> getBrdMainList(String brdID, String companyID, String lang) throws Exception;
+	
+	public List<ResGetScheduleListVO> getScheduleList(String ownerID, String companyID, String startDate, String endDate, String writerName, String writerDept) throws Exception;
+	
+	public List<ResGetScheduleListMainVO> getScheduleListMain(String ownerID, String companyID, String startDate, String endDate) throws Exception;
+	
+	public List<ResGetScheduleListVO> getScheduleListRepetiti(String ownerID, String companyID, String startDate, String endDate, String writerName, String writerDept) throws Exception;
+	
+	public List<ResGetScheduleListMainVO> getScheduleListRepetitim(String ownerID, String companyID, String startDate) throws Exception;
 	
 	public ResGetAdminFlagVO getAdminFlag(String companyID, String resID, String memberID) throws Exception; 
 	
