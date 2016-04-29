495ed4d44262ae4d5127c8703ae1af1ef8f8e7ce
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-04-29 02:09:59 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@2306 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezResource/vo/ResBrdListVO.java
@@ -0,0 +1,197 @@
+package egovframework.ezEKP.ezResource.vo;
+
+public class ResBrdListVO {
+	/** 첨부 제한 크기*/
+	private int attachSize;
+	/** 게시판 접근 거부 메시지*/
+	private String brdAccess;
+	/** 회사ID*/
+	private String brdCompany;
+	/** 게시판 수*/
+	private int brdCount;
+	/** 게시판 설명*/
+	private String brdExplain;
+	/** 게시구분*/
+	private String brdGb;
+	/** 게시그룹*/
+	private String brdGroup;
+	/** 게시판ID*/
+	private String brdID;
+	/** 관련 게시판의 계층 레벨*/
+	private int brdLevel;
+	/** 게시판명*/
+	private String brdNm;
+	/** 게시 만료 기간*/
+	private int brdPostterm;
+	/** 최상위게시판ID*/
+	private int brdRef;
+	/** 관련 게시판 내 순서*/
+	private int brdStep;
+	/** 상위 게시판 ID*/
+	private int brdUpper;
+	/** 게시판 연결  URL*/
+	private String brdUrl;
+	/** 생성일*/
+	private String makeDate;
+	/** 담당 부서ID*/
+	private String ownDeptID;
+	/** 담당 부서명*/
+	private String ownDeptNm;
+	/** 담당자 연락처*/
+	private String ownerCall;
+	/** 담당자 ID*/
+	private String ownerID;
+	/** 담당자명*/
+	private String ownerNm;
+	/** 담당자 직위*/
+	private String ownerPosition;
+	/** 게시물 답변 시 메일 발송 허용 여부*/
+	private int replyMailFg;
+	/** 자원위치*/
+	private String resLocation;
+	
+	public int getAttachSize() {
+		return attachSize;
+	}
+	public void setAttachSize(int attachSize) {
+		this.attachSize = attachSize;
+	}
+	public String getBrdAccess() {
+		return brdAccess;
+	}
+	public void setBrdAccess(String brdAccess) {
+		this.brdAccess = brdAccess;
+	}
+	public String getBrdCompany() {
+		return brdCompany;
+	}
+	public void setBrdCompany(String brdCompany) {
+		this.brdCompany = brdCompany;
+	}
+	public int getBrdCount() {
+		return brdCount;
+	}
+	public void setBrdCount(int brdCount) {
+		this.brdCount = brdCount;
+	}
+	public String getBrdExplain() {
+		return brdExplain;
+	}
+	public void setBrdExplain(String brdExplain) {
+		this.brdExplain = brdExplain;
+	}
+	public String getBrdGb() {
+		return brdGb;
+	}
+	public void setBrdGb(String brdGb) {
+		this.brdGb = brdGb;
+	}
+	public String getBrdGroup() {
+		return brdGroup;
+	}
+	public void setBrdGroup(String brdGroup) {
+		this.brdGroup = brdGroup;
+	}
+	public String getBrdID() {
+		return brdID;
+	}
+	public void setBrdID(String brdID) {
+		this.brdID = brdID;
+	}
+	public int getBrdLevel() {
+		return brdLevel;
+	}
+	public void setBrdLevel(int brdLevel) {
+		this.brdLevel = brdLevel;
+	}
+	public String getBrdNm() {
+		return brdNm;
+	}
+	public void setBrdNm(String brdNm) {
+		this.brdNm = brdNm;
+	}
+	public int getBrdPostterm() {
+		return brdPostterm;
+	}
+	public void setBrdPostterm(int brdPostterm) {
+		this.brdPostterm = brdPostterm;
+	}
+	public int getBrdRef() {
+		return brdRef;
+	}
+	public void setBrdRef(int brdRef) {
+		this.brdRef = brdRef;
+	}
+	public int getBrdStep() {
+		return brdStep;
+	}
+	public void setBrdStep(int brdStep) {
+		this.brdStep = brdStep;
+	}
+	public int getBrdUpper() {
+		return brdUpper;
+	}
+	public void setBrdUpper(int brdUpper) {
+		this.brdUpper = brdUpper;
+	}
+	public String getBrdUrl() {
+		return brdUrl;
+	}
+	public void setBrdUrl(String brdUrl) {
+		this.brdUrl = brdUrl;
+	}
+	public String getMakeDate() {
+		return makeDate;
+	}
+	public void setMakeDate(String makeDate) {
+		this.makeDate = makeDate;
+	}
+	public String getOwnDeptID() {
+		return ownDeptID;
+	}
+	public void setOwnDeptID(String ownDeptID) {
+		this.ownDeptID = ownDeptID;
+	}
+	public String getOwnDeptNm() {
+		return ownDeptNm;
+	}
+	public void setOwnDeptNm(String ownDeptNm) {
+		this.ownDeptNm = ownDeptNm;
+	}
+	public String getOwnerCall() {
+		return ownerCall;
+	}
+	public void setOwnerCall(String ownerCall) {
+		this.ownerCall = ownerCall;
+	}
+	public String getOwnerID() {
+		return ownerID;
+	}
+	public void setOwnerID(String ownerID) {
+		this.ownerID = ownerID;
+	}
+	public String getOwnerNm() {
+		return ownerNm;
+	}
+	public void setOwnerNm(String ownerNm) {
+		this.ownerNm = ownerNm;
+	}
+	public String getOwnerPosition() {
+		return ownerPosition;
+	}
+	public void setOwnerPosition(String ownerPosition) {
+		this.ownerPosition = ownerPosition;
+	}
+	public int getReplyMailFg() {
+		return replyMailFg;
+	}
+	public void setReplyMailFg(int replyMailFg) {
+		this.replyMailFg = replyMailFg;
+	}
+	public String getResLocation() {
+		return resLocation;
+	}
+	public void setResLocation(String resLocation) {
+		this.resLocation = resLocation;
+	}
+}
