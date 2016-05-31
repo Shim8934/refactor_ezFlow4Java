dbe1a32304f61ae0f242cba791afc9e5b0597c3b
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-05-31 00:23:37 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@2588 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezResource/web/EzResourceController.java
+++ src/main/java/egovframework/ezEKP/ezResource/web/EzResourceController.java
@@ -32,6 +32,8 @@ import egovframework.ezEKP.ezResource.service.EzResourceService;
 import egovframework.ezEKP.ezResource.vo.ResBrdListVO;
 import egovframework.ezEKP.ezResource.vo.ResBrdVO;
 import egovframework.ezEKP.ezResource.vo.ResGetItemListVO;
+import egovframework.ezEKP.ezResource.vo.ResGetRepDateTimesVO;
+import egovframework.ezEKP.ezResource.vo.ResGetScheduleVO;
 import egovframework.ezEKP.ezResource.vo.ResSelectFormIDVO;
 import egovframework.let.user.login.service.LoginService;
 import egovframework.let.user.login.vo.LoginVO;
@@ -291,7 +293,6 @@ public class EzResourceController extends EgovFileMngUtil {
 		
 			if (cmd.equals("get")) {
 				startDate = xmlDom.getElementsByTagName("STARTDATETIME").item(0).getTextContent();
-System.out.println("startDate:"+startDate);
 				endDate = xmlDom.getElementsByTagName("ENDDATETIME").item(0).getTextContent();
 
 				if (viewType.equals("list")) {
@@ -928,7 +929,7 @@ System.out.println("startDate:"+startDate);
 		Date date = new Date();
 		@SuppressWarnings("deprecation")
 		int curYear = date.getYear()-100;
-System.out.println("curYear:"+curYear);
+
 		for (int i=curYear; i>=curYear-6; i--) {
 			if((curYear-3) == i) {
 				iYear.append("<Option Value=\"" + String.valueOf(i) + "\" selected>" + String.valueOf(i) + "</Option>" );
@@ -940,7 +941,7 @@ System.out.println("curYear:"+curYear);
 		
 		@SuppressWarnings("deprecation")
 		int curMonth = date.getMonth()+1;
-System.out.println("curMonth:"+curMonth);
+
 		for (int j=1; j<= 12; j++) {
 			if (curMonth == j) {
 				iMonth.append("<Option Value=\"" + String.valueOf(j) + "\" selected>" + String.valueOf(j) + "</Option>" );
@@ -980,8 +981,275 @@ System.out.println("curMonth:"+curMonth);
 	 * 자원관리 자원 예약 화면 호출 함수
 	 */
 	@RequestMapping(value = "/ezResource/scheduleAdd.do")
-	public String scheduleAdd(Model model) throws Exception {
-		model.addAttribute("userLang", "1");
+	public String scheduleAdd(@CookieValue("loginCookie") String loginCookie,LoginVO userInfo, HttpServletRequest req, Model model) throws Exception {
+		userInfo = commonUtil.userInfo(loginCookie);
+		String useIE11Browser = "";
+		String editor = config.getProperty("EDITOR");
+		String nonActiveX = "YES";
+		String resID = "";
+		String brdName = "";
+		String cmdStr = "";
+		String fromStr = "";
+		String dayView = "";
+		String orgNum = "";
+		String orgOwnerID = "";
+		String typeVal = "";
+		String startDateVal = "";
+		String endDateVal = "";
+		String deptNm = "";
+		String ownerNm = "";
+		String title = "";
+		String loc = "";
+		String importance = "";
+		String gresFlag = "0";
+		String reFlag = "";
+		String startDateTime = "";
+		String endDateTime = "";
+		String startDateTime2 = "";
+		String endDateTime2 = "";
+		String alertTime = "";
+		String timeDisplay = "";
+		String content = "";
+		String ownerID = "";
+		String writerID = "";
+		String curStartDateTime = "";
+		String curEndDateTime = "";
+		String curStartDateTime2 = "";
+		String curEndDateTime2 = "";
+		String checkSDT = "";
+		String checkEDT = "";
+		String allDay = "";
+		int pNum = 0;
+		
+		
+		if (config.getProperty("config.IE11EDITOR").equals("CK")) {
+			useIE11Browser = "CK";
+		}
+		if (req.getParameter("ownerID") != null) {
+			resID = req.getParameter("ownerID");
+		}
+		if (req.getParameter("brdName") != null) {
+			brdName = req.getParameter("brdName");
+		}
+
+		String adminFg = ezResourceService.getACL(userInfo.getCompanyID(), resID, userInfo.getId(), "");
+		String brdApproveFlag = ezResourceService.getBrdApproveFlag(Integer.parseInt(resID), userInfo.getCompanyID());
+		
+		if (req.getParameter("cmd") != null) {
+			cmdStr = req.getParameter("cmd");
+		}
+		if (req.getParameter("from") != null) {
+			fromStr = req.getParameter("from");
+		}
+		if (req.getParameter("dayView") != null) {
+			dayView = req.getParameter("dayView");
+		}
+		if (cmdStr.equals("mod")) {
+			if (req.getParameter("num") != null) {
+				orgNum = req.getParameter("num").trim();
+			}
+			if (req.getParameter("ownerID") != null) {
+				orgOwnerID = req.getParameter("ownerID").trim();
+			}
+			if (req.getParameter("type") != null) {
+				typeVal = req.getParameter("type").trim();
+			}
+			if (req.getParameter("startDate") != null) {
+				startDateVal = req.getParameter("startDate").trim();
+			}
+			if (req.getParameter("endDate") != null) {
+				endDateVal = req.getParameter("endDate").trim();
+			}
+			ResGetScheduleVO getSchedule = new ResGetScheduleVO();
+			if (typeVal.equals("Master") || typeVal.equals("Readonly")) {
+				getSchedule = ezResourceService.getSchedule(Integer.parseInt(orgNum), orgOwnerID, userInfo.getCompanyID());
+			}
+			Document scheduleXML = commonUtil.convertStringToDocument(commonUtil.getQueryResult(getSchedule));  
+			int num = getSchedule.getNum();
+			pNum = getSchedule.getpNum();
+			ownerID = getSchedule.getOwnerID();
+			writerID = getSchedule.getWriterID();
+			
+			String propList = "homemdb;extensionAttribute1;department;displayName;mail;mailNickname;title;description;company;physicalDeliveryOfficeName;TelephoneNumber;extensionAttribute10";
+			String infoXML = ezOrganService.getPropertyList(writerID, propList, userInfo.getLang());
+			
+			Document xmlDom2 = commonUtil.convertStringToDocument(infoXML);
+			
+			if (userInfo.getLang().equals("1")) {
+				deptNm = xmlDom2.getElementsByTagName("DESCRIPTION1").item(0).getTextContent();
+				ownerNm = xmlDom2.getElementsByTagName("DISPLAYNAME1").item(0).getTextContent();
+			} else {
+				deptNm = xmlDom2.getElementsByTagName("DESCRIPTION" + userInfo.getLang()).item(0).getTextContent();
+				ownerNm = xmlDom2.getElementsByTagName("DISPLAYNAME" + userInfo.getLang()).item(0).getTextContent();
+			}
+			title = getSchedule.getTitle();
+			
+			if (title != null) {
+				 title = title.replace("'", "&#39;");
+                 title = title.replace("\"", "&quot;");
+			}
+			loc = getSchedule.getLocation();
+System.out.println("loc:"+loc);
+			if (loc != null) {
+				loc = title.replace("'", "&#39;");
+                loc = title.replace("\"", "&quot;");
+			}
+			timeDisplay = getSchedule.getTimeDisplay();
+			startDateTime = getSchedule.getStartDate();
+			endDateTime = getSchedule.getEndDate();
+			alertTime = getSchedule.getAlertTime();
+			reFlag = getSchedule.getReFlag();
+			gresFlag = getSchedule.getGresFlag();
+System.out.println("gresFlag:"+gresFlag);
+			content = getSchedule.getContent();
+			importance = getSchedule.getImportance();
+			
+			if (importance.equals("")) {
+				importance = "2";
+			}
+			String entryList = getSchedule.getEntryList();
+			allDay = getSchedule.getAllDay();
+			String publicFlag = getSchedule.getPublicFlag();
+			int characterID2 = getSchedule.getCharacterID();
+			String saveApproveFlag = getSchedule.getApproveFlag();
+			
+			ResGetRepDateTimesVO repDateTimes = ezResourceService.getRepDateTimes(orgOwnerID, userInfo.getCompanyID(), Integer.parseInt(orgNum));
+			String startDateTimeRepeat = "";
+			String endDateTimeRepeat = "";
+			if (repDateTimes != null) {
+				startDateTimeRepeat = ezResourceService.getLocalTime(repDateTimes.getStartDateTime());
+				endDateTimeRepeat = ezResourceService.getLocalTime(repDateTimes.getEndDateTime());
+			}
+		} else {
+			importance = "2";
+			String selSd = "";
+			String selEd = "";
+			String cDate = "";
+			String cTime = "";
+			String sTime = "";
+			String eTime = "";
+			
+			if (req.getParameter("selsd") != null) {
+				selSd = req.getParameter("selsd");
+			}
+System.out.println("selSD:"+selSd);
+			if (req.getParameter("seled") != null) {
+				selEd = req.getParameter("seled");
+			}
+			if (selSd.equals("") || selEd.equals("")) {
+				cDate = ezResourceService.getLocalTime(EgovDateUtil.getToday("time"));
+				cTime = cDate.split(" ")[1].substring(0, 2);
+				
+				if (req.getParameter("startDate") != null) {
+					cDate = req.getParameter("startDate");
+				}
+				cDate = cDate.substring(0, 10);
+				startDateTime = cDate + " " + cTime + ":00:00";
+				
+				if (req.getParameter("endDate") != null) {
+					cDate = req.getParameter("endDate");
+				}
+				cDate = cDate.substring(0, 10);
+				endDateTime = cDate + " " + cTime + ":30:00";
+System.out.println("cDate:"+cDate);
+System.out.println("startDateTime:"+startDateTime);
+System.out.println("endDateTime:"+endDateTime);
+			} else {
+				if (selSd.length() == 10) {
+					cDate = ezResourceService.getLocalTime(EgovDateUtil.getToday("time"));
+					cTime = cDate.split(" ")[1].substring(0, 2);
+					cDate = cDate.substring(0, 10);
+					startDateTime = selSd + " " + cTime + ":00:00";
+					endDateTime = selEd + " " + cTime + ":30:00";
+
+				} else {
+					startDateTime = selSd;
+					endDateTime = selEd;
+				}
+			}
+			if (req.getParameter("ownerID") != null) {
+				ownerID = req.getParameter("ownerID");
+			}
+		}
+		String curDate = ezResourceService.getLocalTime(EgovDateUtil.getToday("time"));
+		String curTime = curDate.split(" ")[1].substring(0, 2);
+			
+		if (req.getParameter("startDate") != null) {
+			curDate = req.getParameter("startDate");
+		}
+		if (curDate.length() == 9) {
+			curDate = curDate.substring(0, 9);
+		} else {
+			curDate = curDate.split(" ")[0];
+		}
+		curStartDateTime = curDate + " " + curTime + ":00:00";
+			
+		if (req.getParameter("endDate") != null) {
+			curDate = req.getParameter("endDate");
+		}
+		if (curDate.length() == 9) {
+			curDate = curDate.substring(0, 9);
+		} else {
+			curDate = curDate.split(" ")[0];
+		}
+		curEndDateTime = curDate + " " + curTime + ":30:00";
+		
+		curStartDateTime = EgovDateUtil.convertDate(curStartDateTime, "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm:ss", "");
+		curEndDateTime = EgovDateUtil.convertDate(curEndDateTime, "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm:ss", "");
+		
+		curStartDateTime2 = EgovDateUtil.convertDate(curStartDateTime, "yyyy-MM-dd HH:mm:ss", "yyyy-M-d H:mm", "");
+		curEndDateTime2 = EgovDateUtil.convertDate(curEndDateTime, "yyyy-MM-dd HH:mm:ss", "yyyy-M-d H:mm", "");
+		
+		startDateTime = ezResourceService.convertDate(startDateTime, "", "", "");
+		endDateTime = ezResourceService.convertDate(endDateTime, "", "", "");
+	System.out.println(startDateTime);
+	System.out.println(endDateTime);
+		startDateTime2 = ezResourceService.isoUTFDate(startDateTime);
+		endDateTime2 = ezResourceService.isoUTFDate(endDateTime);
+System.out.println("startDateTime2:"+startDateTime2);		
+		checkSDT = EgovDateUtil.convertDate(startDateTime, "yyyy-MM-dd aa h:mm:ss", "yyyy-M-d H:mm", "");
+		checkEDT = EgovDateUtil.convertDate(endDateTime, "yyyy-MM-dd aa h:mm:ss", "yyyy-M-d H:mm", "");
+		
+		model.addAttribute("userInfo", userInfo);
+		model.addAttribute("useIE11Browser", useIE11Browser);
+		model.addAttribute("editor", editor);
+		model.addAttribute("nonActiveX", nonActiveX);
+		model.addAttribute("adminFg", adminFg);
+		model.addAttribute("brdName", brdName);
+		model.addAttribute("resID", resID);
+		model.addAttribute("approveFlag", brdApproveFlag);
+		model.addAttribute("cmdStr", cmdStr.toLowerCase());
+		model.addAttribute("fromStr", fromStr);
+		model.addAttribute("dayView", dayView);
+		model.addAttribute("pNum", pNum);
+		model.addAttribute("gresFlag", gresFlag);
+		model.addAttribute("reFlag", reFlag);
+		model.addAttribute("content", content);
+		model.addAttribute("importance", importance);
+		model.addAttribute("loc", loc);
+		model.addAttribute("title", title);
+		model.addAttribute("allDay", allDay);
+		model.addAttribute("startDateTime", startDateTime);
+		model.addAttribute("endDateTime", endDateTime);
+		model.addAttribute("startDateTime2", startDateTime2);
+		model.addAttribute("endDateTime2", endDateTime2);
+		
+		if (reFlag.equals("1")) {
+			model.addAttribute("strTmpReFlagVal", "2");
+			model.addAttribute("strDspMod1", "style='display:none'");
+			model.addAttribute("strDspMod2", "");
+		} else {
+			model.addAttribute("strTmpReFlagVal", "0");
+			model.addAttribute("strDspMod1", "");
+			model.addAttribute("strDspMod2", "style='display:none'");
+		}
+		
+		if (reFlag.equals("")) {
+			model.addAttribute("strIReFlagVal", "0");
+		} else {
+			model.addAttribute("strIReFlagVal", reFlag);
+		}
+		
 		return "/ezResource/resScheduleAdd";
 	}
 	
@@ -1101,8 +1369,6 @@ System.out.println("curMonth:"+curMonth);
 			String resID = xmlDom.getDocumentElement().getChildNodes().item(0).getTextContent();
 
 			ResSelectFormIDVO selectFormID = ezResourceService.selectFormID(resID);
-			String temp = commonUtil.getQueryResult(selectFormID);
-			Document docXML = commonUtil.convertStringToDocument(temp);
 
 			if (selectFormID == null) {
 				return "FALSE";
