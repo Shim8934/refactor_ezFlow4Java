346a8ba2cebcf6483486981c25a5c339479f83b9
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-06-10 01:38:46 +0000

    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@2679 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezResource/dao/EzResourceAdminDAO.java
@@ -0,0 +1,9 @@
+package egovframework.ezEKP.ezResource.dao;
+
+import org.springframework.stereotype.Repository;
+import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
+
+@Repository("EzResourceAdminDAO")
+public class EzResourceAdminDAO extends EgovAbstractDAO {
+
+}
