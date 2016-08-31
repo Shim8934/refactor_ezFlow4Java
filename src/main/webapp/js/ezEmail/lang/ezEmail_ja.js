67f7b489025aa498b73d7dee8b869e6ce2a517c1
skyblue0o0 <skyblue0o0@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-08-31 02:00:03 +0000

    * 일본어 관련 파일 추가
    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@3199 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/webapp/js/ezEmail/lang/ezEmail_ja.js
@@ -0,0 +1,441 @@
+﻿var strLang1 = "この表示で表示する項目がありません。";
+var strLang2 = "フォルダを表示することができません。サーバー内部のエラーが発生しました。";
+var strLang3 = "表示種類を持っている変数です。";
+var strLang4 = "ゴミ箱";
+var strLang5 = "移動に失敗しました。";
+var strLang6 = "削除に失敗しました。";
+var strLang7 = "作業に失敗しました。";
+var strLang8 = "作業に失敗しました。";
+var strLang9 = "受信日付";
+var strLang10 = "削除日付";
+var strLang11 = "宛先";
+var strLang12 = "サイズ";
+var strLang13 = "タイトル別に";
+var strLang14 = "午前";
+var strLang15 = "午後";
+var strLang16 = "日";
+var strLang17 = "月";
+var strLang18 = "火";
+var strLang19 = "水";
+var strLang20 = "木";
+var strLang21 = "金";
+var strLang22 = "土";
+var strLang23 = "年";
+var strLang24 = "年 ";
+var strLang25 = "月 ";
+var strLang26 = "日";
+var strLang27 = "曜日";
+var strLang28 = "切り取り";
+var strLang29 = "コピー";
+var strLang30 = "張り付け";
+var strLang31 = "削除";
+var strLang32 = "全体選択";
+var strLang33 = "Gullim;word-break:break-all";
+var strLang34 = "Gullim";
+var strLang35 = "メッセージ";
+var strLang36 = "未読メッセージ";
+var strLang37 = "差出人別に";
+var strLang38 = "対話主題別に";
+var strLang39 = "宛先別に";
+var strLang40 = "未読項目を対話主題別に";
+var strLang41 = "2行表示";
+var strLang42 = "保存するメールを選択してください。";
+var strLang43 = "返信するメールを選択してください。";
+var strLang44 = "返信するメールを一つだけ選択してください。";
+var strLang45 = "全体返信するメールを選択してください。";
+var strLang46 = "全体返信するメールを一つだけ選択してください。";
+var strLang47 = "転送するメールを選択してください。";
+var strLang48 = "伝送するメールを一つだけ選択してください。";
+var strLang49 = "メールを選択してください。";
+var strLang50 = "受信確認リストを見るメールを一つだけ選択してください。";
+var strLang51 = "移動またはコピーするメールを選択してください。";
+var strLang52 = "メールのコピー中、エラーが発生しました。";
+var strLang53 = "コピーしました。";
+var strLang54 = "ゴミ箱のメールが全部、永久削除されます。\n\n続きますか?";
+var strLang55 = "フォルダを空きにする作業中にエラーが発生しました。";
+var strLang56 = "フォルダを空きにする作業をしている時、エラーが発生しました。";
+var strLang57 = "削除するメールを選択してください。";
+var strLang58 = "このメールを永久削除しますか?";
+var strLang59 = "このメールを削除しますか?";
+var strLang60 = "受信拒否に指定するメールを選択してください。";
+var strLang61 = "迷惑メール処理されました。";
+var strLang62 = "エラーが発生しました。";
+var strLang63 = "受信トレイ"
+var strLang64 = "보낸 편지함"
+var strLang65 = "임시 보관함"
+var strLang66 = "지운 편지함"
+var strLang67 = "개인 편지함"
+var strLang68 = "受信箱";
+var strLang69 = "送信済みメール";
+var strLang70 = "下書き";
+var strLang71 = "個人フォルダ";
+var strLang72 = "正しくない URLを Encodeするのに失敗しました。";
+var strLang73 = "正しくない URLを Decodeするのに失敗しました。";
+var strLang74 = "全体添付ファイルサイズ";
+var strLang75 = "一般添付ファイル総大きさが ";
+var strLang76 = "を超えることができません。";
+var strLang77 = "一つの添付ファイルサイズが ";
+var strLang78 = "を超える場合には大容量メール送信として処理されます。\n\n該当するメールの添付ファイルは ";
+var strLang79 = "以後自動削除されます。";
+var strLang80 = "のアップロード中です。 ";
+var strLang81 = "全体添付ファイルの中で一般添付含みの有無 (N:一般添付含み)";
+var strLang82 = "該当する添付ファイルの大容量ファイルの有無 (Y:大容量添付)";
+var strLang83 = "大容量添付を除いた一般添付リスト";
+var strLang84 = "のアップロード中、エラーが発生しました。\n\nサーバー側エラー";
+var strLang85 = "のアップロード中、エラーが発生しました。";
+var strLang86 = "添付ファイルの追加中です。";
+var strLang87 = "以前追加されたファイルリストを初期化する。";
+var strLang88 = "アップロードするローカルファイルを追加する。";
+var strLang89 = "ファイルアップロードに失敗しました。";
+var strLang90 = "削除する添付ファイルを選択してください。";
+var strLang91 = "添付ファイルの削除中です。";
+var strLang92 = "件名を入力してください。";
+var strLang93 = "腕先を指定してください。";
+var strLang94 = "保存または送信が進行中です。";
+var strLang95 = "タイトルは 120字以内でなければなりません。";
+var strLang96 = "メール全体サイズまたは受信引受がサーバーで制限した限度を超過して\n管理者の承認を得ないと実際に送信が成り立ちません。\n送信しますか?";
+var strLang97 = "タイトルなし";
+var strLang98 = "このメールを";
+var strLang99 = "に送信されます。\n\n発送をキャンセルするには、管理者が送信を承認した後に";
+var strLang100 = "「予約送信管理」";
+var strLang101 = "メニューで送信をキャンセルできます。";
+var strLang102 = "に送信します。\n\n";
+var strLang103 = "メールの送信中にエラーが発生しました。";
+var strLang104 = "メールの保存中にエラーが発生しました。";
+var strLang105 = "ウェプサーバーとの連結エラー";
+var strLang106 = "メールの送信中にエラーが発生しました。 - ";
+var strLang107 = "メールの保存中にエラーが発生しました。 - ";
+var strLang108 = "メールを下書きに保存しました。";
+var strLang109 = "組職図アドレス録のインポート中に、エラーが発生しました。 - ";
+var strLang110 = "部署";
+var strLang111 = "アドレス録のインポート中にエラーが発生しました。 - ";
+var strLang112 = "アドレス録";
+var strLang113 = "共用グループのインポート中にエラーが発生しました。 - ";
+var strLang114 = "共用グループ";
+var strLang115 = "ただ渡すディスプレー名を使って情報を得ます。";
+var strLang116 = "決栽本文";
+var strLang117 = "決栽文書 : ";
+var strLang118 = "掲示日付 : ";
+var strLang119 = "掲示者 : ";
+var strLang120 = "タイトル : ";
+var strLang121 = "掲示送信 : ";
+var strLang122 = "大容量添付ファイル (該当する添付ファイルは ";
+var strLang123 = "以後自動削除されます。)";
+var strLang124 = "件名なし-- メールの作成";
+var strLang125 = "メールの作成";
+var strLang126 = "グループメール";
+var strLang127 = "にあるソースの Index;";
+var strLang128 = "結果物で渡される値の数字をカウント";
+var strLang129 = "全角文字で作ります。";
+var strLang130 = "空白をとり除きます。";
+var strLang131 = "メールの削除中、エラーが発生しました。";
+var strLang132 = "メールの移動中、エラーが発生しました。";
+var strLang133 = "アドレスの追加中、エラー発生 - ";
+var strLang134 = "すでに同じ名前と電子メールアドレスが保存されています。";
+var strLang135 = "アドレスの追加中、エラー発生";
+var strLang136 = "アドレスを追加しました。";
+var strLang137 = "電子メールアドレスまたは拒否するキーワードを捜すことができなくて迷惑メールに登録することができません。";
+var strLang138 = "エラーが発生しました。";
+var strLang139 = "タブが指定されました。";
+var strLang140 = "タブが解除されました。";
+var strLang141 = "添付ファイルを選択してください。";
+var strLang142 = "宛先の多いメールの回収の時、システムに負荷を発生させる恐れがあります。\n\nまた、既読のメールと外部システムの場合、回収できません。\n\n回収しますか?";
+var strLang143 = "宛先の多いメールの回収の時、システムに負荷を発生させる恐れがあります。\n\nまた、既読のメールと外部システムの場合、回収できません。\n\n回収しますか?";
+var strLang144 = "メールの回収中、エラー発生。";
+var strLang145 = "メール回収の進行中です。\n\nメールを全部回収するのにしばらく時間がかかることがあります。";
+var strLang146 = "回収が完了しました。";
+var strLang147 = "検索中ですから、しばらくお待ちください。";
+var strLang148 = "は検索語で使うことができません。";
+var strLang149 = "すべてのメールフォルダ";
+var strLang150 = "検索中にエラーが発生しました。";
+var strLang151 = "件名";
+var strLang152 = "内容";
+var strLang153 = "差出人";
+var strLang154 = "宛先";
+var strLang155 = "検索結果がありません。";
+var strLang156 = "総 ";
+var strLang157 = "個";
+var strLang158 = "検索中にエラーが発生しました。 - ";
+var strLang159 = "送信日付";
+var strLang160 = "差出人";
+var strLang161 = "受信箱";
+var strLang162 = "送信済みメール";
+var strLang163 = "下書き";
+var strLang164 = "ゴミ箱";
+var strLang165 = "";
+var strLang166 = "掲示物が削除されました。";
+var strLang167 = "空のファイルは添付することができません."
+var strLang168 = "大容量添付派一寵大きさが "
+var strLang169 = "を超えることができません。"
+var strLang170 = "様式を選択してください"
+var strLang171 = "フォームを1つだけ選択してください。"
+var strLang172 = "ファイル名を入力してください。"
+var strLang173 = "ファイル名に特殊文字を含めることはできません。"
+var strLang174 = "同じ名前のファイルがあります。重ねて書き込みますか？"
+var strLang175 = "同じ名前のライブラリが存在しています。"
+var strLang176 = "最上位のボックスを削除することはできません。"
+var strLang177 = "選択し、フォルダ、およびサブフォルダとファイルがすべて削除されます。\n 削除してもよろしいですか？"
+var strLang178 = "に返信しています"
+var strLang179 = "に全員へ返信しています"
+var strLang180 = "に提供しています"
+var strLang181 = "上記のメールは、有料フェウォンイル場合のみメールを取得することができます"
+var strLang182 = "受信者が多いので、個々の発信機能を使用することはできません。 \r\n 個々の発信を解除してメールを送信しますか？";
+var strLang183 = "添付ファイルの削除中にエラーが発生しました。";
+//######################### 메일 이전 다음 기능#########################
+var strLang184 = "前の手紙がご利用いただけます。";
+var strLang185 = "次の手紙がご利用いただけます。";
+//######################### 메일 이전 다음 기능#########################
+
+
+var strLang187 = "ひな形を選択して下さい。"
+var strLang188 = "ひな形を一つだけ選んでください。"
+
+var strLang191 = "迷惑メール";
+var strLang192 = "PERSONAL";
+
+var strLang193 = "グループのメールを追加することはできません。\n メンバーのビューの選択]ボタンを使用してください";
+var strLang194 = "簡単で、アドレス帳は100件まで設定可能です。";
+var strLang195 = "簡単にアドレスを入力する際にエラーが発生";
+var strLang196 = "氏名を入力してください。　";
+var strLang197 = "メールアドレスを入力してください。";
+var strLang198 = "メールアドレスが正しくありません。";
+var strLang199 = "- アドレス-";
+
+// ### 자동분류
+var strLang200 = "データを読み込んでいます。";
+var strLang201 = "完全に削除";
+var strLang202 = "登録されたデータがありません。カテゴリを登録してください。";
+var strLang203 = "次の条件のとき：";
+var strLang204 = "差出人は：";
+var strLang205 = "宛先は：";
+var strLang206 = "\"を含むとき。";
+var strLang207 = "メールのタイトルに：";
+var strLang208 = "メールの本文に：";
+var strLang209 = "\"特定の単語が含まれたとき。";
+var strLang210 = "または";
+var strLang211 = "次の操作を実行します：";
+var strLang212 = "保存せずに永久に削除";
+var strLang213 = "フォルダに移動";
+var strLang214 = "カテゴリを選択してください。";
+var strLang215 = "削除しました。";
+var strLang216 = "削除中にエラーが発生しました。";
+var strLang217 = "変更中にエラーが発生しました。";
+var strLang218 = "これ以上の条件を追加することはできません。 \n 編集中に変更してください。";
+var strLang219 = "選択する";
+var strLang220 = "\"(へ)に 移動";
+var strLang221 = "同じ値があります。";
+var strLang222 = "輸入的值是不是電子郵件地址的格式。\n 例子）hongkildong@naver.com請在相同的格式。";
+var strLang223 = "項目を入力してください。";
+var strLang224 = "既にその条件を選択しています。";
+var strLang225 = "既に条件が設定されています。続行すると以前の設定は消去されます。\n 新しい設定に置き換えますか。";
+var strLang226 = "差出人を登録してください。　例）\"hong@kaoni.com\"";
+var strLang227 = "受信者を登録してください。  例）\"hong@kaoni.com\"";
+var strLang228 = "タイトルを含む単語を登録してください。例子）\"広告\"";
+var strLang229 = "本文に含まれて単語を登録してください。例子）\"ローン\"";
+var strLang230 = "ルールの件名を入力してください。　";
+var strLang231 = "複数の条件を選択してください。";
+var strLang232 = "実行するタスクを選択してください。";
+var strLang233 = "エラーが発生しました。";
+var strLang234 = "\"保存せずに永久に削除\"";
+var strLang235 = " または \"";
+var strLang236 = "\" または \"";
+var strLang237 = "\"ゴミ箱\"(へ)に 移動";
+var strLang238 = " エラーが発生しました。";
+var strLang239 = "追加";
+var strLang240 = "修正";
+// ### 자동분류
+
+//사서함 크기 초과 문구 추가
+var strLang241 = "メールボックスが最大メールボックスサイズを超えています.";
+var strLang242 = "選択されたルールは、現在のグループウェアでは表示されません。これらのルールを表示するにはOutlook Web AppまたはOutlookを使用してください。";
+var strLang243 = "予約メールを保存することはできません\n既に出荷または削除処理されます。";
+var strLang244 = "※発送30分前までは、件名をクリックして内容を変更することができます。";
+
+var strLang245 = "大容量の添付ファイル";
+var strLang246 = "ダウンロード期間 : ";
+var strLang247 = "大容量の添付ファイルは、 ";
+var strLang248 = "日";
+var strLang249 = "保管";
+
+var strLang250 = "フィールドではJavaScriptが有効になっていません。";
+
+var strLang251 = "コピー";
+var strLang252 = "移動";
+var strLang253 = "削除";
+var strLang254 = "検索語を入力してください。";
+var strLang255 = "未読メール";
+var strLang256 = "全体メール";
+var strLang257 = "通";
+var strLang258 = "以前";
+var strLang259 = "次";
+var strLang260 = "署名のサイズを超えました";
+var strLang261 = "既に回収したメールです結果を表示しますか？";
+var strLang300 = "人";
+var strLang301 = "メールアドレスがありません。";
+var strLang302 = "メール検索時の特殊文字は<>は使用できません。 <>削除した後検索します。";
+var strLang303 = "右の表示比率の合計が100を超えることができません。"
+var strLang304 = "下を見るの割合の合計が100を超えることができません。"
+var strLang305 = "前のページをロードします.\nロードした後、自動的にメール閲覧ウィンドウが開きます.";
+var strLang306 = "次のページをロードしています.\nロードした後、自動的にメール閲覧ウィンドウが開きます.";
+
+var strLang307 = "差出人のドメインに";
+var strLang308 = "ドメインを登録してください。";
+var strLang309 = "入力した形式が正しくありません。";
+var strLang310 = "入力した形式が正しくありません。\n例）@kaoni.comのように入力してください。";
+var strLang311 = "メールを選択してください。";
+var strLang312 = "検索用語を確認してください。";
+
+var strLang313 = "キャンセルしますか。";
+
+var strLang314 = "アドレス";
+var strLang315 = "差出人";
+var strLang316 = "受信者";
+
+var strLang317 = "ファイル名";
+var strLang318 = "ファイルのサイズ";
+var strLang319 = "不在メッセージの画像を入れることができません.";
+
+var strLang320 = "処理しました.";
+var strLang321 = "処理中にエラーが発生しました.";
+var strLang322 = "グループメールは追加されません。";
+var strLang323 = "限られた拡張アップロードファイル。";
+var strLang324 = "同じドメイン内に存在しています。";
+
+var strLang325 = "待つ";
+var strLang326 = "取消";
+var strLang327 = "読む";
+var strLang328 = "消す";
+
+var strLang329 = "デフォルトフォルダPCアーカイブを変更しました。";
+var strLang330 = "フォルダを削除しますか？";
+var strLang331 = "フォルダを選択してください。";
+var strLang332 = "あなたは、最上位フォルダを削除することはできません。";
+var strLang333 = "あなたは永久にフォルダ内のすべてのファイルを削除してもよろしいですか？";
+var strLang334 = "フォルダを選択してください。";
+var strLang335 = "許可されていない文字が含まれています。";
+var strLang336 = "경로를 찾을 수 없습니다.";
+
+var strLang337 = "匿名掲示板またはフォト掲示板またはURLリンク掲示板にはメール/決栽文書を掲示することができません。";
+
+// ### 자동분류 추가
+var strLang338 = "件名または本文に含む単語を登録してください。例子）\"広告\"";
+var strLang339 = "件名または本文に";
+var strLang340 = "すべてのメッセージに適用";
+var strLang341 = "メッセージを開封済みにする";
+var strLang342 = "\" に コピーする";
+var strLang343 = "メッセージの重要度 : ";
+var strLang344 = "メッセージを次へ転送する";
+var strLang345 = "メッセージを次へリダイレクトする";
+var strLang346 = "低";
+var strLang347 = "標準";
+var strLang348 = "高";
+var strLang349 = "ただし次の場合を除く(OR) : ";
+var strLang700 = "データが存在しません。";
+// ### 자동분류 추가
+// ### 수신거부 추가
+var strLang350 = "項目を選択してください。";
+var strLang351 = "入力形式が正しくありません。";
+var strLang352 = "同じ組織のアドレスであるため、受信拒否リストに追加することができません。";
+var strLang353 = "同じドメインまたはメールアドレスが存在します。";
+// ### 수신거부 추가
+
+var strLang354 = "ドメイン名を入力してください。";
+
+function GetLocalTime(Offset , pDateTime)
+{
+    if (pDateTime == "")
+        return "";
+
+    var pOffset = Offset.split("|")[1];
+	var pOffsetHour = pOffset.split(":")[0];
+	var pOffsetMinute = pOffset.split(":")[1];
+	
+	var szYear = pDateTime.substring(0,4);
+	var szMonth = pDateTime.substring(5,7);
+	var szDay = pDateTime.substring(8,10);
+	var szHr = Number(pDateTime.substring(11,13)) + Number(pOffsetHour);
+	var szMin = Number(pDateTime.substring(14,16)) + Number(pOffsetMinute);
+	var szSec = pDateTime.substring(17,19)
+	var ibjD = new Date();	
+	
+	
+	
+	ibjD.setFullYear(szYear ,szMonth -1 , szDay );
+	ibjD.setHours(szHr ,szMin , szSec );
+	
+	var rYear = ibjD.getFullYear();
+	var rMonth = addzero(ibjD.getMonth()+1);
+	var rDate = addzero(ibjD.getDate());
+	var rHour = addzero(ibjD.getHours());
+	var rMin = addzero(ibjD.getMinutes());
+	var rSec = addzero(ibjD.getSeconds());	
+	
+	//javascript 31 보정
+	/*if (szDay =="31" && (Number(pDateTime.substring(11,13)) + Number(pOffsetHour) < 24))
+	{
+		rMonth = addzero(Number(rMonth) -1) ;
+		rDate = 31 ;		
+	}
+	*/
+
+	return rYear + "-" + rMonth + "-" + rDate + "T" + rHour + ":" + rMin;//  + ":" + rSec;
+}
+
+function ReversGetLocalTime(Offset , pDateTime)
+{
+	var pOffset = Offset.split("|")[1];
+	var pOffsetHour = pOffset.split(":")[0];
+	var pOffsetMinute = pOffset.split(":")[1];
+	
+	var szYear = pDateTime.substring(0,4);
+	var szMonth = pDateTime.substring(5,7);
+	var szDay = pDateTime.substring(8,10);
+	var szHr = Number(pDateTime.substring(11,13)) - Number(pOffsetHour);
+	var szMin = Number(pDateTime.substring(14,16)) - Number(pOffsetMinute);
+	var szSec = pDateTime.substring(17,19)
+	var ibjD = new Date();	
+	
+	ibjD.setFullYear(szYear ,szMonth - 1 , szDay);
+	
+	
+	ibjD.setHours(szHr ,szMin , szSec );	
+	var rYear = ibjD.getFullYear();
+	var rMonth = addzero(ibjD.getMonth() + 1);
+	var rDate = addzero(ibjD.getDate());
+	var rHour = addzero(ibjD.getHours());
+	var rMin = addzero(ibjD.getMinutes());
+	var rSec = addzero(ibjD.getSeconds());	
+	
+	//javascript 31 보정
+	/*if (szDay =="31" && rDate =="01")
+	{
+		rMonth = addzero(Number(rMonth) -1) ;
+		rDate = 31 ;		
+	}*/
+	
+	
+	return rYear + "-" + rMonth + "-" + rDate + " " + rHour+ ":" +rMin + ":" + rSec;
+}
+function addzero(arg)
+{
+	if (arg < 10)
+	{
+		arg = "0" + arg;
+	}
+	return arg
+
+}
+function folderdisnameChange(pFolderPath)
+{
+	var FolderNameKo = new Array( "받은 편지함" ,  "보낸 편지함" ,  "임시 보관함" ,  "지운 편지함" ,  "개인편지함" );
+	var FolderNameEn = new Array("Inbox", "Sent Items", "Drafts", "Deleted Items", "PERSONAL");
+	var DispName = new Array(strLang68, strLang69, strLang70, strLang4, strLang71);
+	
+	var ArrayPath = pFolderPath.split("/")
+	for (var j=0; j<5; j++)
+	{
+		if (ArrayPath[0] == FolderNameEn[j]) pFolderPath = pFolderPath.replace(ArrayPath[0] , DispName[j])
+		if (ArrayPath[0] == FolderNameKo[j]) pFolderPath = pFolderPath.replace(ArrayPath[0] , DispName[j])
+	}
+	return pFolderPath
+}
