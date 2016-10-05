b487f03f5074a6c6e9ecfd70d42f343155086c74
seokz <seokz@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-10-05 01:35:28 +0000

    * imagePortletVO 추가
    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@3496 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

---
+++ src/main/java/egovframework/ezEKP/ezPortal/vo/PortalImagePortletVO.java
@@ -0,0 +1,69 @@
+package egovframework.ezEKP.ezPortal.vo;
+
+public class PortalImagePortletVO {
+	/** */
+	private String uID_;
+	/** */
+	private String imagePath;
+	/** */
+	private int imageWidth;
+	/** */
+	private int imageHeight;
+	/** */
+	private String imageType;
+	/** */
+	private String openMode;
+	/** */
+	private String windowOption;
+	/** */
+	private String maxURL;
+	
+	public String getuID_() {
+		return uID_;
+	}
+	public void setuID_(String uID_) {
+		this.uID_ = uID_;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public int getImageWidth() {
+		return imageWidth;
+	}
+	public void setImageWidth(int imageWidth) {
+		this.imageWidth = imageWidth;
+	}
+	public int getImageHeight() {
+		return imageHeight;
+	}
+	public void setImageHeight(int imageHeight) {
+		this.imageHeight = imageHeight;
+	}
+	public String getImageType() {
+		return imageType;
+	}
+	public void setImageType(String imageType) {
+		this.imageType = imageType;
+	}
+	public String getOpenMode() {
+		return openMode;
+	}
+	public void setOpenMode(String openMode) {
+		this.openMode = openMode;
+	}
+	public String getWindowOption() {
+		return windowOption;
+	}
+	public void setWindowOption(String windowOption) {
+		this.windowOption = windowOption;
+	}
+	public String getMaxURL() {
+		return maxURL;
+	}
+	public void setMaxURL(String maxURL) {
+		this.maxURL = maxURL;
+	}
+}
