b75de2f81d89bd44c21826c9fcdf9b66974dd55b
dhlee <dhlee@746f5ef6-3e3b-473a-8c3d-618c0f8083c2>
2016-10-20 06:15:09 +0000

    * 메일   - 메일 쓰기 창에서 현재 추가한 이미지 파일들을 '발송' 시에는 삭제하도록 함.
    git-svn-id: svn://svn.opensol2014.com/repo1/ezEKP4Java@3753 746f5ef6-3e3b-473a-8c3d-618c0f8083c2

--- src/main/java/egovframework/ezEKP/ezEmail/web/EzEmailMailWriteController.java
+++ src/main/java/egovframework/ezEKP/ezEmail/web/EzEmailMailWriteController.java
@@ -2507,28 +2507,32 @@ public class EzEmailMailWriteController extends EgovFileMngUtil {
 		        
 	        }
 	        
-	        //file system의 inline image 파일 삭제 - 경로가 upload_common인 파일만 삭제(스케줄러로 지우기로함.)
-//	        NodeList imagePathList = root.getElementsByTagName("IMAGEPATH");
-//	        if (imagePathList != null && imagePathList.getLength() > 0) {
-//	            String imagePath = "";
-//	            
-//	        	for (int i=0; true; i++) {
-//	            	if (imagePathList.item(i) == null) {
-//	            		break;
-//	            	}
-//	            	
-//	            	imagePath = imagePathList.item(i).getTextContent();
-//	            	
-//	            	if (!imagePath.trim().equals("") && imagePath.contains(config.getProperty("upload_common.ROOT"))) {
-//	                	imagePath = new URL(imagePath).getPath();
-//	                	String pDirPath = realPath + imagePath;
-//	            		File f = new File(pDirPath);
-//	            		if (f.exists()) {
-//	            			f.delete();
-//	            		}
-//	            	}
-//	        	}
-//	        }
+	        // file system의 inline image 파일 삭제 - 경로가 upload_common인 파일만 삭제
+	        // 발송의 경우에만 삭제하고 저장의 경우에는 쓰기 창이 계속 표시되어 있는 상태이므로 삭제하지 않고 유지한다.
+	        // 남아있는 이미지 파일들은 스케쥴러에 의해 삭제되어야 함.
+	        if (cmd.equalsIgnoreCase("SEND")) {
+    	        NodeList imagePathList = root.getElementsByTagName("IMAGEPATH");
+    	        if (imagePathList != null && imagePathList.getLength() > 0) {
+    	            String imagePath = "";
+    	            
+    	        	for (int i=0; true; i++) {
+    	            	if (imagePathList.item(i) == null) {
+    	            		break;
+    	            	}
+    	            	
+    	            	imagePath = imagePathList.item(i).getTextContent();
+    	            	
+    	            	if (!imagePath.trim().equals("") && imagePath.contains(config.getProperty("upload_common.ROOT"))) {
+    	                	imagePath = new URL(imagePath).getPath();
+    	                	String pDirPath = realPath + imagePath;
+    	            		File f = new File(pDirPath);
+    	            		if (f.exists()) {
+    	            			f.delete();
+    	            		}
+    	            	}
+    	        	}
+    	        }
+	        }
 	        
 	        folder.close(true);
 	        // sendmail_2010변환 끝
