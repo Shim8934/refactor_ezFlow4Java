5a89cfc6feacb94d8d27a7a45a9b8323c7c32dbb
Hyojin Lee <hyojin0414@kaoni.com>
2017-02-28 19:20:14 +0900

    * 전자결재 분류코드,단위업무관리 String -> DB에서 꺼내게 수정중

--- src/main/java/egovframework/ezEKP/ezApprovalG/vo/ApprGListHeaderVO.java
+++ src/main/java/egovframework/ezEKP/ezApprovalG/vo/ApprGListHeaderVO.java
@@ -4,16 +4,42 @@ public class ApprGListHeaderVO {
 
 	/** 헤더명*/
 	private String name;
+	/** 헤더명*/
+	private String name2;
+	/** 헤더명*/
+	private String name3;
+	/** 헤더명*/
+	private String name4;
 	/** 헤더길이*/
 	private String width;
 	/** 컬럼명*/
 	private String colName;
+	/** */
+	private String tablename;
 	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getName2() {
+		return name2;
+	}
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+	public String getName3() {
+		return name3;
+	}
+	public void setName3(String name3) {
+		this.name3 = name3;
+	}
+	public String getName4() {
+		return name4;
+	}
+	public void setName4(String name4) {
+		this.name4 = name4;
 	}
 	public String getWidth() {
 		return width;
@@ -27,6 +53,10 @@ public class ApprGListHeaderVO {
 	public void setColName(String colName) {
 		this.colName = colName;
 	}
-	
-	
+	public String getTablename() {
+		return tablename;
+	}
+	public void setTablename(String tablename) {
+		this.tablename = tablename;
+	}
 }
