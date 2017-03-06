b892c76e5205ce437acd60b7d13fe51224f45c3d
fomace <fomace@naver.com>
2017-03-06 16:00:35 +0900

    그룹웨어 배경화면 및 달력 수정 & 이미지 업데이트

--- src/main/webapp/js/ezSchedule/Calendar/CalendarMini_IEEIP.js
+++ src/main/webapp/js/ezSchedule/Calendar/CalendarMini_IEEIP.js
@@ -130,9 +130,9 @@ function GetTableMiniBodyObj() {
     var month = parseInt(document.getElementById("iMon").value, 10);
 
     if (DefaultView == 0)
-        dayOfWeeks = strLang5_1; // 일>토
+        dayOfWeeks = strLang5; // 일>토
     else if (DefaultView == 1)
-        dayOfWeeks = strLang6_1; // 월>일
+        dayOfWeeks = strLang6; // 월>일
 
     oBeforeDate = new Date(new Date(year, month - 1, 1) - 86400000);  // 이전달
     oThisDate = new Date(year, month - 1, 1); // 현재달
