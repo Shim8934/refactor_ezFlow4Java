b3970a1e8cbb3350516b2d14fad9998b21a4f799
Hyojin Lee <hyojin0414@kaoni.com>
2017-03-07 13:43:10 +0900

    * 커뮤니티 생성 시 alert창 community 리소스에서 가져오게 수정

--- src/main/java/egovframework/ezEKP/ezCommunity/service/impl/EzCommunityServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezCommunity/service/impl/EzCommunityServiceImpl.java
@@ -472,7 +472,7 @@ public class EzCommunityServiceImpl extends EgovAbstractServiceImpl implements E
 		
 		if (clubVO == null) {
 			response.getWriter().write("<script language='javascript'>\n");
-			response.getWriter().write("alert('Community" + egovMessageSource.getMessage("ezCommunity.t1027", userInfo.getLocale()) + "');\n");
+			response.getWriter().write("alert('" + egovMessageSource.getMessage("ezCommunity.t1529", userInfo.getLocale()) + egovMessageSource.getMessage("ezCommunity.t1027", userInfo.getLocale()) + "');\n");
 			response.getWriter().write("document.location.href = '/ezCommunity/commMake.do?flag=1';\n");
 			response.getWriter().write("</script>");
 			response.getWriter().flush();
