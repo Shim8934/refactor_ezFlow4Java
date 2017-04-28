214754f28d2c96795b8f57a6cbeefd2d16c80997
fomace <fomace@naver.com>
2017-04-28 15:15:57 +0900

    ckeditor 4.6 최종버전 업로드

--- src/main/webapp/js/ckEditor/config.js
+++ src/main/webapp/js/ckEditor/config.js
@@ -21,28 +21,19 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] }
 	];
 
-	config.filebrowserUploadUrl = "/ezCommon/ckSimpleUpload.do";	
-	config.extraPlugins = "_Insert_Image,lineheight,pastefromexcel,justify,colorbutton,colordialog";
+	config.uploadUrl = "/ezCommon/ckSimpleUpload.do";	
+	config.extraPlugins = "_Insert_Image,pastefromexcel";
     config.line_height = "0.5;1;1.5;2;2.5;3;3.5;4;4.5;5";
-    config.allowedContent = true;
-    //config.font_defaultLabel = '맑은 고딕';
-	//config.font_names = '맑은 고딕; 돋움; 굴림; 궁서; 바탕;' +  CKEDITOR.config.font_names;
+    config.allowedContent = true;    
     config.specialChars = ['!', '&quot;', '#', '$', '%', '&amp;', "'", '(', ')', '*', '+', '-', '.', '/', ':', ';', '&lt;', '=', '&gt;', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', "&euro;", "&lsquo;", "&rsquo;", "&ldquo;", "&rdquo;", "&ndash;", "&mdash;", "&iexcl;", "&cent;", "&pound;", "&curren;", "&yen;", "&brvbar;", "&sect;", "&uml;", "&copy;", "&ordf;", "&laquo;", "&not;", "&reg;", "&macr;", "&deg;", "&sup2;", "&sup3;", "&acute;", "&micro;", "&para;", "&middot;", "&cedil;", "&sup1;", "&ordm;", "&raquo;", "&frac14;", "&frac12;", "&frac34;", "&iquest;", "&Agrave;", "&Aacute;", "&Acirc;", "&Atilde;", "&Auml;", "&Aring;", "&AElig;", "&Ccedil;", "&Egrave;", "&Eacute;", "&Ecirc;", "&Euml;", "&Igrave;", "&Iacute;", "&Icirc;", "&Iuml;", "&ETH;", "&Ntilde;", "&Ograve;", "&Oacute;", "&Ocirc;", "&Otilde;", "&Ouml;", "&times;", "&Oslash;", "&Ugrave;", "&Uacute;", "&Ucirc;", "&Uuml;", "&Yacute;", "&THORN;", "&szlig;", "&agrave;", "&aacute;", "&acirc;", "&atilde;", "&auml;", "&aring;", "&aelig;", "&ccedil;", "&egrave;", "&eacute;", "&ecirc;", "&euml;", "&igrave;", "&iacute;", "&icirc;", "&iuml;", "&eth;", "&ntilde;", "&ograve;", "&oacute;", "&ocirc;", "&otilde;", "&ouml;", "&divide;", "&oslash;", "&ugrave;", "&uacute;", "&ucirc;", "&uuml;", "&yacute;", "&thorn;", "&yuml;", "&OElig;", "&oelig;", "&#372;", "&#374", "&#373", "&#375;", "&sbquo;", "&#8219;", "&bdquo;", "&hellip;", "&trade;", "&#9658;", "&bull;", "&rarr;", "&rArr;", "&hArr;", "&diams;", "&asymp;"]
-    config.skin = 'moonocolor';
-    //config.fontSize_defaultLabel = '12px';
-    //config.format_p = { element: 'p', attributes: { 'font-size': '12px' } };
-	//Remove some buttons provided by the standard plugins, which are
-	//not needed in the Standard(s) toolbar.
+    config.skin = 'moonocolor';    
 	config.removeButtons = 'Image,Underline,Subscript,Superscript,Styles';
-	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
-	// Simplify the dialog windows.
 	config.removeDialogTabs = 'link:target;link:advanced;image:Link;image:advanced;image:Upload';
 	config.colorButton_colors =
 	    '000,800000,8B4513,2F4F4F,008080,000080,4B0082,696969,' +
 	    'B22222,A52A2A,DAA520,006400,40E0D0,0000CD,800080,808080,' +
 	    'F00,FF8C00,FFD700,008000,0FF,00F,EE82EE,A9A9A9,' +
 	    'FFA07A,FFA500,FFFF00,00FF00,AFEEEE,ADD8E6,DDA0DD,D3D3D3,' +
-	    'FFF0F5,FAEBD7,FFFFE0,F0FFF0,F0FFFF,F0F8FF,E6E6FA,FFF';
-	
+	    'FFF0F5,FAEBD7,FFFFE0,F0FFF0,F0FFFF,F0F8FF,E6E6FA,FFF';	
 };
