bbb5b6e9639005c0b6ebaf8db666e630987ebfdb
skyblue0o0 <skyblue0o0@kaoni.com>
2017-05-02 13:44:49 +0900

    * getBodyInfo에 locale 추가

--- src/main/java/egovframework/ezEKP/ezEmail/web/EzEmailReservationController.java
+++ src/main/java/egovframework/ezEKP/ezEmail/web/EzEmailReservationController.java
@@ -395,7 +395,7 @@ public class EzEmailReservationController extends EgovFileMngUtil {
 			
 			// analyze the message and retrieve the attached file list.
 			List<Map<String, String>> attachedFileList = new ArrayList<Map<String, String>>();
-			List<String> bodyInfoList = ezEmailUtil.getBodyInfo(message, draftsFolderName, uid, -1, attachedFileList, false);					
+			List<String> bodyInfoList = ezEmailUtil.getBodyInfo(message, draftsFolderName, uid, -1, attachedFileList, false, locale);					
 			body = EgovStringUtil.getSpclStrCnvr2(bodyInfoList.get(0));
 			
 			if (attachedFileList.size() > 0) {
