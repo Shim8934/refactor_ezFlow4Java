7cedebfe8cd9f1ccc57c60b3ac65b7c9ac443fde
skyblue0o0 <skyblue0o0@kaoni.com>
2017-05-15 22:05:01 +0900

    * ~ck.do 이름 붙은거 ck뺌 -> tagfree도 같이 사용하도록 수정할 예정이므로.

--- src/main/webapp/js/ckEditor/plugins/_Insert_Image/plugin.js
+++ src/main/webapp/js/ckEditor/plugins/_Insert_Image/plugin.js
@@ -21,7 +21,7 @@ function Insert_ImageCmd_Complete(rtn) {
 		    if (!CrossYN()) {
 		    	var rtn;
 		    	
-		    	if (parent.document.location.href.toLowerCase().indexOf("/ezemail/mailsignatureck.do") > -1) {
+		    	if (parent.document.location.href.toLowerCase().indexOf("/ezemail/mailsignature.do") > -1) {
 		    		rtn = showModalDialog("/ezEmail/ckImageUpload.do", this, "dialogHeight:400px; dialogWidth:470px; status:no;scroll:no; help:no; edge:sunken");
 		    		parent.document.getElementById("iFramePanel").style.left = "165px";
 		    	} else {
@@ -36,7 +36,7 @@ function Insert_ImageCmd_Complete(rtn) {
 		        var imgHeight = rtn[2];
 		        CKEDITOR.instances.editor1.insertHtml('<img src=\"' + imgSrc + '\" width=\"' + imgWidth + '\"  height=\"' + imgHeight + '\" />')
 		    } else {
-			    if (parent.document.location.href.toLowerCase().indexOf("/ezemail/mailsignatureck.do") > -1) {
+			    if (parent.document.location.href.toLowerCase().indexOf("/ezemail/mailsignature.do") > -1) {
 			    	if (parent.document.getElementById("mailPanel") != null) {
 			            parent.DivPopUpShow(470, 400, "/ezEmail/ckImageUpload.do");
 			    		parent.document.getElementById("iFramePanel").style.left = "165px";
