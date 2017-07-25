dbe8f15dc45b6dc2f8ebbc199900e3231c198a5e
fomace <fomace@naver.com>
2017-07-25 16:47:58 +0900

    MCommon 환경설정정보 함수 개발 및 일정관리 List

--- src/main/java/egovframework/ezMobile/ezOption/service/impl/MOptionServiceImpl.java
+++ src/main/java/egovframework/ezMobile/ezOption/service/impl/MOptionServiceImpl.java
@@ -11,24 +11,43 @@ import org.springframework.stereotype.Service;
 
 import egovframework.ezMobile.ezOption.dao.MOptionDAO;
 import egovframework.ezMobile.ezOption.service.MOptionService;
+import egovframework.ezMobile.ezOption.vo.MCommonVO;
 import egovframework.ezMobile.ezPortal.web.MPortalController;
 import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
 
 @Service("MOptionService")
 public class MOptionServiceImpl extends EgovAbstractServiceImpl implements MOptionService {
 
-	private static final Logger logger = LoggerFactory.getLogger(MPortalController.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(MPortalController.class);
 
 	@Resource(name = "MOptionDAO")
 	private MOptionDAO mOptionDAO;
 	
 	@Override
 	public String saveOption(String id, String langFlag, String dpBoardCnt, String resourceChk, String resourceYN, int tenantId) throws Exception {
-		logger.debug("saveOption started");
+		LOGGER.debug("saveOption started");
 		
 		Map<String, Object> map = new HashMap<String, Object>();
-		MOptionDAO.insertOption(map);
-		logger.debug("saveOption ended");
+		mOptionDAO.insertOption(map);
+		
+		LOGGER.debug("saveOption ended");
+		
 		return null;
 	}
+
+	@Override
+	public MCommonVO commonInfo(String serverName, String userId) throws Exception {
+		LOGGER.debug("commonInfo started");
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("serverName", serverName);
+		map.put("userId", userId);
+		
+		MCommonVO info = mOptionDAO.commonInfo(map);
+		
+		LOGGER.debug("commonInfo ended");
+		
+		return info;
+	}	
+	
 }
