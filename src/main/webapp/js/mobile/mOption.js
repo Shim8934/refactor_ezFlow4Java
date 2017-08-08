4094ae46c3cd2c69e698e622b8dab73fbb20d3e4
naman79 <naman79@kaoni.com>
2017-08-08 18:06:26 +0900

    모바일 환경설정 W/C js 추가

--- src/main/webapp/js/mobile/mOption.js
+++ src/main/webapp/js/mobile/mOption.js
@@ -39,3 +39,33 @@
     		}
         });
  	}
+	
+	function saveOption() {
+		var resourceChk = "";
+		
+        $("input[name=resourceChk]:checked").each(function() {
+        	resourceChk += $(this).val() + ",";
+        });
+        
+        var lang = $('input[name = radio-view]:checked').val();
+        var timeZone = $('#select-custom-1 option:selected').val();
+        var listCnt = $('input[name = slider-2]').val();
+        var useSearch = $('input[name = radio-view3]:checked').val();
+        var useSecurity = $('input[name = radio-view4]:checked').val(); 
+        
+        console.log(lang);
+        console.log(timeZone);
+        console.log(listCnt);
+        console.log(useSearch);
+        console.log(useSecurity);
+        
+
+	}
+	
+  function addPlus(flag) {
+	  if (flag == '1') {
+	  	$('#plus').closest('.ui-btn').hide(); 
+	  } else {
+		$('#plus').closest('.ui-btn').show(); 
+	  }
+  }
