4c366752688a3a42e0da6b3883e4177a246aa877
naman79 <naman79@kaoni.com>
2017-08-11 13:48:18 +0900

    모바일 환경설정 게이트웨이 소스정리

--- src/main/java/egovframework/ezMobile/ezOption/web/MOptionGWController.java
+++ src/main/java/egovframework/ezMobile/ezOption/web/MOptionGWController.java
@@ -1,6 +1,6 @@
 package egovframework.ezMobile.ezOption.web;
 
-import java.util.Map;
+
 import java.util.Properties;
 
 import javax.annotation.Resource;
@@ -24,7 +24,6 @@ import egovframework.ezMobile.ezOption.service.MOptionService;
 import egovframework.ezMobile.ezOption.vo.MCommonVO;
 import egovframework.ezMobile.ezOption.vo.MOptionVO;
 import egovframework.let.user.login.service.LoginService;
-import egovframework.let.utl.fcc.service.ClientUtil;
 import egovframework.let.utl.fcc.service.CommonUtil;
 import egovframework.let.utl.sim.service.EgovFileScrty;
 
@@ -54,6 +53,7 @@ private static final Logger LOGGER = LoggerFactory.getLogger(MOptionGWController
 	/**
 	 * 모바일 G/W 환경설정 [get] 환경설정조회
 	 */
+    @SuppressWarnings("unchecked")
 	@RequestMapping(value="/mobile/ezoption/option/users/{userId}", method= RequestMethod.GET, produces="application/json;charset=utf-8")
 	public JSONObject optionDetail(@PathVariable String userId, HttpServletRequest request) throws Exception {		
 		LOGGER.debug("MOBILE G/W OPTION [GET /mobile/ezoption/option/users/{userId}] started.");
@@ -63,7 +63,6 @@ private static final Logger LOGGER = LoggerFactory.getLogger(MOptionGWController
 		try {
 			
 			String serverName = request.getHeader("x-user-host");
-			//String serverName = "http://localhost:8080";
 			MCommonVO info = mOptionService.commonInfo(serverName, userId);
 			int tenantId = info.getTenantId();
 			LOGGER.debug("userId: " + userId);
@@ -97,6 +96,7 @@ private static final Logger LOGGER = LoggerFactory.getLogger(MOptionGWController
 	/**
 	 * 모바일 G/W 환경설정 [put] 환경설정수정
 	 */
+    @SuppressWarnings("unchecked")
 	@RequestMapping(value="/mobile/ezoption/option/users/{userId}", method= RequestMethod.PUT, produces="application/json;charset=utf-8")
 	public JSONObject optionUpdate(@PathVariable String userId, @RequestBody JSONObject jsonObject, HttpServletRequest request) throws Exception {		
 		LOGGER.debug("MOBILE G/W OPTION [PUT /mobile/ezoption/option/users/{userId}] started.");
@@ -111,40 +111,13 @@ private static final Logger LOGGER = LoggerFactory.getLogger(MOptionGWController
 			
 			String serverName = request.getHeader("x-user-host");
 			MCommonVO info = mOptionService.commonInfo(serverName, userId);
-			
-			String timeZone = "235|+09:00";
-			String lang = "1";
-			String mainType = "P";
-			String listCnt = "10";
-			String useSearch = "Y";
-			String useSecurity = "N";
-			int tenantId = 0;
-			
-			if(jsonObject.containsKey("timeZone")){
-				timeZone = jsonObject.get("timeZone").toString();
-			}
-			
-			if(jsonObject.containsKey("lang")){
-				lang = jsonObject.get("lang").toString();
-			}
-			
-			if(jsonObject.containsKey("listCnt")){
-				listCnt = jsonObject.get("listCnt").toString();
-			}
-			
-			if(jsonObject.containsKey("useSearch")){
-				useSearch = jsonObject.get("useSearch").toString();
-			}
-			
-			if(jsonObject.containsKey("useSecurity")){
-				useSecurity = jsonObject.get("useSecurity").toString();
-			}
-
-			if(jsonObject.containsKey("mainType")){
-				mainType = jsonObject.get("mainType").toString();
-			}
-			
-			tenantId = info.getTenantId();
+			String	timeZone = jsonObject.get("timeZone").toString();
+			String	lang = jsonObject.get("lang").toString();
+			String	listCnt = jsonObject.get("listCnt").toString();
+			String	useSearch = jsonObject.get("useSearch").toString();
+			String	useSecurity = jsonObject.get("useSecurity").toString();
+			String	mainType = jsonObject.get("mainType").toString();			
+			int tenantId = info.getTenantId();
 			
 			LOGGER.debug("userId: " + userId);
 			LOGGER.debug("timeZone: " + timeZone);
@@ -158,38 +131,13 @@ private static final Logger LOGGER = LoggerFactory.getLogger(MOptionGWController
 			mOptionService.updateOption(userId, timeZone, lang, mainType, listCnt, useSearch, useSecurity, tenantId);
 			
 			MOptionVO opt = mOptionService.optionInfo(userId, tenantId);
-			
-	        String ip = ClientUtil.getClientIP(request);
-	        
-	        String acceptLanguage = request.getHeader("Accept-Language"); 
-	        
-	        String returnValue = "";
-	        
-	        String primaryLang = ezCommonService.getTenantConfig("PrimaryLang", tenantId);
-	        
-		    if (acceptLanguage != null) {
-		        returnValue = acceptLanguage.substring(0, 2);
-		    //이유는 정확히 알 수 없지만 로그를 확인한 결과 윗 라인에서 acceptLanguage가 null인 경우가 발생하여 추가함.
-		    } else {				        
-		        returnValue = commonUtil.getTwoLetterLangFromLangNum(primaryLang);
-		    }
-	        
-		    StringBuilder cookieInfo = new StringBuilder();
-		   
-			cookieInfo.append("{\"uid\" : \"" + userId + "\", \"ip\" : \"" + ip + "\", \"locale\" : \"" + returnValue + "\", \"lang\" : \"" + lang + "\", \"timeZone\" : \"" + timeZone + "\", \"tenantId\" : " + tenantId);
-			cookieInfo.append("\"mainType\" : \"" + mainType + "\", \"listCnt\" : \"" + listCnt + "\" }");
-		    
+
+
 			LOGGER.debug("opt: " + opt.toString());
-			
-			String obj = "";
-			
-			Gson gson = new Gson();
-			
-			obj = gson.toJson(opt);
-			
+
 			result.put("status", "ok");
 			result.put("code", 0);			
-			result.put("data", cookieInfo.toString());
+			result.put("data", opt);
 			
 		} catch (Exception e) {
 			
