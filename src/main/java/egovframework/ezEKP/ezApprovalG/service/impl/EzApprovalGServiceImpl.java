b5089c9acfed1e374b314bf79aaee471114628b0
jungjin <park88123@kaoni.com>
2017-09-14 23:57:31 +0900

    전자결재 재사용 시 로그인 한 사람이 기안자가 아니면 로그인 한사람으로 결재선이 리셋

--- src/main/java/egovframework/ezEKP/ezApprovalG/service/impl/EzApprovalGServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezApprovalG/service/impl/EzApprovalGServiceImpl.java
@@ -17173,6 +17173,12 @@ public class EzApprovalGServiceImpl extends EgovFileMngUtil implements EzApprova
 		// 진행중인 결재 리스트 추출
 		List<ApprGAprLineVO> apprGAprLineVOList = ezApprovalGDAO.getAprLineInfo(map);
 		
+		if (isUsed.equals("reuse")) {
+			for (int i = 0; i < apprGAprLineVOList.size(); i++) {
+				apprGAprLineVOList.get(i).setAprState("");
+			}
+			apprGAprLineVOList.get(apprGAprLineVOList.size()-1).setAprState("002");
+		}
 		StringBuffer sb = new StringBuffer();
         sb.append("<DATA>");
         
