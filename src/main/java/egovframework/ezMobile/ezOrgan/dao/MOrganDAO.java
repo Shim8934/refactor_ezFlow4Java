9d6dc54a17636fc93dd5d73f3266171ee42055e9
Yoon <yoonz44@kaoni.com>
2017-09-14 20:44:20 +0900

    전자결재 모바일 수정

--- src/main/java/egovframework/ezMobile/ezOrgan/dao/MOrganDAO.java
+++ src/main/java/egovframework/ezMobile/ezOrgan/dao/MOrganDAO.java
@@ -38,4 +38,14 @@ public class MOrganDAO extends EgovAbstractDAO {
 	public List<MOrganListVO> getDeptMemberList(Map<String, Object> map) throws Exception {
 		return (List<MOrganListVO>) list("MOrganDAO.getDeptMemberList", map);
 	}
+
+	@SuppressWarnings("unchecked")
+	public List<MOrganListVO> getOrganList(Map<String, Object> map) throws Exception {
+		return (List<MOrganListVO>) list("MOrganDAO.getOrganList", map);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<MOrganListVO> getLowDeptInfo(Map<String, Object> map) throws Exception {
+		return (List<MOrganListVO>) list("MOrganDAO.getLowDeptInfo", map);
+	}
 }
