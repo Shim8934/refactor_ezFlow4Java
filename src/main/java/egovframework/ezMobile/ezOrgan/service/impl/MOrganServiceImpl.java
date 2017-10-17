2df9165ad4530ed1319fdfbdb37b91e0963eb143
JeongSeok Ji <seokz@kaoni.com>
2017-10-17 15:32:47 +0900

    * bug.kaoni.com(#9931) 수신대상을 %%로 검색하면 모든 유저가 출력되는 현상

--- src/main/java/egovframework/ezMobile/ezOrgan/service/impl/MOrganServiceImpl.java
+++ src/main/java/egovframework/ezMobile/ezOrgan/service/impl/MOrganServiceImpl.java
@@ -179,7 +179,7 @@ public class MOrganServiceImpl implements MOrganService {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("type", selectType);
 		map.put("searchFlag", searchFlag);
-		map.put("deptID", deptID);
+		map.put("deptID", deptID.replace("%", "\\%").replace("_", "\\_"));
 		map.put("lang", commonUtil.getMultiData(lang, tenantId));
 		map.put("tenantID", tenantId);
 		
