c5f479f962980430fe6e440b88c57e31dedf21eb
naman79 <naman79@kaoni.com>
2017-10-20 17:56:08 +0900

    모바일 자원관리 승인관리 수정 수정

--- src/main/java/egovframework/ezMobile/ezResource/web/MResourceGWController.java
+++ src/main/java/egovframework/ezMobile/ezResource/web/MResourceGWController.java
@@ -782,9 +782,69 @@ public class MResourceGWController extends EgovFileMngUtil {
 			String userId = jsonObject.get("userId").toString();
 			String approve = jsonObject.get("approveType").toString();
 			MCommonVO info = mOptionService.commonInfo(serverName, userId);
+			//String loginCookie = (String) jsonObject.get("loginCookie");
+			//LoginVO userInfo = commonUtil.userInfo(loginCookie);
+			//String langStr = userInfo.getLang();
+			//String ownerID = resourceId.toString();
 			
 			ezResourceService.updateSchedule(Integer.parseInt(scheduleId), resourceId, info.getCompanyId(), approve, info.getTenantId());
 			
+			//MResourceScheduleVO resVO = mResourceService.getResScheduleDetail(resourceId, scheduleId, info.getCompanyId(), info.getTenantId(), langStr);
+			
+			//String title = resVO.getTitle();
+			//String startDateTime = resVO.getStartDate();
+			//String endDateTime = resVO.getEndDate();
+			
+			
+			//LOGGER.debug("title: " + title);
+			//LOGGER.debug("loginCookie: " + loginCookie);
+			//LOGGER.debug("langStr: " + langStr);
+			//LOGGER.debug("ownerID: " + ownerID);
+			//LOGGER.debug("startDateTime: " + startDateTime);
+			//LOGGER.debug("endDateTime: " + endDateTime);
+			
+			//startDateTime = commonUtil.getDateStringInUTC(startDateTime, userInfo.getOffset(), false);
+			//endDateTime = commonUtil.getDateStringInUTC(endDateTime, userInfo.getOffset(), false);
+			
+			//LOGGER.debug("ownerID=" + ownerID + ",title=" + title + ",startDateTime=" + startDateTime + ",endDateTime=" + endDateTime);
+			
+			/*ResAdminVO resInfo = ezResourceService.getResourceAdminInfo(ownerID, userInfo.getTenantId());
+	        
+	        StringBuilder bodyContent = new StringBuilder();
+
+	        bodyContent.append("<DIV id=\"msgBody\" style=\"FONT-SIZE: 10pt; FONT-FAMILY: gulim,arial,verdana\" name=\"urn:schemas:httpmail:textdescription\">");
+	        
+	        if (userInfo.getPrimary().equals("1")) {
+	        	bodyContent.append(userInfo.getDisplayName() +"[" + userInfo.getDeptName() + "] " + egovMessageSource.getMessage("ezResource.t9900002", userInfo.getLocale()));
+	        } else {
+	        	bodyContent.append(userInfo.getDisplayName2() +"[" + userInfo.getDeptName2() + "] " + egovMessageSource.getMessage("ezResource.t9900002", userInfo.getLocale()));
+	        }
+	        
+	        bodyContent.append("<br>&nbsp;&nbsp;&nbsp;-&nbsp;" + egovMessageSource.getMessage("ezResource.t9900003", userInfo.getLocale()) + " : " +resInfo.getBrdNm()); 
+	        bodyContent.append("<br>&nbsp;&nbsp;&nbsp;-&nbsp;" + egovMessageSource.getMessage("ezResource.t9900004", userInfo.getLocale()) + " : " +startDateTime + "&nbsp;~&nbsp;" + endDateTime);
+	        bodyContent.append("</DIV>");
+	        
+	        String subject = "[" + egovMessageSource.getMessage("ezResource.t171", userInfo.getLocale()) + resInfo.getBrdNm() + "] " + title;
+	        
+	        
+	    	InternetAddress from = new InternetAddress();
+	    	from.setPersonal(userInfo.getDisplayName(), "UTF-8");
+	    	from.setAddress(userInfo.getEmail());
+	    	
+	    	String emailAddress = resInfo.getMailAddress();
+	    	String accessName = resInfo.getOwnerNm();
+	    	
+	    	if (accessName.indexOf("(") > -1) {
+	    		accessName = accessName.split("(")[0];
+	    	}
+	    	
+	    	InternetAddress to = new InternetAddress();
+	    	to.setPersonal(accessName, "UTF-8");
+	    	to.setAddress(emailAddress);
+	        	
+	        
+	        ezEmailService.sendMail(loginCookie, from, new InternetAddress[]{to}, null, null, subject, bodyContent.toString(), false);
+			*/
 			result.put("status", "ok");
 			result.put("code", 0);			
 			result.put("data", "");
