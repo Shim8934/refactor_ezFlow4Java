9caa7506f661fdf452388b67d398f46ce2bb7e42
Hyojin Lee <hyojin0414@kaoni.com>
2017-11-03 17:44:14 +0900

    * CK 에디터 스킨 복구

--- src/main/webapp/js/ezEditor/ckEditor/config.js
+++ src/main/webapp/js/ezEditor/ckEditor/config.js
@@ -36,8 +36,8 @@ CKEDITOR.editorConfig = function( config ) {
                            "&szlig;", "&agrave;", "&aacute;", "&acirc;", "&atilde;", "&auml;", "&aring;", "&aelig;", "&ccedil;", "&egrave;", "&eacute;", "&ecirc;", "&euml;", 
                            "&igrave;", "&iacute;", "&icirc;", "&iuml;", "&eth;", "&ntilde;", "&ograve;", "&oacute;", "&ocirc;", "&otilde;", "&ouml;", "&divide;", "&oslash;", 
                            "&ugrave;", "&uacute;", "&ucirc;", "&uuml;", "&yacute;", "&thorn;", "&yuml;", "&OElig;", "&oelig;", "&#372;", "&#374", "&#373", "&#375;", "&sbquo;", 
-                           "&#8219;", "&bdquo;", "&hellip;", "&trade;", "&#9658;", "&bull;", "&rarr;", "&rArr;", "&hArr;", "&diams;", "&asymp;"]
-    config.skin = 'moonocolor';    */
+                           "&#8219;", "&bdquo;", "&hellip;", "&trade;", "&#9658;", "&bull;", "&rarr;", "&rArr;", "&hArr;", "&diams;", "&asymp;"] */
+    config.skin = 'moonocolor';
     config.specialChars = ['&larr;', '&uarr;', '&rarr;', '&darr;', '&harr;', '&#8597', '&#8598', '&#8599', '&#8600', '&#8601', '&#8606', '&#8607', '&#8608', '&#8609', '&lArr;', '&uArr;', '&rArr;',  
                            '&dArr;', '&hArr;', '&laquo;', '&raquo;', '&#8560', '&#8561', '&#8562', '&#8563', '&#8564', '&#8565', '&#8566', '&#8567', '&#8568', '&#8569', '&#8570', '&#8571', '&#8544',  
                            '&#8545', '&#8546', '&#8547', '&#8548', '&#8549', '&#8550', '&#8551', '&#8552', '&#8553', '&#8554', '&#8555', '&#8251', '&#162', '&#163', '&#164', '&#165', '&#169', 
