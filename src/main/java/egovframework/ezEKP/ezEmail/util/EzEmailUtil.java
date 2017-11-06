fa0ececd80ccaea313bb7cc70347ac7b0215a7b7
fomace <fomace@naver.com>
2017-11-06 21:03:39 +0900

    #10351 파일첨부 시  단위 수정

--- src/main/java/egovframework/ezEKP/ezEmail/util/EzEmailUtil.java
+++ src/main/java/egovframework/ezEKP/ezEmail/util/EzEmailUtil.java
@@ -118,12 +118,15 @@ public class EzEmailUtil {
 		
 		if (size > 1024 * 1024) {
 			size = size / 1024.0 / 1024.0;
-			strSize = String.format("%.1fMB", size);
+			strSize = Math.floor(size / 1024 / 1024 * 10) / 10 + "MB";
+			/*strSize = String.format("%.1fMB", size);*/
 		} else if (size > 1024) {
 			size = size / 1024.0;
-			strSize = String.format("%dKB", (int)size);
+			strSize = (int)(size/1024) + "KB";
+			/*strSize = String.format("%dKB", (int)size);*/
 		} else {
-			strSize = size + "B";
+			/*strSize = size + "B";*/
+			strSize = (int)size + "B";
 		}
 
 		return strSize;
