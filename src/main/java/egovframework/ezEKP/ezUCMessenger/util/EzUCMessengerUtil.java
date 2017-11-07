eae87ecb8f0c4aa4d6cfce7dca1f8395727de894
skyblue0o0 <skyblue0o0@kaoni.com>
2017-11-07 20:12:13 +0900

    * UC메신저 > 필요없는 함수 삭제

--- src/main/java/egovframework/ezEKP/ezUCMessenger/util/EzUCMessengerUtil.java
+++ src/main/java/egovframework/ezEKP/ezUCMessenger/util/EzUCMessengerUtil.java
@@ -19,30 +19,10 @@ import org.springframework.stereotype.Component;
 public class EzUCMessengerUtil {
 	private static final Logger logger = LoggerFactory.getLogger(EzUCMessengerUtil.class);
 	
+	//AES128로 변경
 //	private static final String APB = "S0LKK1EAY021BOH1LNY3U8O3E7MI00I0";
 	private static final String APB = "S0LKK1EAY021BOH1";
     private static final String IV = "S2H1S4GU5S7E7H3D";
-
-    /**
-	 * 아이디와 패스워드를 조합하여 만든 암호화된 문자열 리턴.
-	 * 
-	 * @param id 아이디
-	 * @param pw 패스워드
-	 * @return 암호화된 문자열 리턴, exception 발생 시 null 리턴
-	 */
-	public String encryptId(String id, String pw) {
-		String result = null;
-		
-		try {
-			result = id + ":" + pw + ":" + System.currentTimeMillis();
-			result = encryptAES(result);
-		} catch (Exception e) {
-			result = null;
-			e.printStackTrace();
-		}
-		
-		return result;
-	}
     
 	public String encryptAES(String s) throws Exception {               	
         SecretKeySpec skeySpec = new SecretKeySpec(APB.getBytes("UTF-8"), "AES");            
