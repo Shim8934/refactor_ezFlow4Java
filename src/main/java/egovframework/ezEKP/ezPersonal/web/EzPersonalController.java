b274aad3a71c0db03298c3d6ade8ed407164c683
JeongSeok Ji <seokz@kaoni.com>
2017-11-27 14:02:59 +0900

    * useOCS 컨피그 삭제

--- src/main/java/egovframework/ezEKP/ezPersonal/web/EzPersonalController.java
+++ src/main/java/egovframework/ezEKP/ezPersonal/web/EzPersonalController.java
@@ -731,12 +731,12 @@ public class EzPersonalController extends EgovFileMngUtil {
 		logger.debug("personSearch started");
 
 		userInfo = commonUtil.userInfo(loginCookie);
-		String useOCS = config.getProperty("config.USE_OCS");
+		
 		String searchString = "";
 		if (req.getParameter("searchString") != null && !req.getParameter("searchString").equals("")) {
 			searchString = req.getParameter("searchString");
 		}
-		model.addAttribute("useOCS", useOCS);
+		
 		model.addAttribute("userInfo", userInfo);
 		model.addAttribute("searchString", searchString);
 
@@ -752,9 +752,7 @@ public class EzPersonalController extends EgovFileMngUtil {
 		logger.debug("personSearchPrint started");
 
 		userInfo = commonUtil.userInfo(loginCookie);
-		String useOCS = config.getProperty("config.USE_OCS");
 		
-		model.addAttribute("useOCS", useOCS);
 		model.addAttribute("userInfo", userInfo);
 
 		logger.debug("personSearchPrint ended");
