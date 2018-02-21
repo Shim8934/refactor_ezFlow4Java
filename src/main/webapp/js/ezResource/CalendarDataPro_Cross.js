6e2b11efd03c01c63bbc49c9ed36a03eaef76f3a
Kiha Kim <is8200@jgw.kaoni.com>
2018-02-21 12:37:25 +0900

    bug.kaoni.com(#11638) 자원관리 > 회의실예약 > 시간을 분단위로 임의등록가능 // 등록후 시간에서도 "0" 이 사라짐

--- src/main/webapp/js/ezResource/CalendarDataPro_Cross.js
+++ src/main/webapp/js/ezResource/CalendarDataPro_Cross.js
@@ -651,7 +651,15 @@ function CalMonthDataBind(oAppointment, oAppointment2) {
                 pTime = oAppointment2.oDtstart.replace('T', ' ').substring(0, 16) + " - " + oAppointment.oDtend.replace('T', ' ').substring(0, 16);
             else
                 pTime = oAppointment.oDtstart.replace('T', ' ').substring(0, 16) + " - " + oAppointment.oDtend.replace('T', ' ').substring(0, 16);
-            pSubject.innerHTML = oAppointment.odtstartDisplay + " - " + oAppointment.odtendDisplay + "<p style='display:inline-block; width:10px; margin:0; padding:0;'></P>" + oAppointment.oSubject;
+          
+            /*2018.02.21 김기하 #11638*/
+            //pSubject.innerHTML = oAppointment.odtstartDisplay + " - " + oAppointment.odtendDisplay + "<p style='display:inline-block; width:10px; margin:0; padding:0;'></P>" + oAppointment.oSubject;
+            
+            var timesSplit = oAppointment.odtstartDisplay.split(":");
+            var timeeSplit = oAppointment.odtendDisplay.split(":");
+            var timeofStart = timesSplit[0]+ ":" + ((timesSplit[1].length == 1)?("0" + timesSplit[1]):timesSplit[1]); 
+            var timeofEnd = timeeSplit[0] + ":" + ((timeeSplit[1].length == 1)?("0" + timeeSplit[1]):timeeSplit[1]); 
+            pSubject.innerHTML = timeofStart + " - " + timeofEnd + "<p style='display:inline-block; width:10px; margin:0; padding:0;'></P>" + oAppointment.oSubject;
         }
         else {
             pTime = strLang126;
