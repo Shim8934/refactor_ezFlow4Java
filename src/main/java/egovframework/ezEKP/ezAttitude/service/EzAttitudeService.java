3a756a6e4405bbb5bd53e6833e14eb4a5a29d5b2
Bae Hyeon Sang <plm2333@kaoni.com>
2018-03-05 20:15:52 +0900

    service 구현

--- src/main/java/egovframework/ezEKP/ezAttitude/service/EzAttitudeService.java
+++ src/main/java/egovframework/ezEKP/ezAttitude/service/EzAttitudeService.java
@@ -1,5 +1,48 @@
 package egovframework.ezEKP.ezAttitude.service;
 
-public interface EzAttitudeService {
+import java.util.List;
+import java.util.Map;
 
+import egovframework.ezEKP.ezAttitude.vo.AttitudeTypeVO;
+
+public interface EzAttitudeService {
+	public Object getAttitudeInfo(String userId, String date, String typeId, int tenantId) throws Exception;
+	
+	public int insertAttitude(String writerId, String deptId, String startdate, String enddate, String starttime, String endtime, String region,
+			String mobile, String bizsub, String content, String ip, String typeId, String companyId, int tenantId) throws Exception;
+	
+	public List<Object> getAttitudeList(String pidList, String yrmh, String typeId, int tenantId) throws Exception;
+	
+	public List<Object> getAttitudeStatisticsList(String pidList, String yrmh, int tenantId) throws Exception;
+	
+	public List<AttitudeTypeVO> getAttitudeTypeList(String companyId, int tenantId) throws Exception;
+	
+	public String getWriteFormHtml(String formId, int tenantId) throws Exception;
+	
+	public void updateAttitude(String attitudeId, String startdate, String enddate, String starttime, String endtime, String region,
+			String mobile, String bizsub, String content, String ip, String typeId, String companyId, int tenantId) throws Exception;
+	
+	public void deleteAttitude(String attitudeId, int tenantId) throws Exception;
+	
+	//메일로 발송하는 부분.... 전자결재 부분보고 알아보자 AND 상세부분
+	
+	public void insertAttitudeApplication(String attitudeId, String writerId, String writerName, String writerName2, String writerTitle,
+			String writerTitle2, String writerDeptId, String writerDeptName, String writerDeptName2, String changeDate, String changeTime,
+			String content, String companyId, int tenantId) throws Exception;
+	
+	public String getAttitudeApplStatus(String attitudeId, int tenantId) throws Exception;
+	
+	public void updateAttitudeApplication(String attitudeId, String changeDate, String changeTime, String content, String companyId, int tenantId) throws Exception;
+	
+	public void updateAttitudeApplicationApproval(String attitudeId, String apprUserId, String apprUserName, String apprUserName2, String apprStatus, 
+			int tenantId) throws Exception;
+	
+	public void deptExcelDownload(String downMode, String pidList, int tenantId) throws Exception;
+	
+	//25번 리스트보기 출력
+	public List<Map<String, String>> getDeptAttitudeList(String pidList, int tenantId) throws Exception;
+	
+	//조즥도
+	
+	
 }
---
+++ src/main/java/egovframework/ezEKP/ezAttitude/vo/AttitudeTypeVO.java
@@ -0,0 +1,77 @@
+package egovframework.ezEKP.ezAttitude.vo;
+
+public class AttitudeTypeVO {
+	/** 근태유형아이디 */
+	private String typeId;
+	/** 회사아이디 */
+	private String companyId;
+	/** 테넌트 */
+	private int tenantId;
+	/** 유형명 */
+	private String typeName;
+	/** 유형명2 */
+	private String typeName2;
+	/** 사용여부*/
+	private String isuse;
+	/** 이미지경로 */
+	private String imagePath;
+	/** parentId*/
+	private String parentId;
+	/** 작성양식아이디*/
+	private String formId;
+	public String getTypeId() {
+		return typeId;
+	}
+	public void setTypeId(String typeId) {
+		this.typeId = typeId;
+	}
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public int getTenantId() {
+		return tenantId;
+	}
+	public void setTenantId(int tenantId) {
+		this.tenantId = tenantId;
+	}
+	public String getTypeName() {
+		return typeName;
+	}
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+	public String getTypeName2() {
+		return typeName2;
+	}
+	public void setTypeName2(String typeName2) {
+		this.typeName2 = typeName2;
+	}
+	public String getIsuse() {
+		return isuse;
+	}
+	public void setIsuse(String isuse) {
+		this.isuse = isuse;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public String getParentId() {
+		return parentId;
+	}
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+	public String getFormId() {
+		return formId;
+	}
+	public void setFormId(String formId) {
+		this.formId = formId;
+	}
+	
+}
