021e48e23673ee2a914e203e7f1fa0cdc0527e3a
Bae Hyeon Sang <plm2333@kaoni.com>
2018-03-06 10:24:42 +0900

    Controller 기본셋팅

--- src/main/java/egovframework/ezEKP/ezAttitude/web/EzAttitudeController.java
+++ src/main/java/egovframework/ezEKP/ezAttitude/web/EzAttitudeController.java
@@ -2,12 +2,16 @@ package egovframework.ezEKP.ezAttitude.web;
 
 import java.util.Properties;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import egovframework.com.cmm.EgovMessageSource;
 import egovframework.let.utl.fcc.service.CommonUtil;
+import egovframework.let.utl.sim.service.EgovFileScrty;
 
 @Controller
 public class EzAttitudeController {
@@ -18,4 +22,12 @@ public class EzAttitudeController {
 	
 	@Autowired
 	private Properties config;
+	
+	@Resource(name="crypto")
+	private EgovFileScrty egovFileScrty;
+	
+	@Resource(name="egovMessageSource")
+	private EgovMessageSource egovMessageSource;
+	
+	
 }
--- src/main/java/egovframework/ezEKP/ezAttitude/web/EzAttitudeGWController.java
+++ src/main/java/egovframework/ezEKP/ezAttitude/web/EzAttitudeGWController.java
@@ -2,12 +2,22 @@ package egovframework.ezEKP.ezAttitude.web;
 
 import java.util.Properties;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.ezEKP.ezAttitude.service.EzAttitudeService;
 import egovframework.let.utl.fcc.service.CommonUtil;
+import egovframework.let.utl.sim.service.EgovFileScrty;
 
 @RestController
 public class EzAttitudeGWController {
@@ -18,4 +28,43 @@ public class EzAttitudeGWController {
 	
 	@Autowired
 	private Properties config;
+	
+	@Resource(name="crypto")
+	private EgovFileScrty egovFileScrty;
+	
+	@Resource(name="egovMessageSource")
+	private EgovMessageSource egovMessageSource;
+	
+	@Resource(name = "EzAttitudeService")
+	private EzAttitudeService ezAttitudeService;
+	
+	@RequestMapping(value = "/ezattitude/users/{userId}/attitude-list", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+	public JSONObject userAttitudeMainList(@PathVariable String userId, HttpServletRequest request) {
+		LOGGER.debug("G/W EzAttitude [GET /ezattitude/users/" + userId + "/attitude-list] started.");
+		
+		JSONObject result = new JSONObject();
+		
+		try{
+			
+		} catch (Exception e) {
+			
+		}
+		LOGGER.debug("G/W EzAttitude [GET /ezattitude/users/" + userId + "/attitude-list] ended.");
+		return result;
+	}
+	
+	@RequestMapping(value = "/ezattitude/attitude-list", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+	public JSONObject AttitudeList (HttpServletRequest request) {
+		LOGGER.debug("G/W EzAttitude [GET /ezattitude/attitude-list] started.");
+		
+		JSONObject result = new JSONObject();
+		
+		try{
+			
+		} catch (Exception e) {
+			
+		}
+		LOGGER.debug("G/W EzAttitude [GET /ezattitude/attitude-list] ended.");
+		return result;
+	}
 }
