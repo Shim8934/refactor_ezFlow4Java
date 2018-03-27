01135c99b94d38ad35c25be8b872f31aba02c21e
Juyeon Seo <jayzm@kaoni.com>
2018-03-27 14:45:52 +0900

    * ezJournal JYcontroller 수정

--- src/main/java/egovframework/ezEKP/ezJournal/web/EzJournalJYController.java
+++ src/main/java/egovframework/ezEKP/ezJournal/web/EzJournalJYController.java
@@ -30,7 +30,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
