4da34d9e20c961af8442301756c0a002ea584ebd
KangMinSu <kang6047@kaoni.com>
2018-04-18 16:21:42 +0900

    관리자 > 전체문서조회 > 첨부파일 다운로드 오류 수정

--- src/main/java/egovframework/ezEKP/ezApprovalG/web/EzApprovalGController.java
+++ src/main/java/egovframework/ezEKP/ezApprovalG/web/EzApprovalGController.java
@@ -2269,11 +2269,6 @@ public class EzApprovalGController extends EgovFileMngUtil{
 		String approvalFlag = ezCommonService.getTenantConfig("ApprovalFlag", userInfo.getTenantId());
 		String checkLine = "";
 		
-		// 결재 문서의 첨부를 다운받으려 하는사람이 결재라인에 있는지 체크 추가 2018-04-10 천성준
-		if (docID != null && docStatus != null) {
-			checkLine = ezApprovalGService.checkAprLine(docID.trim(), docStatus, userInfo.getId(), userInfo.getCompanyID(), userInfo.getTenantId());
-		}
-
 		logger.debug("docID : " + docID);
 		logger.debug("docStatus : " + docStatus);
 		logger.debug("filePath : " + filePath);
@@ -2314,6 +2309,11 @@ public class EzApprovalGController extends EgovFileMngUtil{
 			}
 			
 			logger.debug("docStatus = " + docStatus + "|| result = " + result);
+		}
+		
+		// 결재 문서의 첨부를 다운받으려 하는사람이 결재라인에 있는지 체크 추가 2018-04-10 천성준
+		if (docID != null && docStatus != null && !result.equals("PERMISSION")) {
+			checkLine = ezApprovalGService.checkAprLine(docID.trim(), docStatus, userInfo.getId(), userInfo.getCompanyID(), userInfo.getTenantId());
 		}
 		
 		//2017-04-02 클라이언트단에서 replace해서 받아와야함.
