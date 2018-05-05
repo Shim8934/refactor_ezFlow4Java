c1cb2ad115e38ae95aae5c85a139094b7f58e72f
Dongho Lee <dhlee@kaoni.com>
2018-05-05 14:29:24 +0900

    IMAP 타임아웃값 변경

--- src/main/java/egovframework/ezEKP/ezEmail/logic/IMAPAccess.java
+++ src/main/java/egovframework/ezEKP/ezEmail/logic/IMAPAccess.java
@@ -54,8 +54,9 @@ public class IMAPAccess {
 	private Locale locale;
 	// 간혹 SocketTimeoutException이 발생하는 경우가 있어 시간을 짧게 설정하고 retry를 수행하도록 함.
 	// James 서버의 netty 버전을 3.10.6.Final로 올린 이후로는 SocketTimeoutException이 발생하지 않는 것으로 보임.
-	private int timeout = 10000; 
-	private int connectionTimeout = 10000; 
+	// 청와대에서 용량이 큰 메일을 다수 삭제할 경우 타임아웃이 발생하여 10초에서 120초로 변경함 
+	private int timeout = 120000; 
+	private int connectionTimeout = 20000; 
 	
 	private static final String DEFAULT_FOLDER_NAMES 
 		= "INBOX,보낸 편지함,임시 보관함,지운 편지함,개인 편지함,정크 메일" // 한국어
