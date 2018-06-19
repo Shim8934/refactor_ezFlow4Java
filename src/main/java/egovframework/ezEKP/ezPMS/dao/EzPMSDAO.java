906db16274346083fe1eae41499ed19d1056e7b9
dphong <dphong@kaoni.com>
2018-06-19 22:30:58 +0900

    업무삭제하면서 그룹이나 프로젝트에 업무가 존재하지 않을 경우 처리중.

--- src/main/java/egovframework/ezEKP/ezPMS/dao/EzPMSDAO.java
+++ src/main/java/egovframework/ezEKP/ezPMS/dao/EzPMSDAO.java
@@ -533,4 +533,8 @@ public class EzPMSDAO extends EgovAbstractDAO {
 	public List<String> getDateTaskList(Map<String, Object> map) {
 		return (List<String>) list("EzPMSDAO.getDateTaskList", map);
 	}
+
+	public ProjectGroupVO getUpperGroupDate(Map<String, Object> map) {
+		return (ProjectGroupVO) select ("EzPMSDAO.getUpperGroupDate", map);
+	}
 }
