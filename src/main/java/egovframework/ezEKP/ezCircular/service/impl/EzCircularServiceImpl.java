ef3dd6b4c1d6f4a41a7665c0fab43c778ed0cc87
Hyojin Lee <hyojin0414@kaoni.com>
2018-07-16 17:48:33 +0900

    * 회람판 작성, 임시저장시 파일첨부확인

--- src/main/java/egovframework/ezEKP/ezCircular/service/impl/EzCircularServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezCircular/service/impl/EzCircularServiceImpl.java
@@ -195,7 +195,7 @@ public class EzCircularServiceImpl implements EzCircularService {
 		//첨부파일 저장
 		Map<String, Object> attachMap = new HashMap<String, Object>();
 		
-		if (fileList != null && !fileList.equals("[]")) {
+		if (fileList != null && !fileList.equals("")) {
 			//2018-02-13 주홍선 파일경로 잘못되어있던 것 수정
 			File file = new File(pDirPath + "uploadFile" + commonUtil.separator + circularID + "_uploadFile");
 
