8ca877f8759f140cafec3e3914abe8e713982adb
Khac <khacbao@kaoni.com>
2018-07-18 16:22:46 +0900

    * Add page navigation in related file

--- src/main/webapp/js/ezCabinet/lang/ezCabinet_ko.js
+++ src/main/webapp/js/ezCabinet/lang/ezCabinet_ko.js
@@ -51,5 +51,6 @@ var CabinetMessages = {
 	strItemErr   : "파일을 선택하세요.",
 	strMoveItem  : "파일을 이동에 성공하셨습니다.",
 	strCopyItem  : "파일을 복사에 성공하셨습니다.",
-	strExist     : "이미 추가되어 있습니다."
+	strExist     : "이미 추가되어 있습니다.",
+	strResult    : "검색결과"
 }
