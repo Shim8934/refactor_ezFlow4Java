8c5351669184b7da65bf312f3c3b5f812ec29413
yjks <yjks@kaoni.com>
2018-07-24 18:15:28 +0900

    * 구성원이 없는 부서 - 일본어 번역
     Conflicts:
    	src/main/webapp/js/ezEmail/lang/ezEmail_en.js
    	src/main/webapp/js/ezEmail/lang/ezEmail_ja.js

--- src/main/webapp/js/ezEmail/lang/ezEmail_en.js
+++ src/main/webapp/js/ezEmail/lang/ezEmail_en.js
@@ -308,7 +308,7 @@ var strLangKMS08 = "폴더는 업로드 할 수 없습니다."
 var strLangKYJ01 = "Not selected mail.";
 var strLangKYJ02 = "재작성할 메일을 하나만 선택해주세요.";
 var strLangKYJ03 = "공백은 입력할 수 없습니다."
-
+var strLangLSD02 = "구성원이 없는 부서입니다.";
 
 function GetLocalTime(Offset , pDateTime)
 {
