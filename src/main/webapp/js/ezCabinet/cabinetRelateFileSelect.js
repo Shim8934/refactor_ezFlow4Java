3525b64c4175ead7d1a4900c1f1476900b5f6d9a
hyejeongk <hyejeongk@kaoni.com>
2018-08-07 18:01:17 +0900

    캐비넷 업무일지 수정 구현

--- src/main/webapp/js/ezCabinet/cabinetRelateFileSelect.js
+++ src/main/webapp/js/ezCabinet/cabinetRelateFileSelect.js
@@ -94,6 +94,7 @@ var CabinetRlFileSelect = function() {
 			case "resrc"  : if (parentWd && parentWd.CabinetResourceFile)  {selectedArr = parentWd.CabinetResourceFile.get()  ;} break;
 			case "schedl" : if (parentWd && parentWd.CabinetScheduleFile)  {selectedArr = parentWd.CabinetScheduleFile.get()  ;} break;
 			case "todo"   : if (parentWd && parentWd.CabinetTodoFile)      {selectedArr = parentWd.CabinetTodoFile.get()      ;} break;
+			case "jounl"  : if (parentWd && parentWd.CabinetJournalFile)   {selectedArr = parentWd.CabinetJournalFile.get()   ;} break;
 			default       : if (parentWd && parentWd.CabinetAddFile)       {selectedArr = parentWd.CabinetAddFile.get()       ;}
 		}
 		
@@ -152,6 +153,7 @@ var CabinetRlFileSelect = function() {
 			case "resrc"  : if (parentWd && parentWd.CabinetResourceFile)  {parentWd.CabinetResourceFile.save(data) ;} break;
 			case "schedl" : if (parentWd && parentWd.CabinetScheduleFile)  {parentWd.CabinetScheduleFile.save(data) ;} break;
 			case "todo"   : if (parentWd && parentWd.CabinetTodoFile)      {parentWd.CabinetTodoFile.save(data)     ;} break;
+			case "jounl"  : if (parentWd && parentWd.CabinetJournalFile)   {parentWd.CabinetJournalFile.save(data)  ;} break;
 			default       : if (parentWd && parentWd.CabinetAddFile)       {parentWd.CabinetAddFile.save(data)      ;}
 		}
 		
