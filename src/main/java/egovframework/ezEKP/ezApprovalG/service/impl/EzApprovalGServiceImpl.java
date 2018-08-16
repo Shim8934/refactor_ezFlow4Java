b15ed0c6b38aa1c7a5e0c96404357faecb48c651
ChunSeongJun <chunsjun@kaoni.com>
2018-08-16 13:40:41 +0900

    * 료비 전자결재 > 분류코드 > 보존기간, 보안등급 한글로 나오는 버그 수정

--- src/main/java/egovframework/ezEKP/ezApprovalG/service/impl/EzApprovalGServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezApprovalG/service/impl/EzApprovalGServiceImpl.java
@@ -454,11 +454,11 @@ public class EzApprovalGServiceImpl extends EgovFileMngUtil implements EzApprova
 			resultXML.append("</CELL>");
 			
 			resultXML.append("<CELL>");
-			resultXML.append("<VALUE><![CDATA[" + ezApprovalGAdminService.getEtcName("SA52", k.getKeepingPeriod(), primaryData, userInfo.getCompanyID(), userInfo.getTenantId()) + "]]></VALUE>");
+			resultXML.append("<VALUE><![CDATA[" + ezApprovalGAdminService.getEtcName("SA52", k.getKeepingPeriod(), primaryData, userInfo.getLang(), userInfo.getCompanyID(), userInfo.getTenantId()) + "]]></VALUE>");
 			resultXML.append("</CELL>");
 			
 			resultXML.append("<CELL>");
-			resultXML.append("<VALUE><![CDATA[" + ezApprovalGAdminService.getEtcName("SA51", k.getItemSecurity(), primaryData, userInfo.getCompanyID(), userInfo.getTenantId()) + "]]></VALUE>");
+			resultXML.append("<VALUE><![CDATA[" + ezApprovalGAdminService.getEtcName("SA51", k.getItemSecurity(), primaryData, userInfo.getLang(), userInfo.getCompanyID(), userInfo.getTenantId()) + "]]></VALUE>");
 			resultXML.append("</CELL>");
 			
 			resultXML.append("<CELL>");
@@ -16614,11 +16614,11 @@ public class EzApprovalGServiceImpl extends EgovFileMngUtil implements EzApprova
 				
 				resultXML.append("</CELL>");
 				resultXML.append("<CELL>");
-				resultXML.append("<VALUE>" + ezApprovalGAdminService.getEtcName("SA52", makeListField(docXML.getElementsByTagName("KEEPINGPERIOD").item(k).getTextContent()), primaryData, companyID, tenantID) + "</VALUE>");
+				resultXML.append("<VALUE>" + ezApprovalGAdminService.getEtcName("SA52", makeListField(docXML.getElementsByTagName("KEEPINGPERIOD").item(k).getTextContent()), primaryData, strType, companyID, tenantID) + "</VALUE>");
 				resultXML.append("</CELL>");
 				
 				resultXML.append("<CELL>");
-				resultXML.append("<VALUE>" + ezApprovalGAdminService.getEtcName("SA51", makeListField(docXML.getElementsByTagName("ITEMSECURITY").item(k).getTextContent()), primaryData, companyID, tenantID) + "</VALUE>");
+				resultXML.append("<VALUE>" + ezApprovalGAdminService.getEtcName("SA51", makeListField(docXML.getElementsByTagName("ITEMSECURITY").item(k).getTextContent()), primaryData, strType, companyID, tenantID) + "</VALUE>");
 				resultXML.append("</CELL>");
 				
 				resultXML.append("<CELL>");
