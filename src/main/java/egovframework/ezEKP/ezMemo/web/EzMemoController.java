a17ebd1e426430de2408b646937c07b5db943cf6
yunho <hwang9yh@kaoni.com>
2018-08-17 14:57:09 +0900

    * memo > controller 연결

--- src/main/java/egovframework/ezEKP/ezMemo/web/EzMemoController.java
+++ src/main/java/egovframework/ezEKP/ezMemo/web/EzMemoController.java
@@ -1,5 +1,6 @@
 package egovframework.ezEKP.ezMemo.web;
 
+import java.util.HashMap;
 import java.util.Properties;
 
 import javax.annotation.Resource;
@@ -8,15 +9,29 @@ import javax.servlet.http.HttpServletRequest;
 
 
 
+
+
+
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import egovframework.com.cmm.EgovMessageSource;
 import egovframework.ezEKP.ezCommon.service.EzCommonService;
@@ -46,6 +61,7 @@ public class EzMemoController {
 	
 	@Autowired
 	private SimpMessagingTemplate template;
+	
 	/**
 	 * 메모 호출
 	 * */
@@ -59,8 +75,26 @@ public class EzMemoController {
 			brdID = request.getParameter("brdID");
 		}
 		
-	
 		LoginVO userInfo = commonUtil.userInfo(loginCookie);
+		
+		HashMap<String, Object> param = new HashMap<String, Object>();
+		JSONObject resultBody = commonUtil.getJsonFromMemoRestApi("/rest/ezMemo/memo-list/users/" + userInfo.getId(), param, request, "get", null);
+		String status = resultBody.get("status").toString();
+		
+		if (status.equals("ok")) {			
+		
+		/*	JSONArray list = (JSONArray) resultBody.get("data");
+			model.addAttribute("list", list);*/
+			
+		}
+		
+		
+		logger.debug("memoMain ended");
 		return "ezMemo/memoMain";
+	}
+	
+	@RequestMapping(value = "/ezMemo/memoFolderManage.do")
+	public String memoFolderManage(@CookieValue("loginCookie") String loginCookie, ModelMap modelMap, HttpServletRequest request, Model model) throws Exception {
+		return "ezMemo/memoFolderManage";
 	}
 }
--- src/main/java/egovframework/ezEKP/ezMemo/web/EzMemoGWController.java
+++ src/main/java/egovframework/ezEKP/ezMemo/web/EzMemoGWController.java
@@ -3,20 +3,31 @@ package egovframework.ezEKP.ezMemo.web;
 import java.util.Properties;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import egovframework.com.cmm.EgovMessageSource;
 import egovframework.ezEKP.ezCommon.service.EzCommonService;
+import egovframework.ezEKP.ezJournal.web.EzJournalGWController;
 import egovframework.ezEKP.ezLadder.service.EzLadderService;
 import egovframework.ezEKP.ezOrgan.service.EzOrganService;
 import egovframework.ezMobile.ezOption.service.MOptionService;
+import egovframework.ezMobile.ezOption.vo.MCommonVO;
 import egovframework.let.user.login.service.LoginService;
 import egovframework.let.utl.fcc.service.CommonUtil;
 
 @RestController
 public class EzMemoGWController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(EzMemoGWController.class);
+	
 	@Autowired
 	private CommonUtil commonUtil;
 	
@@ -40,4 +51,27 @@ public class EzMemoGWController {
 	
 	@Autowired
 	private EzOrganService ezOrganService;
+	
+	@RequestMapping(value = "/rest/ezMemo/memo-list/users/{userId}", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+	public JSONObject gwMemoList(@PathVariable String userId, HttpServletRequest request) throws Exception {
+		LOGGER.debug("G/W MEMO [GET /rest/ezMemo/memo-list/users/]" +userId + " started.");
+		
+		
+		JSONObject result = new JSONObject();
+		try {
+			String serverName = request.getHeader("x-user-host");
+			MCommonVO info = MOptionService.commonInfoWeb(serverName, request.getParameter("userId"));
+			
+			
+			result.put("status", "ok");
+			result.put("code", 0);
+			result.put("data", "");
+		} catch(Exception e) {
+			result.put("code", 1);
+			result.put("status", "error");
+			result.put("data", "");
+		}
+		LOGGER.debug("G/W MEMO [GET /rest/ezMemo/memo-list/users/] ended.");
+		return result;
+	}
 }
