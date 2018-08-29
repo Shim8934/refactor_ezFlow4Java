924f34362295e0a4d4a1bc6e37545464bb76719a
parksungbin <sbpark14@kaoni.com>
2018-08-29 17:48:55 +0900

    * bug.kaoni.com(#13517) 관리자 > 결재건수조회 시, 처리과별 건수 조회 > 발신/수신부서명이 영어로 나타남

--- src/main/java/egovframework/ezEKP/ezApprovalG/service/impl/EzApprovalGAdminServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezApprovalG/service/impl/EzApprovalGAdminServiceImpl.java
@@ -1723,7 +1723,7 @@ public class EzApprovalGAdminServiceImpl extends EgovFileMngUtil implements EzAp
 		map1.put("v_MODE", pMode);
 		map1.put("v_FROM", szFrom);
 		map1.put("v_TO", szTo);
-		map1.put("v_LANGTYPE", lang);
+		map1.put("v_LANGTYPE", commonUtil.getPrimaryData(lang, tenantID));
 		map1.put("companyID", companyID);
 		map1.put("tenantID", tenantID);
 		
