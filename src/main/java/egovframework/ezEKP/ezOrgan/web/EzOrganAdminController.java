2910f2a7c51f95fd1b0f88ba3998cb76807ed990
suasua <tndk19@kaoni.com>
2018-08-30 13:34:30 +0900

    조직도 겸직 순서 : tbl_addjobmaster orderby 컬럼 ezflow 로드시 자동 생성

--- src/main/java/egovframework/ezEKP/ezOrgan/web/EzOrganAdminController.java
+++ src/main/java/egovframework/ezEKP/ezOrgan/web/EzOrganAdminController.java
@@ -124,6 +124,7 @@ public class EzOrganAdminController extends EgovFileMngUtil {
     	logger.debug("init started.");
 
     	ezCommonService.createTblCompanyConfig();
+    	ezCommonService.addAddJobMasterOrderBy();
     	
     	logger.debug("init ended.");
     }
