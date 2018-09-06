a4e0ada0c8ab1be4b5a1c9f7a13005575537b072
Khac <khacbao@kaoni.com>
2018-09-06 13:54:04 +0900

    * 특수문자 검색 버그 고치

--- src/main/java/egovframework/ezEKP/ezCabinet/service/impl/EzCabinetServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezCabinet/service/impl/EzCabinetServiceImpl.java
@@ -1356,6 +1356,14 @@ public class EzCabinetServiceImpl extends EgovFileMngUtil implements EzCabinetSe
 			endDate             = commonUtil.getDateStringInUTC(endDateTmp, offset, true);
 		}
 		
+		if (!title.equals("")) {
+			title = title.replace("\\", "\\\\").replace("%", "\\%").replace("_", "\\_");
+		}
+		
+		if (!summary.equals("")) {
+			summary = summary.replace("\\", "\\\\").replace("%", "\\%").replace("_", "\\_");
+		}
+		
 		CabinetItemSearchVO searchVO = new CabinetItemSearchVO(Integer.parseInt(cabinetId), listCntSize, tenantId, userId, primary, offsetMinute, title, summary, creatorName, startDate, endDate, sqlQuery, srchMode, srchOption);
 		List<CabinetItemVO> itemList = new ArrayList<>();
 		
