4f5883140e90ffdd689fb1999e7ddf14766ec06b
KimMinSeong <mins99@kaoni.com>
2018-09-12 18:59:23 +0900

    * 삭제 문구 수정

--- src/main/webapp/js/ezMemo/lang/ezMemo_ko.js
+++ src/main/webapp/js/ezMemo/lang/ezMemo_ko.js
@@ -1,6 +1,6 @@
 var strLang1 = "선택된 영역이 없습니다.";
 var strLang2 = "메모를 추가하였습니다.";
 var strLang3 = "메모 추가에 실패하였습니다.";
-var strLang4 = "삭제";
+var strLang4 = "메모를 삭제하시겠습니까?";
 var strLang5 = "확인";
 var strLang6 = "취소";
