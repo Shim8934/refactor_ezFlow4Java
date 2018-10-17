95c083235270ea76676970868abf73939d6adcee
moonsurnup <moonsurnup@kaoni.com>
2018-10-17 17:39:02 +0900

    새로운 파일 추가

---
+++ src/main/java/egovframework/ezEKP/ezPortal/web/EzPortalExchangeController.java
@@ -0,0 +1,82 @@
+package egovframework.ezEKP.ezPortal.web;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.w3c.dom.Document;
+
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.service.EgovFileMngUtil;
+import egovframework.ezEKP.ezCommon.service.EzCommonService;
+import egovframework.ezEKP.ezOrgan.service.EzOrganService;
+import egovframework.ezEKP.ezPersonal.service.EzPersonalService;
+import egovframework.ezEKP.ezPoll.service.EzPollService;
+import egovframework.ezEKP.ezPortal.service.EzPortalAdminService;
+import egovframework.ezEKP.ezPortal.service.EzPortalService;
+import egovframework.ezEKP.ezQuestion.service.EzQuestionService;
+import egovframework.let.user.login.service.LoginService;
+import egovframework.let.user.login.vo.LoginVO;
+import egovframework.let.utl.fcc.service.ClientUtil;
+import egovframework.let.utl.fcc.service.CommonUtil;
+import egovframework.let.utl.fcc.service.EgovDateUtil;
+import egovframework.let.utl.sim.service.EgovFileScrty;
+
+@Controller
+public class EzPortalExchangeController extends EgovFileMngUtil {
+private static final Logger logger = LoggerFactory.getLogger(EzPortalController.class);
+	
+	@Autowired
+	private CommonUtil commonUtil;
+
+	@Autowired
+	private Properties config;
+	
+	@Resource(name="EzPortalService")
+	private EzPortalService ezPortalService;
+	
+	@Resource(name="EzPortalAdminService")
+	private EzPortalAdminService ezPortalAdminService;
+	
+	@Resource(name="loginService")
+	private LoginService loginService;
+
+	@Resource(name="crypto") 
+	private EgovFileScrty egovFileScrty;
+	
+	@Resource(name="egovMessageSource")
+	private EgovMessageSource egovMessageSource;
+	
+	@Resource(name="EzCommonService")
+	private EzCommonService ezCommonService;
+	
+}
