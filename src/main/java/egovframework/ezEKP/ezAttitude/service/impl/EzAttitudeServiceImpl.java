2723510a316d287f9b750f62a964973ab9d1b36b
Bae Hyeon Sang <plm2333@kaoni.com>
2018-11-20 15:21:54 +0900

    근태관리 관리자 오라클 컨버팅으로 인한 파라메타 추가

--- src/main/java/egovframework/ezEKP/ezAttitude/service/impl/EzAttitudeServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezAttitude/service/impl/EzAttitudeServiceImpl.java
@@ -576,6 +576,8 @@ public class EzAttitudeServiceImpl implements EzAttitudeService{
 		map.put("orderCell", orderCell);
 		map.put("orderOption", orderOption);
 		map.put("offsetMin", offsetMin);
+		map.put("startRow", limit + 1);
+		map.put("endRow", limit + Integer.valueOf(listSize));
 		if (primary.equals("1")) {
 			primary = "";
 		}
@@ -973,6 +975,8 @@ public class EzAttitudeServiceImpl implements EzAttitudeService{
 		map.put("tenantId", tenantId);
 		map.put("limit", limit);
 		map.put("deptIdList", deptIdList);
+		map.put("startRow", limit + 1);
+		map.put("endRow", limit + Integer.valueOf(listSize));
 		if (primary.equals("1")) {
 			primary = "";
 		}
