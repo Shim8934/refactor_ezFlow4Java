97c43ba5dc791d9f6317404152a9a1f32613ac44
EunJeong Yoo <aliceyoo@kaoni.com>
2018-12-05 14:38:39 +0900

    퀵링크 권한 추가

--- src/main/java/egovframework/ezEKP/ezNewPortal/service/EzNewPortalService.java
+++ src/main/java/egovframework/ezEKP/ezNewPortal/service/EzNewPortalService.java
@@ -41,8 +41,8 @@ public interface EzNewPortalService {
 	public List<MenuInfoVO> getCompanyMenuList(String companyId, int tenantId, String langType, String userId, String deptId) throws Exception;
 	public void updateUserMenuOrder(String companyId, int tenantId, String userId, JSONObject jObj) throws Exception;
 	public void deleteUserMenuOrder(String companyId, int tenantId, String userId) throws Exception;
-	public List<?> getQuickLinkList(String companyId, int tenantId, int page, int limit) throws Exception; 
-	public int getQuickLinkTotalPageCnt(String companyId, int tenantId, int limit) throws Exception;
+	public List<?> getQuickLinkList(String companyId, int tenantId, int page, int limit, String userId, String deptId) throws Exception; 
+	public int getQuickLinkTotalPageCnt(String companyId, int tenantId, int limit, String userId, String deptId) throws Exception;
 //	public List<?> getUserPortletList(String portletLang, String userId, int tenantId, String companyId, String deptId) throws Exception;
 	public List<PortletInfoVO> getUserPortletList(int themeId, String portletLang, String userId, int tenantId, String companyId, String deptId, boolean config) throws Exception;
 	public List<?> getUserFrameListAndSelectedFrame(String companyId, int tenantId, String userId) throws Exception;
