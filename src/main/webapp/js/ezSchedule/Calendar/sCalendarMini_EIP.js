35968b0ddd3942b181b27ce400d44865f8663622
guhaean <zdki129@kaoni.com>
2018-12-17 11:02:18 +0900

    *테마1, 3 버그수정

--- src/main/webapp/js/ezSchedule/Calendar/sCalendarMini_EIP.js
+++ src/main/webapp/js/ezSchedule/Calendar/sCalendarMini_EIP.js
@@ -348,6 +348,19 @@ function CalendarMiniDataSource() {
 function sTempData() {
 }
 
+function dateDiff(_date1, _date2) {
+    var diffDate_1 = _date1 instanceof Date ? _date1 : new Date(_date1);
+    var diffDate_2 = _date2 instanceof Date ? _date2 : new Date(_date2);
+ 
+    diffDate_1 = new Date(diffDate_1.getFullYear(), diffDate_1.getMonth()+1, diffDate_1.getDate());
+    diffDate_2 = new Date(diffDate_2.getFullYear(), diffDate_2.getMonth()+1, diffDate_2.getDate());
+ 
+    var diff = Math.abs(diffDate_2.getTime() - diffDate_1.getTime());
+    diff = Math.ceil(diff / (1000 * 3600 * 24));
+ 
+    return diff;
+}
+
 function getCalendarMiniDataSource_after(resultList) {
     var tempData = new Array();
     var k = 0;
@@ -363,6 +376,10 @@ function getCalendarMiniDataSource_after(resultList) {
     		var betweenDay = new Date(_Dtend.substring(0, 10)) - new Date(_Dtstart.substring(0, 10));
     		var day = 1000 * 60 * 60 * 24;
     		betweenDay = parseInt(betweenDay / day, 10);
+    		
+            if (_Dtend.substring(10) == " 00:00:00.0") {
+            	betweenDay = betweenDay - 1;
+            }
     		
     		for (var j = 0; j <= betweenDay; j++) {
     			
