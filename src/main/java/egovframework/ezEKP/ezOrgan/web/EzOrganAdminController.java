e13cac7f917405fd9b527192c8e4e66af78ca53c
skyblue0o0 <skyblue0o0@jgw.kaoni.com>
2019-01-03 23:59:44 +0900

    * 사원 삭제 시 alias 메일주소 삭제되도록 수정, 사원 삭제 시 주소록 삭제 서비스 수정 등

--- src/main/java/egovframework/ezEKP/ezOrgan/web/EzOrganAdminController.java
+++ src/main/java/egovframework/ezEKP/ezOrgan/web/EzOrganAdminController.java
@@ -1294,15 +1294,6 @@ public class EzOrganAdminController extends EgovFileMngUtil {
 							
 							logger.debug("updateGroupDel rc=" + rc);							
 						}
-						
-						// 공유사서함 기능 사용 시 공유사서함의 공유자에서 해당 유저를 제외한다.
-						String useSharedMailbox = ezCommonService.getTenantConfig("useSharedMailbox", tenantID);
-			    		
-			    		if (useSharedMailbox.equals("YES")) {
-			    			rc = ezEmailService.deleteUserFromAllSharedMailbox(cn[i], tenantID);
-			    			
-			    			logger.debug("deleteUserFromAllSharedMailbox rc=" + rc);
-			    		}
 					} else {
 						logger.debug("retiring the user '" + mailAddr + "' failed.");
 						
@@ -1360,11 +1351,25 @@ public class EzOrganAdminController extends EgovFileMngUtil {
 				// 퇴직자 계정을 삭제한다.
 				ezEmailUserAdminService.removeUser(mailAddr);
 				
-				// 해당 사용자의 메일박스들을 모두 제거한다.
-				ezEmailUserAdminService.removeUserAllMailboxes(mailAddr);
+				// 공유사서함 기능 사용 시 공유사서함의 공유자에서 해당 유저를 제외한다.
+				String useSharedMailbox = ezCommonService.getTenantConfig("useSharedMailbox", tenantID);
+	    		
+	    		if (useSharedMailbox.equals("YES")) {
+	    			rc = ezEmailService.deleteUserFromAllSharedMailbox(cn[i], tenantID);
+	    			logger.debug("deleteUserFromAllSharedMailbox rc=" + rc);
+	    		}
 				
+	    		// 해당 사용자의 alias 메일주소를 삭제한다.
+	    		rc = ezEmailService.deleteIndividualAlias(cn[i], tenantID);
+	    		logger.debug("deleteIndividualAlias rc=" + rc);
+	    		
+				// 해당 사용자의 메일박스들을 모두 제거한다.
+	    		rc = ezEmailUserAdminService.removeUserAllMailboxes(mailAddr);
+	    		logger.debug("removeUserAllMailboxes rc=" + rc);
+	    		
 				// 해당 사용자의 개인주소록 및 주소록 관련 설정을 모두 제거한다.
-				ezAddressService.removeUserAddress(mailAddr);
+	    		rc = ezAddressService.removeUserAddress(mailAddr);
+	    		logger.debug("removeUserAddress rc=" + rc);
 			}
 			// dhlee - end
 		}		
