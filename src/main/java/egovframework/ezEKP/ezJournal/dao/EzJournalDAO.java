63dcb3844a25b875e88a963b87f1694dba0a09a5
parksungbin <sbpark14@kaoni.com>
2019-01-14 13:51:11 +0900

    * 업무일지 수신자 정보 저장 수정

--- src/main/java/egovframework/ezEKP/ezJournal/dao/EzJournalDAO.java
+++ src/main/java/egovframework/ezEKP/ezJournal/dao/EzJournalDAO.java
@@ -343,7 +343,7 @@ public class EzJournalDAO extends EgovAbstractDAO{
 	 */
 	public void insertViewInfo(Map<String, Object> map){
 		try {
-			insert("insertViewDate",map);
+			map.put("isUser", select("selectRecvUser",map));
 			update("updateRecvDate",map);
 		} catch (Exception e) {
 			e.printStackTrace();
