10bbbc0e1671495373060dcffe786c0918d6199e
HongSeungBi <hsb1024@kaoni.com>
2019-04-01 14:49:20 +0900

    게시판 > 에러 확인 로그 및 게시물 저장 관련 로그 추가

--- src/main/java/egovframework/ezEKP/ezBoard/web/EzBoardController.java
+++ src/main/java/egovframework/ezEKP/ezBoard/web/EzBoardController.java
@@ -6005,6 +6005,7 @@ public class EzBoardController extends EgovFileMngUtil{
 				rtnValue = "OK";
 			} catch (Exception e) {
 				logger.error("EzBoard :: deleteImageItem");
+				e.printStackTrace();
 				rtnValue = "ERROR";
 			}
 		} else if (mod.equals("Mod")) {
@@ -7426,6 +7427,7 @@ public class EzBoardController extends EgovFileMngUtil{
 				}
 			} catch (Exception e) {
 				logger.debug("uploadBackImage error");
+				e.printStackTrace();
 			}
 		}
 		
@@ -8204,6 +8206,7 @@ public class EzBoardController extends EgovFileMngUtil{
 				try {
 					zos.close();
 				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
 		}
