06c781063d9f852faf372f173b36dc679dbfa0f2
skyblue0o0 <skyblue0o0@jgw.kaoni.com>
2019-05-23 13:25:56 +0900

    * isHtmlMessage 함수를 EzEmailUtil 클래스로 옮김

--- src/main/java/egovframework/ezEKP/ezEmail/web/EzEmailMailWriteController.java
+++ src/main/java/egovframework/ezEKP/ezEmail/web/EzEmailMailWriteController.java
@@ -1132,7 +1132,7 @@ public class EzEmailMailWriteController extends EgovFileMngUtil {
 		        		}
 		        		
 		        		//set bodyType
-		        		bodyType = isHtmlMessage(orgMessage) ? "0" : "1";
+		        		bodyType = ezEmailUtil.isHtmlMessage(orgMessage) ? "0" : "1";
 		        		
 		        		if (orgMessage.getHeader("Return-Receipt-To") != null) {
 		        			replySendTime = "1";
@@ -5685,55 +5685,6 @@ public class EzEmailMailWriteController extends EgovFileMngUtil {
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	private boolean isHtmlMessage(Message message) throws MessagingException, IOException {
-		if (message.getHeader("Content-Type") == null) {
-			return true;
-		}
-		
-		String tempBodyType = message.getHeader("Content-Type")[0];
-		String contentType = tempBodyType.split(";")[0].trim();
-
-		if (contentType.equals("text/plain")) {
-			return false;
-		} else if (contentType.equals("multipart/alternative")) {
-			return true;
-		}
-		
-		Object content = message.getContent();
-		
-		if (content instanceof Multipart) {
-			return containsHtmlMultipart((Multipart) content);
-		}
-		
-		return true;
-	}
-	
-	private boolean containsHtmlMultipart(Multipart multipart) throws MessagingException, IOException {
-		int partCount = multipart.getCount();
-		
-		Object partContent;
-
-		for (int i = 0; i < partCount; i++) {
-			BodyPart bodyPart = multipart.getBodyPart(i);
-			
-			if (BodyPart.ATTACHMENT.equalsIgnoreCase(bodyPart.getDisposition())) {
-				continue;
-			}
-			
-			partContent = bodyPart.getContent();
-			
-			if (partContent instanceof Multipart && containsHtmlMultipart((Multipart) partContent)) {
-				return true;
-			}
-
-			if (bodyPart.isMimeType("text/html") || bodyPart.isMimeType("message/*")) {
-				return true;
-			}
-		}
-
-		return false;
 	}
 	
 	/**
