59c81e804c1af76eaa0c1a0c23a246dc90a80637
eunseok <kimeunseok@kaoni.com>
2019-07-29 14:33:57 +0900

    관리자 - 근태관리 - 연차현황 - 액셀 업로드 - 기본양식 헤더 영어로 변경

--- src/main/java/egovframework/ezEKP/ezAttitude/web/EzAttitudeAdminController.java
+++ src/main/java/egovframework/ezEKP/ezAttitude/web/EzAttitudeAdminController.java
@@ -2287,9 +2287,9 @@ public class EzAttitudeAdminController {
 		pFileName = "annualReport_format";
 		
 		//header
-		row.createCell(0).setCellValue(egovMessageSource.getMessage("ezEmail.t263", userInfo.getLocale()));
-		row.createCell(1).setCellValue(egovMessageSource.getMessage("ezAttitude.t289", userInfo.getLocale()));
-		row.createCell(2).setCellValue(egovMessageSource.getMessage("ezAttitude.t290", userInfo.getLocale()));
+		row.createCell(0).setCellValue(egovMessageSource.getMessage("ezAttitude.t330", userInfo.getLocale()));
+		row.createCell(1).setCellValue(egovMessageSource.getMessage("ezAttitude.t331", userInfo.getLocale()));
+		row.createCell(2).setCellValue(egovMessageSource.getMessage("ezAttitude.t332", userInfo.getLocale()));
 		row.getCell(0).setCellStyle(headerStyle);
 		row.getCell(1).setCellStyle(headerStyle);
 		row.getCell(2).setCellStyle(headerStyle);
