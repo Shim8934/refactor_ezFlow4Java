f964f13700dac4fdbb51bc1ce1c3c705f73f0855
Dongho Lee <dhlee@kaoni.com>
2019-11-09 01:12:15 +0900

    개인정보관리에서 프로필이미지를 포함 개인정보 수정 시 ezTalkSyncServer를 호출하여 동기화 수행하도록 함.
      - 프로필 이미지 삭제 시에도 동기화 수행하도록 함.

--- src/main/java/egovframework/ezEKP/ezPersonal/web/EzPersonalController.java
+++ src/main/java/egovframework/ezEKP/ezPersonal/web/EzPersonalController.java
@@ -1243,6 +1243,13 @@ public class EzPersonalController extends EgovFileMngUtil {
 		
 		String result = ezOrganService.updateProperty(userInfo.getId(), "extensionAttribute2", "", "user", userInfo.getTenantId());
 
+	    SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        date.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String nowDate = date.format(new Date()); 
+
+        // 비즈메카톡과의 프로필 사진 연동을 위해 updateDT 필드를 갱신한다.
+        ezOrganAdminService.updateProperty(userInfo.getId(), "updateDT", nowDate, "user", userInfo.getTenantId());
+		
         String useBizmekaTalk = ezCommonService.getTenantConfig("UseBizmekaTalk", userInfo.getTenantId());
         
         if (useBizmekaTalk.equals("YES")) {
