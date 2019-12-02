84c893bc43019863dccaa79ec04883241a085338
HongSeungBi <hsb1024@kaoni.com>
2019-12-02 16:26:07 +0900

    * bug.kaoni.com(#17024) 게시판 > 게시글 임시저장 > 재작성 > 옵션에 예약 게시일이 나타나지 않음
    임시저장과 예약게시 기능을 동시에 사용 가능하도록 수정

--- src/main/java/egovframework/ezEKP/ezBoard/vo/BoardListVO.java
+++ src/main/java/egovframework/ezEKP/ezBoard/vo/BoardListVO.java
@@ -142,6 +142,8 @@ public class BoardListVO {
 	private String content;
 	/** 2019-11-06 홍승비 - 미리보기 시 댓글옵션 변수 추가 */
 	private String oneLineReply;
+	/** 2019-12-02 홍승비 - 임시저장 기능과 예약게시 기능의 동시 사용을 위한 예약게시물 여부 변수 추가 */
+	private String isReserved;
 
 	public int getrNum() {
 		return rNum;
@@ -563,5 +565,10 @@ public class BoardListVO {
 	public void setOneLineReply(String oneLineReply) {
 		this.oneLineReply = oneLineReply;
 	}
-	
+	public String getIsReserved() {
+		return isReserved;
+	}
+	public void setIsReserved(String isReserved) {
+		this.isReserved = isReserved;
+	}
 }
