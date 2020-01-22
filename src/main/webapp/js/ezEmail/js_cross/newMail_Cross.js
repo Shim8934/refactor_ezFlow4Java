5402c1f4993c6377e114ec35cc90af576a9c84f9
jwseo99 <jwseo99@kaoni.com>
2020-01-22 09:48:26 +0900

    메일 쓰기 수신인 추가시 따옴표 넣을때 x이미지로 삭제 안 됨

--- src/main/webapp/js/ezEmail/js_cross/newMail_Cross.js
+++ src/main/webapp/js/ezEmail/js_cross/newMail_Cross.js
@@ -3468,6 +3468,9 @@ function GetGroupEmail(pAddressId) {
 function PrepareMailTag(iWhich, type, name, email, href) {
     var TopSpan = document.createElement("span");
     var newElem = document.createElement("span");
+    // 앞 뒤로 따옴표 제거
+    name = name.replace(/^["']/, "").replace(/["']$/, "");
+    email = email.replace(/^["']/, "").replace(/["']$/, "");
     
     // 수신인 추가 정보 (부서 이름 또는 이메일 주소)
     if (g_useAdditionalInfo) {
