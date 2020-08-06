88587d9ac7eb17d156bcd72b5f13218e5a127e38
KimMinSeong <mins99@kaoni.com>
2020-08-06 14:32:23 +0900

    * 메뉴별 숨김 기능 관련 init데이터 추가

--- src/main/java/egovframework/ezEKP/ezOrgan/web/EzOrganAdminController.java
+++ src/main/java/egovframework/ezEKP/ezOrgan/web/EzOrganAdminController.java
@@ -224,6 +224,7 @@ public class EzOrganAdminController extends EgovFileMngUtil {
 	    	ezCommonService.insertBeforeOutComeAttitudeType(); // 2020-06-03  김정언 - 근태관리 휴가유형 전일퇴근 추가
 	    	ezCommonService.insertMobileAttitudeColumn();			// 2020-06-10 김민성 - 모바일 근태관리 기능 추가
 	    	ezCommonService.createTblNoticeBoard(); //2020-06-17 홍승비 - 회사별 공지사항 게시판 기능을 위한 NOTICEBOARD 테이블 추가
+	    	ezCommonService.createMenuTenantConfig();		// 2020-08-06 김민성 - 메뉴 숨김처리 관련 config 추가
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
