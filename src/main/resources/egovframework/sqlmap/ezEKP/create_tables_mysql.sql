5b7919a07d95a886e6136852b4a9b0ea0d2f4469
HongSeungBi <hsb1024@kaoni.com>
2021-04-06 16:11:15 +0900

    * 회람판 > 테이블 인덱스 추가 (TBL_CIRCULAR_LINK, TBL_CIRCULAR_USER)
    JOIN에 사용되나 인덱스가 존재하지 않아 속도가 느려지는 현상 방지
    표준DB(10.0.102.8)에도 반영함

--- src/main/resources/egovframework/sqlmap/ezEKP/create_tables_mysql.sql
+++ src/main/resources/egovframework/sqlmap/ezEKP/create_tables_mysql.sql
@@ -5258,7 +5258,9 @@ CREATE TABLE `tbl_circular_link` (
   `memberId` varchar(100) DEFAULT NULL,
   `tenantId` mediumint(5) NOT NULL,
   PRIMARY KEY (`circularLinkId`),
-  KEY `tenantId_memberId_index` (`tenantId`,`memberId`)
+  KEY `tenantId_memberId_index` (`tenantId`,`memberId`),
+  KEY `idx_tbl_circular_link_circularId` (`circularId`),
+  KEY `idx_tbl_circular_link_circularFolderId` (`circularFolderId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -5326,7 +5328,8 @@ CREATE TABLE `tbl_circular_user` (
   `tenantId` mediumint(5) NOT NULL,
   `companyId` varchar(80) DEFAULT NULL,
   PRIMARY KEY (`circularUserId`),
-  KEY `tenantId_memberId_circularId_index` (`tenantId`,`memberId`,`circularId`)
+  KEY `tenantId_memberId_circularId_index` (`tenantId`,`memberId`,`circularId`),
+  KEY `idx_tbl_circular_user_circularId` (`circularId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
