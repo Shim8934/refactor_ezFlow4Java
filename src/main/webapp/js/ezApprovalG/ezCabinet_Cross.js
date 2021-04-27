12a2c733f3b93da59270aef28998b192a44c4fec
KimMinSeong <mins99@kaoni.com>
2021-04-27 13:12:42 +0900

    전자결재 > 미처리문서함 조회 오류 수정

--- src/main/webapp/js/ezApprovalG/ezCabinet_Cross.js
+++ src/main/webapp/js/ezApprovalG/ezCabinet_Cross.js
@@ -1686,7 +1686,7 @@ function makePageSelPage(pTotalCnt) {
             document.getElementById("TitleInfo").innerHTML = "&nbsp;&nbsp;<span style='color:#017BEC;font-weight:bold;'>" + pTotalCnt + "</span>&nbsp;/ " + period;
 
         if (g_sFlag === "UNTREATED") {
-            parent.frames["left"].document.getElementById("countUntreated").innerHTML = "&nbsp;&nbsp;" + pTotalCnt;
+            parent.frames["left"].document.getElementById("COUNTUNTREATED").innerHTML = "&nbsp;&nbsp;" + pTotalCnt;
         }
     }
 
