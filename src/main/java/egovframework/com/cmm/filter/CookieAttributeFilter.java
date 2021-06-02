447cc27a8221ed95f7734707a412af500745e7b1
suasua <tndk19@kaoni.com>
2021-06-02 09:41:43 +0900

    [강남대] SSO 개발
    - (크롬) SSO 글로벌 로그아웃 오류 수정
    - 크롬에서 CSRF 취약점을 방지하기 위해 이전에 쿠키의 SameSite를 None 처리한것과 동일하게 처리했지만 현재는 Lax로 설정한것과 동일하게 동작하게 바뀜
    - 이때문에 통합포탈에서 우리쪽 로그아웃 url 호출시 쿠키가 넘어오지 않는 것으로 확인되어 우리쪽 쿠키를 set-Cookie할때 쿠키의 SameSite를 None으로 설정함

---
+++ src/main/java/egovframework/com/cmm/filter/CookieAttributeFilter.java
@@ -0,0 +1,36 @@
+package egovframework.com.cmm.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+public class CookieAttributeFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		String scheme = request.getScheme();
+		
+		CookieAttributeFilterWrapper rsw = new CookieAttributeFilterWrapper(response);
+		rsw.setIsSecure(scheme);
+		
+		chain.doFilter(request, rsw);
+	}
+}
+
