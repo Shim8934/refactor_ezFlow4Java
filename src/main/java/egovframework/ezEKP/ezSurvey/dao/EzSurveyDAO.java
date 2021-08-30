7398b15ce0cdcd5f46226d9a7cf7d5b1925caab7
HongSeungBi <hsb1024@kaoni.com>
2021-08-30 13:50:21 +0900

    * voc.kaoni.com(#84011) 전자설문 > 대상자 지정 > 개인(사용자)과 함께 부서/직위직책/권한그룹 지정 시 설문저장 불가능 (오류 발생)
    1) 전자설문 대상자 선택 팝업 > 사원 개인 TR 선택 이후 부서추가 시 userType 구분값이 dept가 아닌 오류 수정
    2) 전자설문 대상자 선택 팝업 > 사원 개인을 대상으로 지정하는 경우, deptId값이 부서명으로 저장되는 오류 수정
    3) 사간겸직자를 대상자로 선택하는 경우, 전자설문 수정 시 사원 정보를 찾지 못하는 오류 수정 (겸직정보 가져오도록 수정)
    4) 전자설문 생성 중 임시저장, 저장완료, 취소 동작 시 생성창 진입 확인여부 플래그값 false로 변경

--- src/main/java/egovframework/ezEKP/ezSurvey/dao/EzSurveyDAO.java
+++ src/main/java/egovframework/ezEKP/ezSurvey/dao/EzSurveyDAO.java
@@ -251,4 +251,9 @@ public class EzSurveyDAO extends EgovAbstractDAO {
 	public void deleteRespondents(Map<String, Object> map) {
 		delete("EzSurveyDAO.deleteRespondents", map);
 	}
+
+	/* 2021-08-30 홍승비 - 전자설문 대상자가 사간겸직자인 경우, 해당 부서ID를 조건으로 레코드를 리턴 */
+	public SimpleUserVO getSurveyUserInfoAddJob(Map<String, Object> map) {
+		return (SimpleUserVO)select("EzSurveyDAO.getSurveyUserInfoAddJob", map);
+	}
 }
