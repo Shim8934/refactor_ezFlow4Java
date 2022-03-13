001806e83ed72323c54c561ed871a446201b4f03
JungJuHwan <jungjuhwan1000@kaoni.com>
2022-03-13 14:13:19 +0900

    모두결재 버튼 숨김처리

--- src/main/webapp/js/ezApprovalG/aprmanage_Cross.js
+++ src/main/webapp/js/ezApprovalG/aprmanage_Cross.js
@@ -2162,7 +2162,7 @@ function setbuttonenable() {
                 if (pListTypeValue == "11") {
                 	document.getElementById("tbtnApprove1").style.display = "none";
                 } else {
-                	document.getElementById("tbtnApprove1").style.display = "";
+//                	document.getElementById("tbtnApprove1").style.display = "";
                 }
                 //document.getElementById("tbtnApproveALL").style.display = "";
 
