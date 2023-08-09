a25d32e81d7c9d98fbf2f7b94c76c37bc096f7b1
Min Hyeok Ki <als6998@kaoni.com>
2023-08-09 18:03:49 +0900

    [QST] voc #120193: 전자결재>결재할문서>항목 선택하지 않고 일괄결재 버튼 누를 시 알림창 텍스트 수정 필요
    알림창 텍스트 수정

--- src/main/webapp/js/ezApprovalG/lang/ezapprovalG_US.js
+++ src/main/webapp/js/ezApprovalG/lang/ezapprovalG_US.js
@@ -639,7 +639,7 @@ var strLang926 = "An error occurred during upload. \n \nfile type (extension), p
 var strLang928 = "Records to retrieve information failed!";
 var strLang929 = "Do not have permission to view this document. <br> Draft, please contact or.";
 // ApprovUI/doApprovAllSelect.aspx
-var strLang930 = "The selected article does not.";
+var strLang930 = "There are no documents selected.";
 var strLang931 = "<br> Batch Approval is completed.";
 var strLang931_1 = "Batch Approval is completed.";
 var strLang932 = "An error has occurred in the batch approval. <br>";
