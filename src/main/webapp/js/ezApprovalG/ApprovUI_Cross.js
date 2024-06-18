544bf77460fca99e01dc39b85c777f410f258074
Jihye Yang <jhyang@kaoni.com>
2024-06-18 09:08:05 +0900

    * voc #140573 전자결재G>비전자문서등록 및 분리첨부 등록>등록 후 접수>분리첨부를 편철할수 없음
      1) 분리첨부가 있는 비전자문서 접수 시, 분리첨부의 기록물철을 선택할 수 있도록 수정
      2) 결재 시 분리첨부의 cabinetID를 가져오지 못하는 문제 수정
        ; 중복코드 제거 및 SEPCABINETID를 확인하도록 함
      3) 전결 시 분리첨부 정보를 가져오지 못하는 문제 수정
        ; SEPERATEATTACH 하위 태그를 확인하여 분기를 나눔
    
    * voc #140573 전자결재G>비전자문서등록 및 분리첨부 등록>등록 후 접수>분리첨부를 편철할수 없음 (2)
    
      - 분리첨부 없이 비전자문서등록 후 접수 시 오류 수정

--- src/main/webapp/js/ezApprovalG/ApprovUI_Cross.js
+++ src/main/webapp/js/ezApprovalG/ApprovUI_Cross.js
@@ -1793,27 +1793,11 @@ function SaveApproveInfo(pApproveFlag) {
             
             for (i = 0; i < rows.length; i++) {
                 sepAtt = createNodeAndAppandNode(sepLVXml, root, sepAtt, "SEPATTACH");
-                Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "CABINETID", SelectSingleNodeValue(rows[i], "CABINETID"));
-                Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "TITLE", SelectSingleNodeValue(rows[i], "SEPTITLE"));
-                Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "NUMOFPAGE", SelectSingleNodeValue(rows[i], "SEPNUMOFPAGE"));
-                Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "REGTYPE", SelectSingleNodeValue(rows[i], "SEPREGTYPE"));
-                Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "SUMMARY", SelectSingleNodeValue(rows[i], "SEPSUMMARY"));
-                Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "AVTYPE", SelectSingleNodeValue(rows[i], "SEPRECORDTYPE"));
-            }
-            
-            createNodeAndInsertText(xmlpara, objNode, "NONELECREC_SEPERATEATTACH", getXmlString(rtnXml));
-            
-		} else if (SelectNodes(NonElecXML, "NONELECRECINFO/NONELECREC/SEPERATEATTACH/ROWS/ROW").length > 0) {
-			var sepAtt, Data, i;
-			var rtnXml = createXmlDom();
-	        var root = createNodeInsert(rtnXml, root, "SEPATTACHINFO");
-			var sepLVXml = createXmlDom();
-            	sepLVXml = loadXMLString(nonElecRecInfoXml);
-            var rows = SelectNodes(sepLVXml, "NONELECRECINFO/NONELECREC/SEPERATEATTACH/ROWS/ROW");
-            
-            for (i = 0; i < rows.length; i++) {
-                sepAtt = createNodeAndAppandNode(sepLVXml, root, sepAtt, "SEPATTACH");
-                Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "CABINETID", SelectSingleNodeValue(rows[i],"SEPCABINETID"));
+                if (SelectSingleNodeValue(rows[i], "SEPCABINETID") != "") {
+                    Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "CABINETID", SelectSingleNodeValue(rows[i], "SEPCABINETID"));
+                } else {
+                    Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "CABINETID", SelectSingleNodeValue(rows[i], "CABINETID"));
+                }
                 Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "TITLE", SelectSingleNodeValue(rows[i], "SEPTITLE"));
                 Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "NUMOFPAGE", SelectSingleNodeValue(rows[i], "SEPNUMOFPAGE"));
                 Data = createNodeAndAppandNodeText(sepLVXml, sepAtt, Data, "REGTYPE", SelectSingleNodeValue(rows[i], "SEPREGTYPE"));
