6a742f20dd3b1e05c0ea1dc5a6bbfc866487e75b
Seulgi Han <kihsg@kaoni.com>
2024-06-27 14:13:56 +0900

    voc - 메일 > 보낸편지함 > 수신확인/회수 > 부서메일 전체회수 > 부서원 재발송 버튼 클릭 시 결함(#138578) (2) voc - 메일>보낸편지함>수신확인/회수>재발송>리스트 처음주소를 제외하고 받는사람명이 null로 나타남(결함 #140765)
    	편지읽기 > 수신확인/회수 > 재발송시 메일쓰기 화면에 이름이 null로 나타나는 오류 수정

--- src/main/webapp/js/ezEmail/js_cross/reademail.js
+++ src/main/webapp/js/ezEmail/js_cross/reademail.js
@@ -76,6 +76,25 @@ function ReSend(pURL, pEmail) {
     }*/
 }
 
+// 2024.05.24 한슬기 : 수신인 이름을 사용하기위해 오버라이드
+function ReSend(pURL, pEmail, pReader) {
+    var pheight = window.screen.availHeight;
+    var conHeight = pheight * 0.8;
+    var pwidth = window.screen.availWidth;
+    var pTop = (pheight - conHeight) / 2;
+    var pLeft = (pwidth - minimumWidth) / 2;
+    var feature = "top=" + pTop.toString() + ", left=" + pLeft.toString() + ", height = " + conHeight + "px, width = 890px, status = no, toolbar=no, menubar=no,location=no,resizable=1";
+    
+    var requestUrl = "/ezEmail/mailWrite.do?url=" + encodeURIComponent(pURL) + "&cmd=RESEND&msgto=" + encodeURIComponent(pEmail) + "&reciverName=" + encodeURIComponent(pReader);
+    
+	if (typeof(shareId) != "undefined" && shareId != "") {
+		requestUrl += "&shareId=" + encodeURIComponent(shareId);
+	}
+    
+    window.open(requestUrl, "", feature);
+    
+}
+
 function encoding_mail() {
     try {
         var infolder = "/exchange/" + CutBeforeText(g_paramURL, "/exchange/");
