8cbcbecc7ec5c2879bc4b87e69575586744ee75a
TaeHoon Han <othankloveo@kaoni.com>
2024-07-09 10:35:08 +0900

    [표준모듈] 통합알림 > #voc 141803 포탈>통합알림>설문 알림>신규 등록 된 설문 삭제 후 알림 클릭 시 "권한이 없습니다." 라고 나타남

--- src/main/java/egovframework/ezEKP/ezSurvey/dao/EzSurveyDAO.java
+++ src/main/java/egovframework/ezEKP/ezSurvey/dao/EzSurveyDAO.java
@@ -278,4 +278,10 @@ public class EzSurveyDAO extends EgovAbstractDAO {
 	public void setPreviewFlag(Map<String, Object> map) {
 		update("EzSurveyDAO.setPreviewFlag", map);
 	}
+
+	public boolean comfirmSurveyDeletion(Map<String, Object> map) {
+		boolean isDeletedSurvey = (boolean) select("EzSurveyDAO.comfirmSurveyDeletion", map);
+		
+		return isDeletedSurvey;
+	}
 }
