b08cb8a8682f2447be2b3f0a3998a0e2886f768d
Inkyung Hwang <hik12@kaoni.com>
2024-08-29 17:23:50 +0900

    * voc.kaoni.com(#145175) 회람판 > [의견목록] > 날짜 2줄로 표시되는 결함

--- src/main/webapp/js/ezCircular/circularComment.js
+++ src/main/webapp/js/ezCircular/circularComment.js
@@ -64,7 +64,7 @@ function getCircularComment() {
 					}
 					
 					circularCommentList += "</td>";
-					circularCommentList += "<td style='text-align:right;padding-right:8px'>" + vo.regDate.substring(0, 16) + "</td>";
+					circularCommentList += "<td style='text-align:right;padding-right:5px'>" + vo.regDate.substring(0, 16) + "</td>";
 					circularCommentList += "</tr>";
 					
 					if (vo.status == 0) {
