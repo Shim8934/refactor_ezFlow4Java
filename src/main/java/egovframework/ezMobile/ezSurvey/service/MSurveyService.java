515d75da219868d5cea82efc4e0ac7d684bd6e00
TaeHoon Han <othankloveo@kaoni.com>
2024-09-06 17:16:27 +0900

    * [표준모듈] 포탈 리뉴얼 2차 모바일 > 전자설문 > 아이폰 파일 다운로드 안되는 오류 수정

--- src/main/java/egovframework/ezMobile/ezSurvey/service/MSurveyService.java
+++ src/main/java/egovframework/ezMobile/ezSurvey/service/MSurveyService.java
@@ -17,7 +17,6 @@ public interface MSurveyService {
 	JSONObject checkPermission(List<Long> surveyList, int mode, MCommonVO userInfo) throws Exception;
 	JSONObject getItemInfo(Long surveyId, String mode, String realPath, MCommonVO userInfo) throws Exception;
 	JSONObject getSurveyQuestions(Long surveyId, String logicMode, String realPath, LoginVO userInfo) throws Exception;
-	void getDownloadedFile(String fileName, String filePath, String realPath, String userAgent, HttpServletRequest request, HttpServletResponse response) throws Exception;
 	JSONObject saveResponseItem(JSONArray responses, long surveyId, LoginVO userInfo) throws Exception;
 	JSONObject getSurveyStatistic(Long surveyId, String realPath, LoginVO userInfo, String adminYN) throws Exception;
 }
