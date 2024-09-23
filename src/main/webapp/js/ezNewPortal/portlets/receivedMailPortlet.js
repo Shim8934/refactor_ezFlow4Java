dfff492b7c6a122286d0c309f1c4e1379d4438b4
Daehyeon Kim <dhkim3@kaoni.com>
2024-09-23 17:52:31 +0900

    [voc] #146531 보안메일 포틀릿에서 태그로 표시되는 결함
    	ezMobile 업데이트 필요

--- src/main/webapp/js/ezNewPortal/portlets/receivedMailPortlet.js
+++ src/main/webapp/js/ezNewPortal/portlets/receivedMailPortlet.js
@@ -95,7 +95,11 @@ function getMailList(currPage) {
 					receivedDateStr = mailList[i].receivedDateStr;
 					sender = mailList[i].sender;
 					listHTML2 += "<li class="+readClass+" onclick='open_mail(&#39;" + href + "&#39;)'>";
-					listHTML2 += "<span class='txt'>"+ MakeXMLString(subject) +"</span>";		
+					listHTML2 += "<span class='txt'>";
+					if (mailList[i].securedMail === "true") {
+						listHTML2 += "<img src=\"/images/email/secureMail/security_icon.gif\" width=\"20px\" />";
+					}
+					listHTML2 += MakeXMLString(subject) + "</span>";
 					listHTML2 += "<span class='date'>"+MakeXMLString(receivedDateStr).replace(/-/g, ".")+"</span>";		
 					listHTML2 += "<span class='name'>"+MakeXMLString(sender)+"</span>";	
 					listHTML2 += "</li>";	
