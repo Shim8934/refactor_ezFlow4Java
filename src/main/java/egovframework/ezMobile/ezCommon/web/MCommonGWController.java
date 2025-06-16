aa33e32ce7985a8dbb297dc84fee15ce22d255a3
Jeon Inha <jinha@kaoni.com>
2025-06-16 11:37:02 +0900

    [ezekp365g] EzFAL 모바일 파일다운 적용

--- src/main/java/egovframework/ezMobile/ezCommon/web/MCommonGWController.java
+++ src/main/java/egovframework/ezMobile/ezCommon/web/MCommonGWController.java
@@ -11,6 +11,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import egovframework.let.utl.fcc.service.EzFAL;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -78,7 +79,7 @@ public class MCommonGWController {
 		JSONObject result = new JSONObject();
 		
 		try {
-			File file = new File(filePath);
+			EzFAL.EzFile file = new EzFAL.EzFile(filePath);
 			
 			if (!file.exists()) {
 			    throw new FileNotFoundException(filePath);
@@ -220,7 +221,7 @@ public class MCommonGWController {
 		filePath = realPath + filePath;
 
 		try {
-			File file = new File(filePath);
+			EzFAL.EzFile file = new EzFAL.EzFile(filePath);
 
 			if (!file.exists()) {
 				throw new FileNotFoundException(filePath);
