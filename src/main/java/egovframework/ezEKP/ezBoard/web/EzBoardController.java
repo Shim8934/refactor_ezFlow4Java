85524d13963cd0b74f39ac2ad278bc08638b8d7c
Jeon Inha <jinha@kaoni.com>
2025-07-08 17:02:03 +0900

    #163681 Q&A 게시판> 관리자가 등록한 게시글 표시되지 않음

--- src/main/java/egovframework/ezEKP/ezBoard/web/EzBoardController.java
+++ src/main/java/egovframework/ezEKP/ezBoard/web/EzBoardController.java
@@ -4303,8 +4303,8 @@ public class EzBoardController extends EgovFileMngUtil{
 		}
 		
 		// 어떤 경우든 본인이 작성한 게시글에는 접근할 수 있게 함
-		if (boardItemVO != null) {
-			rtv = boardItemVO.getWriterID().equalsIgnoreCase(userInfo.getId());
+		if (boardItemVO != null && boardItemVO.getWriterID().equalsIgnoreCase(userInfo.getId())) {
+			rtv = true;
 		}
 		logger.debug("accessCheck ended");
 		return rtv;
