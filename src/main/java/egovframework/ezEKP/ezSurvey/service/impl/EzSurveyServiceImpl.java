7cf4975042a0f7b5c9703a87cee7b9102e256eb5
Jeon Inha <jinha@kaoni.com>
2025-11-03 10:21:13 +0900

    Revert "* 전자설문 > 설문수정 > 수정을 저장하지 않아도 수정자 및 수정일 정보가 변경되는 결함 수정"
    This reverts commit 4b2cf617036c3a77ef20a707e5adbc0c0113d161.

--- src/main/java/egovframework/ezEKP/ezSurvey/service/impl/EzSurveyServiceImpl.java
+++ src/main/java/egovframework/ezEKP/ezSurvey/service/impl/EzSurveyServiceImpl.java
@@ -1025,7 +1025,10 @@ public class EzSurveyServiceImpl extends EzFileMngUtil implements EzSurveyServic
 		else if (mode.equals("modify")) {
 			//Change modify status only if it's not draft survey
 			if (survey.getDraftFlag() == 0) {
+				String timeUTC             = commonUtil.getTodayUTCTime("");
 				survey.setModifyFlag(1);
+				survey.setUpdateDate(timeUTC);
+				survey.setUpdateUser(userInfo.getId());
 				survey.setUpdateMode(1);
 				
 				ezSurveyDAO.updateSurveyItem(survey);
@@ -1388,7 +1391,10 @@ public class EzSurveyServiceImpl extends EzFileMngUtil implements EzSurveyServic
 		SurveyVO survey = ezSurveyDAO.getSurveyInfo(map);
 		
 		if (survey.getModifyFlag() == 1) {
+			String timeUTC             = commonUtil.getTodayUTCTime("");
 			survey.setModifyFlag(0);
+			survey.setUpdateDate(timeUTC);
+			survey.setUpdateUser(userInfo.getId());
 			survey.setUpdateMode(1);
 			ezSurveyDAO.updateSurveyItem(survey);
 		}
