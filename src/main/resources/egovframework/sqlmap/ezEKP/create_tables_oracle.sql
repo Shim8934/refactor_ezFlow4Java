ad35ff682921fd673eaaf84be636e60591ca9c37
Dongho Lee <dhlee@kaoni.com>
2026-01-04 03:22:53 +0900

    JMOCHA_ADDRESS_LAST_SENT 테이블명을 소문자로 할 경우 티베로에서 오류 발생하여 수정

--- src/main/resources/egovframework/sqlmap/ezEKP/create_tables_oracle.sql
+++ src/main/resources/egovframework/sqlmap/ezEKP/create_tables_oracle.sql
@@ -608,12 +608,12 @@ AND    ( tbl_aprdocinfo.startdate IS NOT NULL ));
 	"SIMPLE_EMAIL" NVARCHAR2(100)
    ) ;
 --------------------------------------------------------
---  DDL for Table jmocha_address_last_sent
+--  DDL for Table JMOCHA_ADDRESS_LAST_SENT
 -- Oracle 12c 이후부터 제공된 기능: AUTO_INCREMENT = IDENTITY (https://kimvampa.tistory.com/146)
 -- 삽입, 삭제가 빈번한 테이블이라, UNIQUE KEY 사용하지 않음. (* UNIQUE: TENANT_ID, CN, email)
 --------------------------------------------------------
 
-  CREATE TABLE "jmocha_address_last_sent"
+  CREATE TABLE "JMOCHA_ADDRESS_LAST_SENT"
    (	"SEQUENCE" NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	"TENANT_ID" NUMBER(5,0) DEFAULT 0,
 	"CN" NVARCHAR2(80),
