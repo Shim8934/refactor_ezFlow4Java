2117a3ec9db14214f3b51525717d88069f3d0940
Jeon Inha <jinha@kaoni.com>
2026-01-06 11:39:20 +0900

    #155121 관련 테넌트 추가 DML

--- src/main/resources/egovframework/sqlmap/ezEKP/patchScripts/v6.9.15.sql
+++ src/main/resources/egovframework/sqlmap/ezEKP/patchScripts/v6.9.15.sql
@@ -1,1 +1,3 @@
 -- v6.9.15
+-- #155121 결함 수정으로 테넌트 추가
+INSERT INTO TBL_TENANT_CONFIG (TENANT_ID, PROPERTY_NAME, CONFIG_NAME, PROPERTY_VALUE, DESCRIPTION, REGDATE, CONFIG_TYPE) VALUES	(@tenant_id_value, 'accountYearTimeZone', '전자결재 회계년도 계산 시 타임존 선택 옵션', 'KST', '회계년도 계산 시, UTC 또는 KST (한국 표준시)를 사용할지 선택한다. (default: KST)', '2025-04-29 00:00:00', '전자결재G');
